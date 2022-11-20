43601d214ccce40f130da2c378de0336042ce378
tonytonychopper123 <1085500892@qq.com>
2022-11-20 18:47:47 +0800

    fix(base-sheet): remove cellrangemodal
    jiang

--- packages/base-sheets/src/Controller/SheetContainerController.ts
+++ packages/base-sheets/src/Controller/SheetContainerController.ts
@@ -1,6 +1,5 @@
 import { BooleanNumber, CommandManager, ISheetActionData, SheetActionBase } from '@univer/core';
 import { BaseComponentSheet, BaseComponentRender } from '@univer/base-component';
-import { CellRangeModal } from '@univer/style-universheet';
 import { SheetPlugin } from '../SheetPlugin';
 
 export class SheetContainerController {
@@ -94,6 +93,6 @@ export class SheetContainerController {
         for (let k in registerIcon) {
             this._plugin.registerComponent(k, registerIcon[k]);
         }
-        this._plugin.registerModal(CellRangeModal.name, CellRangeModal);
+        // this._plugin.registerModal(CellRangeModal.name, CellRangeModal);
     }
 }

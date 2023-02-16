28641cc35a25d0f3de555112682e0fd2a7250aac
jerry <195658469@qq.com>
2023-02-16 17:49:03 +0800

    flush

--- packages/base-sheets/src/Controller/index.ts
+++ packages/base-sheets/src/Controller/index.ts
@@ -2,11 +2,10 @@
 export * from './AntLineController';
 export * from './CellEditorController';
 export * from './CountBarController';
-export * from './InfoBarController';
 export * from './RightMenuController';
 export * from './Selection/index';
 export * from './SheetBarController';
 export * from './SheetContainerController';
 export * from './SpreadsheetController';
-export * from './ToolbarController';
+export * from './ToolBarController';
 // @endindex

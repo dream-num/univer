1723db6f68efce037c0b90b484e7c7c7057e506b
原心 <yunsin@vip.qq.com>
2023-06-15 17:06:08 +0800

    fix(core/sheets/range): set the operation type for modifying cell data to default (it includes undo)

--- packages/core/src/Sheets/Domain/Range.ts
+++ packages/core/src/Sheets/Domain/Range.ts
@@ -12,7 +12,12 @@ import {
     SetRangeStyleAction,
 } from '../Action';
 
-import { CommandManager, ISheetActionData, Command } from '../../Command';
+import {
+    CommandManager,
+    ISheetActionData,
+    Command,
+    ActionOperationType,
+} from '../../Command';
 
 import { DEFAULT_RANGE, DEFAULT_STYLES, ACTION_NAMES } from '../../Const';
 
@@ -3134,6 +3139,8 @@ export class Range {
                     [startColumn]: value,
                 },
             },
+            // set the operation type for modifying cell data to default (it includes undo)
+            operation: ActionOperationType.DEFAULT_ACTION,
         };
         const command = new Command(
             {

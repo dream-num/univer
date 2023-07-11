a6b6f49c184ffbcf19c7e375bdebe2762e43c279
jerry <195658469@qq.com>
2023-07-11 16:44:44 +0800

    fix(code): numfmt plugin code fix

--- packages/sheets-plugin-numfmt/src/NumfmtPlugin.ts
+++ packages/sheets-plugin-numfmt/src/NumfmtPlugin.ts
@@ -1,9 +1,11 @@
-import { SheetContext, IRangeData, ObjectMatrixPrimitiveType, Plugin, UniverSheet } from '@univerjs/core';
+import { SheetContext, IRangeData, ObjectMatrixPrimitiveType, Plugin } from '@univerjs/core';
 import { NumfmtPluginObserve, install } from './Basics/Observer';
 import { NUMFMT_PLUGIN_NAME } from './Basics/Const/PLUGIN_NAME';
 import { NumfmtController } from './Controller/NumfmtController';
 import { NumfmtModalController } from './Controller/NumfmtModalController';
 import { NumfmtActionExtensionFactory } from './Basics/Register/NumfmtActionExtension';
+import en from './Locale/en';
+import zh from './Locale/zh';
 
 export interface INumfmtPluginConfig {}
 
@@ -22,17 +24,13 @@ export class NumfmtPlugin extends Plugin<NumfmtPluginObserve, SheetContext> {
         return new NumfmtPlugin(config);
     }
 
-    installTo(universheetInstance: UniverSheet): void {
-        this._numfmtController = new NumfmtController(this);
-        this._numfmtModalController = new NumfmtModalController(this);
-    }
-
     onMounted(context: SheetContext): void {
         install(this);
-
+        console.log('NumfmtPlugin onMounted');
+        this.getLocale().load({ en, zh });
         this._numfmtActionExtensionFactory = new NumfmtActionExtensionFactory(this);
-        this._numfmtModalController = new NumfmtModalController(this);
         this._numfmtController = new NumfmtController(this);
+        this._numfmtModalController = new NumfmtModalController(this);
         const actionRegister = this.context.getCommandManager().getActionExtensionManager().getRegister();
         actionRegister.add(this._numfmtActionExtensionFactory);
     }

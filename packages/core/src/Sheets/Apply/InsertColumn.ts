4fd4c64a5b79f89166369636e7630b9cabdb1b15
jerry <195658469@qq.com>
2023-07-24 15:25:09 +0800

    fix(code): core api

--- packages/core/src/Sheets/Apply/InsertColumn.ts
+++ packages/core/src/Sheets/Apply/InsertColumn.ts
@@ -1,26 +1,9 @@
-import { IColumnData } from '../../Types/Interfaces';
-import { ObjectArray, ObjectArrayPrimitiveType } from '../../Shared/ObjectArray';
-import { CommandModel } from '../../Command';
+import { ObjectArray } from '../../Shared/ObjectArray';
 import { IRemoveColumnAction } from '../../Types/Interfaces/IActionModel';
+import { SpreadsheetModel } from '../Model/SpreadsheetModel';
 
-/**
- * Inserts addData into columnData
- * @param columnIndex
- * @param columnData
- * @param insertData
- *
- * @internal
- */
-export function InsertColumn(columnIndex: number = 0, columnCount: number = 1, primitiveData: ObjectArrayPrimitiveType<IColumnData>) {
-    const wrapper = new ObjectArray(primitiveData);
-    wrapper.inserts(columnIndex, new ObjectArray(columnCount));
-}
-
-export function InsertColumnApply(unit: CommandModel, data: IRemoveColumnAction): void {
-    const worksheet = unit.WorkBookUnit!.getSheetBySheetId(data.sheetId);
-    const columnManager = worksheet!.getColumnManager();
-    const primitiveData = columnManager.getColumnData().toJSON();
-
-    const wrapper = new ObjectArray(primitiveData);
-    wrapper.inserts(data.columnIndex, new ObjectArray(data.columnCount));
+export function InsertColumnApply(spreadsheetModel: SpreadsheetModel, data: IRemoveColumnAction): void {
+    const worksheetModel = spreadsheetModel.worksheets[data.sheetId];
+    const columnModel = worksheetModel.column;
+    columnModel.inserts(data.columnIndex, new ObjectArray(data.columnCount));
 }

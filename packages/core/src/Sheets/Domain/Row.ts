d624bff8ca7716d820d6d251d78ab42273b6aeff
jerry <195658469@qq.com>
2023-07-24 11:19:48 +0800

    fix(code): core api

--- packages/core/src/Sheets/Domain/Row.ts
+++ packages/core/src/Sheets/Domain/Row.ts
@@ -1,1 +1,6 @@
-export class Row {}
+import { CommandManager } from '../../Command/CommandManager';
+import { SpreadsheetModel } from '../Model/SpreadsheetModel';
+
+export class Row {
+    constructor(private commandManager: CommandManager, private spreadsheetModel: SpreadsheetModel) {}
+}

6b23fd89c3a742c45415357ee0a28a0c9f871cf5
jerry <195658469@qq.com>
2023-08-16 15:19:08 +0800

    refactor(sheets-plugin-image): remove debugger

--- packages/sheets-plugin-image/src/Controller/OverGridImageController.ts
+++ packages/sheets-plugin-image/src/Controller/OverGridImageController.ts
@@ -10,7 +10,6 @@ export class OverGridImageController {
     protected _toolButton: IToolbarItemProps;
 
     constructor(@Inject(SheetContainerUIController) private readonly _sheetContainerUIController: SheetContainerUIController) {
-        debugger;
         this._toolButton = {
             name: OVER_GRID_IMAGE_PLUGIN_NAME,
             label: '图片',

92420859c9b0ef822af454a656fc57cb056f7a08
jerry <195658469@qq.com>
2023-08-28 18:09:59 +0800

    refactor(base-sheets): worksheet mutation

---
+++ packages/base-sheets/src/Commands/Mutations/set-worksheet-hidden.mutation.ts
@@ -0,0 +1,42 @@
+import { BooleanNumber, CommandType, ICurrentUniverService, IMutation } from '@univerjs/core';
+import { IAccessor } from '@wendellhu/redi';
+
+export interface ISetWorksheetHiddenMutationParams {
+    hidden: BooleanNumber;
+    workbookId: string;
+    worksheetId: string;
+}
+
+export const SetWorksheetHiddenMutationFactory = (accessor: IAccessor, params: ISetWorksheetHiddenMutationParams): ISetWorksheetHiddenMutationParams => {
+    const universheet = accessor.get(ICurrentUniverService).getCurrentUniverSheetInstance();
+    const worksheet = universheet.getWorkBook().getSheetBySheetId(params.worksheetId);
+    if (worksheet == null) {
+        throw new Error('worksheet is null error!');
+    }
+    return {
+        hidden: worksheet.isSheetHidden(),
+        workbookId: params.workbookId,
+        worksheetId: worksheet.getSheetId(),
+    };
+};
+
+export const SetWorksheetHiddenMutation: IMutation<ISetWorksheetHiddenMutationParams> = {
+    id: 'sheet.mutation.set-worksheet-hidden',
+    type: CommandType.MUTATION,
+    handler: async (accessor, params) => {
+        const universheet = accessor.get(ICurrentUniverService).getUniverSheetInstance(params.workbookId);
+
+        if (universheet == null) {
+            return false;
+        }
+
+        const worksheet = universheet.getWorkBook().getSheetBySheetId(params.worksheetId);
+
+        if (!worksheet) {
+            return false;
+        }
+
+        worksheet.getConfig().hidden = params.hidden;
+        return true;
+    },
+};

99bf4b0f7b005ed7a9804f0120179c60b0cb2602
白熱 <sonne@asaki.me>
2023-09-12 16:07:54 +0800

    chore(storybook): fix the missing css variables

--- packages/base-ui/.storybook/preview.js
+++ packages/base-ui/.storybook/preview.js
@@ -1,3 +1,9 @@
+import { defaultSkin } from '../src/Basics/CSS';
+
+const currentSkin = Object.fromEntries(Object.keys(defaultSkin).map((item) => [`--${item.replace(/([A-Z0-9])/g, '-$1').toLowerCase()}`, defaultSkin[item]]));
+
+document.documentElement.style.cssText = Object.keys(currentSkin).map((item) => `${item}: ${currentSkin[item]};`).join(' ');
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
     parameters: {

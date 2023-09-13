1caf6174a115ffcc07e52bcf5ce3b18c282a3136
Dushusir <1414556676@qq.com>
2023-09-13 21:10:28 +0800

    refactor(ui): move interface

--- packages/base-ui/src/Interfaces/CountBar.ts
+++
@@ -1,6 +0,0 @@
-import { BaseComponentProps } from '../BaseComponent';
-
-// TODO remove to component file
-export interface BaseCountBarProps extends BaseComponentProps {
-    changeRatio: (ratio: string) => void;
-}

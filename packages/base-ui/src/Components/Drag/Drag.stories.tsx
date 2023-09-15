586af91ddf57884bac7cdb7c983b0f411817d08a
Dushusir <1414556676@qq.com>
2023-09-15 15:02:36 +0800

    feat(base-ui): add Drag component

--- packages/base-ui/src/Components/Drag/Drag.stories.tsx
+++ packages/base-ui/src/Components/Drag/Drag.stories.tsx
@@ -1,22 +1,54 @@
-import type { Meta } from '@storybook/react';
+// import type { Meta } from '@storybook/react';
 
-import { Drag } from './Drag';
+// import { Drag } from './Drag';
+
+// const meta: Meta<typeof Drag> = {
+//     title: 'Components / Drag',
+//     component: Drag,
+//     parameters: {
+//         layout: 'centered',
+//     },
+//     tags: ['autodocs'],
+//     args: {
+//         children: <div>test drag</div>,
+//     },
+// };
+
+// export default meta;
+
+// export const Playground = {
+//     args: {},
+// };
+
+// Drag.stories.js
+import { Meta, StoryFn } from '@storybook/react';
+
+import { Container } from '../Container/Container'; // Introduction to components for parent containers
+import { BaseDragProps, Drag } from './Drag';
 
 const meta: Meta<typeof Drag> = {
     title: 'Components / Drag',
     component: Drag,
     parameters: {
-        layout: 'centered',
+        layout: 'left',
     },
     tags: ['autodocs'],
-    args: {
-        isDrag: true,
-        children: <div>test drag</div>,
-    },
+    decorators: [
+        (Story) => (
+            <Container style={{ height: '300px' }}>
+                <Story />
+            </Container>
+        ),
+    ],
 };
 
 export default meta;
+// 创建一个模板
+const Template: StoryFn<BaseDragProps> = (args) => <Drag {...args} />;
 
-export const Playground = {
-    args: {},
+// 添加一些默认属性
+export const Default: StoryFn<BaseDragProps> = Template.bind({});
+Default.args = {
+    // 在这里设置您的组件的默认属性
+    children: <div>test drag</div>,
 };
--- packages/base-ui/src/Components/Drag/Drag.tsx
+++ packages/base-ui/src/Components/Drag/Drag.tsx
@@ -1,142 +1,28 @@
-// interface IState {}
-// export interface BaseDragProps {
-//     isDrag: boolean;
-//     className: string;
-//     children: React.ReactNode;
-// }
-// export class Drag extends Component<BaseDragProps, IState> {
-//     root = createRef();
-//     initDragDialog() {
-//         // Drag and drop function (mainly to trigger three events: onpointerdown\onpointermove\onpointerup)
-//         const drag = this.root.current;
-//         // move to center
-//         const { top, left } = this.getPosition();
-//         drag.style.top = `${top}px`;
-//         drag.style.left = `${left}px`;
-//         const divMask = drag.querySelector(`.${styles.dragMask}`);
-//         const dragBarTop = drag.querySelector(`.${styles.dragBarTop}`);
-//         const dragBarBottom = drag.querySelector(`.${styles.dragBarBottom}`);
-//         const dragBarLeft = drag.querySelector(`.${styles.dragBarLeft}`);
-//         const dragBarRight = drag.querySelector(`.${styles.dragBarRight}`);
-//         dragBarTop.addEventListener('pointerdown', pointerDownListener);
-//         dragBarBottom.addEventListener('pointerdown', pointerDownListener);
-//         dragBarLeft.addEventListener('pointerdown', pointerDownListener);
-//         dragBarRight.addEventListener('pointerdown', pointerDownListener);
-//         // When you click on an object, you can use the drag object. Move and up are the global area, that is, the entire document is common. The document object should be used instead of the drag object (otherwise, when the drag object is used, the object can only move to the right or down )
-//         function pointerDownListener(e: PointerEvent) {
-//             e = e || window.event; // Compatible with IE browser
-//             const diffX = e.clientX - drag.offsetLeft; // The distance from the moment the mouse clicks on the object relative to the left border of the object = the distance of the position when clicked relative to the leftmost left of the browser - the distance of the left border of the object relative to the leftmost left of the browser
-//             const diffY = e.clientY - drag.offsetTop;
-//             /* Low version IE bug: When an object is dragged out of the browser’s visual window, a scroll bar will appear,
-//             The solution is to use two unique methods setCapture()\releaseCapture() in IE browser, these two methods,
-//             You can let the mouse slide outside the browser to capture the event, and our bug is when the mouse moves out of the browser,
-//             The function that the limit is exceeded becomes invalid. In this way, this problem can be solved. Note: These two methods are used in onpointerdown and onpointerup */
-//             if (typeof drag.setCapture !== 'undefined') {
-//                 drag.setCapture();
-//             }
-//             // Cover the pop-up window
-//             divMask.style.width = '100%';
-//             divMask.style.height = '100%';
-//             // Prevent the selected event from being triggered when the dragging speed is too fast
-//             document.body.style.userSelect = 'none';
-//             document.addEventListener('pointermove', pointerMoveListener);
-//             document.addEventListener('pointerup', pointerUpListener);
-//             function pointerMoveListener(e: PointerEvent) {
-//                 e = e || window.event; // Compatible with IE browser
-//                 let left = e.clientX - diffX;
-//                 let top = e.clientY - diffY;
-//                 // Control the range of dragged objects can only be in the browser window, and scroll bars are not allowed
-//                 // if (left <0) {
-//                 //     left = 0
-//                 // } else if (left> window.innerWidth-drag.offsetWidth) {
-//                 //     left = window.innerWidth-drag.offsetWidth
-//                 // }
-//                 // if (top <0) {
-//                 //     top = 0
-//                 // } else if (top> window.innerHeight-drag.offsetHeight) {
-//                 //     top = window.innerHeight-drag.offsetHeight
-//                 // }
-//                 if (left < -drag.offsetWidth + 100) {
-//                     left = -drag.offsetWidth + 100;
-//                 } else if (left > window.innerWidth - 100) {
-//                     left = window.innerWidth - 100;
-//                 }
-//                 if (top < -drag.offsetHeight + 100) {
-//                     top = -drag.offsetHeight + 100;
-//                 } else if (top > window.innerHeight - 100) {
-//                     top = window.innerHeight - 100;
-//                 }
-//                 // Regain the distance of the object when moving, and solve the phenomenon of shaking when dragging
-//                 drag.style.left = `${left}px`;
-//                 drag.style.top = `${top}px`;
-//                 drag.style.transform = '';
-//             }
-//             function pointerUpListener(e: PointerEvent) {
-//                 document.removeEventListener('pointermove', pointerMoveListener);
-//                 document.removeEventListener('pointerup', pointerUpListener);
-//                 // Prevent the mouse from looping after it pops up (that is, prevent the mouse from moving when it is put on)
-//                 // Fix low version ie bug
-//                 if (typeof drag.releaseCapture !== 'undefined') {
-//                     drag.releaseCapture();
-//                 }
-//                 // Restore the hidden mask layer
-//                 divMask.style.width = '0';
-//                 divMask.style.height = '0';
-//                 document.body.style.userSelect = '';
-//             }
-//         }
-//     }
-//     getPosition() {
-//         const w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-//         const h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-//         const rootW = this.root.current.offsetWidth;
-//         const rootH = this.root.current.offsetHeight;
-//         const top = (h - rootH) / 2;
-//         const left = (w - rootW) / 2;
-//         return {
-//             top,
-//             left,
-//         };
-//     }
-//     componentDidMount() {
-//         if (this.props.isDrag) {
-//             this.initDragDialog();
-//         }
-//     }
-//     render() {
-//         const { isDrag, className = '' } = this.props;
-//         return (
-//             <>
-//                 {isDrag ? (
-//                     <div className={styles.dragContainer}>
-//                         <div ref={this.root} className={`${styles.dragDialog} ${className}`}>
-//                             {this.props.children}
-//                             <div className={styles.dragBarLeft}></div>
-//                             <div className={styles.dragBarRight}></div>
-//                             <div className={styles.dragBarTop}></div>
-//                             <div className={styles.dragBarBottom}></div>
-//                             <div className={styles.dragMask}></div>
-//                         </div>
-//                     </div>
-//                 ) : (
-//                     this.props.children
-//                 )}
-//             </>
-//         );
-//     }
-// }
 import { IKeyValue } from '@univerjs/core';
 import React, { useEffect, useRef } from 'react';
 
 import styles from './index.module.less';
 
-interface BaseDragProps {
-    isDrag: boolean;
-    className: string;
+export interface BaseDragProps {
     children: React.ReactNode;
+
+    /**
+     * Semantic DOM class
+     * @default ''
+     * */
+    className?: string;
+
+    /**
+     * Whether to enable drag
+     * @default true
+     */
+    isDrag?: boolean;
 }
 
-export function Drag({ isDrag, className = '', children }: BaseDragProps) {
+/**
+ * Drag Component
+ */
+export function Drag({ isDrag = true, className = '', children }: BaseDragProps) {
     const rootRef = useRef<HTMLDivElement | null>(null);
 
     const getPosition = () => {
@@ -169,6 +55,7 @@ export function Drag({ isDrag, className = '', children }: BaseDragProps) {
             e = e || window.event;
             const diffX = e.clientX - drag.offsetLeft;
             const diffY = e.clientY - drag.offsetTop;
+            console.info('diffX', e, drag.offsetLeft, diffX);
 
             if (typeof (drag as IKeyValue).setCapture !== 'undefined') {
                 (drag as IKeyValue).setCapture();
@@ -232,8 +119,8 @@ export function Drag({ isDrag, className = '', children }: BaseDragProps) {
     return (
         <>
             {isDrag ? (
-                <div className={styles.dragContainer}>
-                    <div ref={rootRef} className={`${styles.dragDialog} ${className}`}>
+                <div className={styles.dragContainer} ref={rootRef}>
+                    <div className={`${styles.dragDialog} ${className}`}>
                         {children}
 
                         <div className={styles.dragBarLeft}></div>

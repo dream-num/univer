a4bb699df3ebb6917f7f331d088ed3151810a91d
白熱 <jikkai@users.noreply.github.com>
2023-09-16 16:38:04 +0800

    refactor: remove locale files from base-render (#157)

--- packages/base-render/src/Locale/en.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    renderFont: {
-        defaultFont: 'Times New Roman',
-        fontList: ['Times New Roman', 'Arial', 'Tahoma', 'Verdana'],
-        defaultFontSize: 14,
-        unit: 'pt',
-    },
-};
--- packages/base-render/src/Locale/zh.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    renderFont: {
-        defaultFont: 'Times New Roman',
-        fontList: ['Times New Roman', 'Arial', 'Tahoma', 'Verdana', '微软雅黑', '宋体', '黑体', '楷体', '仿宋', '新宋体', '华文新魏', '华文行楷', '华文隶书'],
-        defaultFontSize: 14,
-        unit: 'pt',
-    },
-};

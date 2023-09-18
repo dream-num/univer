67fc501a14245ab141c79ed27b121137dcef6fc2
Dushusir <1414556676@qq.com>
2023-09-18 10:09:48 +0800

    refactor(base-ui): add basic test for InputNumber component

---
+++ packages/base-ui/src/Components/InputNumber/test/index.test.tsx
@@ -0,0 +1,71 @@
+import { fireEvent, render } from '@testing-library/react';
+
+import { InputNumber } from '../InputNumber';
+
+describe('input number', () => {
+    test('render', () => {
+        const { container } = render(<InputNumber value={0} placeholder="haha" />);
+        expect(container);
+        // test the value of the placeholder
+        expect(container.querySelector('input')?.placeholder).toBe('haha');
+    });
+    test('min max', () => {
+        const { container } = render(<InputNumber value={0} placeholder="haha" min={0} max={5} />);
+        const input = container.querySelector('input');
+        const buttonAddition = container?.querySelectorAll('button')[0];
+        const buttonSubtraction = container?.querySelectorAll('button')[1];
+
+        // buttonAddition clicked 10 times
+        for (let i = 0; i < 10; i++) {
+            fireEvent.click(buttonAddition!);
+        }
+
+        // Test the value of the input
+        expect(input!.value).toBe('5');
+
+        // buttonSubtraction click 10 times
+        for (let i = 0; i < 10; i++) {
+            fireEvent.click(buttonSubtraction!);
+        }
+
+        // Test the value of the input
+        expect(input!.value).toBe('0');
+    });
+
+    test('input/change event', () => {
+        const { container } = render(<InputNumber value={0} placeholder="haha" />);
+        const input = container.querySelector('input');
+        fireEvent.input(input!, { target: { value: 1 } });
+        expect(input!.value).toBe('1');
+        fireEvent.change(input!, { target: { value: 2 } });
+        expect(input!.value).toBe('2');
+    });
+
+    test('blur event', () => {
+        let a = 1;
+        const { container } = render(<InputNumber value={0} onBlur={() => a++} disabled placeholder="haha" />);
+        fireEvent.click(document.querySelector('input')!);
+        fireEvent.blur(document.querySelector('input')!);
+        expect(a).toEqual(2);
+    });
+    test('press enter event', () => {
+        let a = 0;
+        const { container } = render(
+            <InputNumber
+                value={1}
+                placeholder="haha"
+                onPressEnter={(v) => {
+                    a = v;
+                }}
+            />
+        );
+        const input = container.querySelector('input');
+        fireEvent.keyUp(input!, {
+            key: 'Enter',
+            code: 'Enter',
+            keyCode: 13,
+            charCode: 13,
+        });
+        expect(a).toEqual(1);
+    });
+});

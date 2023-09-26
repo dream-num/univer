5c3c8bf67d5bd1250c6c1ffdfeefe891512ac8bf
tonytonychopper123 <40351140+tonytonychopper123@users.noreply.github.com>
2023-09-26 14:27:13 +0800

    fix(base-sheets): ui command

--- packages/base-sheets/src/commands/commands/__tests__/set-style-command.spec.ts
+++ packages/base-sheets/src/commands/commands/__tests__/set-style-command.spec.ts
@@ -2,13 +2,18 @@ import {
     BooleanNumber,
     FontItalic,
     FontWeight,
+    HorizontalAlign,
     ICommandService,
     ICurrentUniverService,
     ITextDecoration,
+    ITextRotation,
+    Nullable,
     RedoCommand,
     SELECTION_TYPE,
     UndoCommand,
     Univer,
+    VerticalAlign,
+    WrapStrategy,
 } from '@univerjs/core';
 import { Injector } from '@wendellhu/redi';
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
@@ -16,11 +21,19 @@ import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 import { NORMAL_SELECTION_PLUGIN_NAME, SelectionManagerService } from '../../../services/selection-manager.service';
 import { SetRangeStyleMutation } from '../../mutations/set-range-styles.mutation';
 import {
+    SetBackgroundColorCommand,
     SetBoldCommand,
+    SetFontFamilyCommand,
+    SetFontSizeCommand,
+    SetHorizontalTextAlignCommand,
     SetItalicCommand,
     SetStrikeThroughCommand,
     SetStyleCommand,
+    SetTextColorCommand,
+    SetTextRotationCommand,
+    SetTextWrapCommand,
     SetUnderlineCommand,
+    SetVerticalTextAlignCommand,
 } from '../set-style.command';
 import { createCommandTestBed } from './create-command-test-bed';
 
@@ -40,6 +53,14 @@ describe('Test style commands', () => {
         commandService.registerCommand(SetItalicCommand);
         commandService.registerCommand(SetUnderlineCommand);
         commandService.registerCommand(SetStrikeThroughCommand);
+        commandService.registerCommand(SetFontSizeCommand);
+        commandService.registerCommand(SetFontFamilyCommand);
+        commandService.registerCommand(SetTextColorCommand);
+        commandService.registerCommand(SetBackgroundColorCommand);
+        commandService.registerCommand(SetVerticalTextAlignCommand);
+        commandService.registerCommand(SetHorizontalTextAlignCommand);
+        commandService.registerCommand(SetTextWrapCommand);
+        commandService.registerCommand(SetTextRotationCommand);
         commandService.registerCommand(SetStyleCommand);
         commandService.registerCommand(SetRangeStyleMutation);
 
@@ -220,6 +241,348 @@ describe('Test style commands', () => {
         describe('fault situations', () => {
             it('will not apply when there is no selected ranges', async () => {
                 const result = await commandService.executeCommand(SetStrikeThroughCommand.id);
+                expect(result).toBeFalsy();
+            });
+        });
+    });
+
+    describe('font size', () => {
+        describe('correct situations', () => {
+            it('will change font size when there is a selected range', async () => {
+                const selectionManager = get(SelectionManagerService);
+                selectionManager.setCurrentSelection({
+                    pluginName: NORMAL_SELECTION_PLUGIN_NAME,
+                    unitId: 'test',
+                    sheetId: 'sheet1',
+                });
+                selectionManager.add([
+                    {
+                        rangeData: { startRow: 0, startColumn: 0, endColumn: 0, endRow: 0 },
+                        cellRange: null,
+                        selectionType: SELECTION_TYPE.NORMAL,
+                        style: null,
+                    },
+                ]);
+
+                function getFontSize(): number | undefined {
+                    return get(ICurrentUniverService)
+                        .getUniverSheetInstance('test')
+                        ?.getWorkBook()
+                        .getSheetBySheetId('sheet1')
+                        ?.getRange(0, 0, 0, 0)
+                        .getFontSize();
+                }
+
+                expect(await commandService.executeCommand(SetFontSizeCommand.id, { value: 18 })).toBeTruthy();
+                expect(getFontSize()).toBe(18);
+            });
+        });
+
+        describe('fault situations', () => {
+            it('will not apply when there is no selected ranges', async () => {
+                const result = await commandService.executeCommand(SetFontSizeCommand.id);
+                expect(result).toBeFalsy();
+            });
+        });
+    });
+
+    describe('font family', () => {
+        describe('correct situations', () => {
+            it('will change font family when there is a selected range', async () => {
+                const selectionManager = get(SelectionManagerService);
+                selectionManager.setCurrentSelection({
+                    pluginName: NORMAL_SELECTION_PLUGIN_NAME,
+                    unitId: 'test',
+                    sheetId: 'sheet1',
+                });
+                selectionManager.add([
+                    {
+                        rangeData: { startRow: 0, startColumn: 0, endColumn: 0, endRow: 0 },
+                        cellRange: null,
+                        selectionType: SELECTION_TYPE.NORMAL,
+                        style: null,
+                    },
+                ]);
+
+                function getFontFamily(): string | undefined {
+                    return get(ICurrentUniverService)
+                        .getUniverSheetInstance('test')
+                        ?.getWorkBook()
+                        .getSheetBySheetId('sheet1')
+                        ?.getRange(0, 0, 0, 0)
+                        .getFontFamily();
+                }
+
+                expect(await commandService.executeCommand(SetFontFamilyCommand.id, { value: 'Arial' })).toBeTruthy();
+                expect(getFontFamily()).toBe('Arial');
+            });
+        });
+
+        describe('fault situations', () => {
+            it('will not apply when there is no selected ranges', async () => {
+                const result = await commandService.executeCommand(SetFontFamilyCommand.id);
+                expect(result).toBeFalsy();
+            });
+        });
+    });
+
+    describe('font color', () => {
+        describe('correct situations', () => {
+            it('will change font color when there is a selected range', async () => {
+                const selectionManager = get(SelectionManagerService);
+                selectionManager.setCurrentSelection({
+                    pluginName: NORMAL_SELECTION_PLUGIN_NAME,
+                    unitId: 'test',
+                    sheetId: 'sheet1',
+                });
+                selectionManager.add([
+                    {
+                        rangeData: { startRow: 0, startColumn: 0, endColumn: 0, endRow: 0 },
+                        cellRange: null,
+                        selectionType: SELECTION_TYPE.NORMAL,
+                        style: null,
+                    },
+                ]);
+
+                function getFontColor(): Nullable<string> | undefined {
+                    return get(ICurrentUniverService)
+                        .getUniverSheetInstance('test')
+                        ?.getWorkBook()
+                        .getSheetBySheetId('sheet1')
+                        ?.getRange(0, 0, 0, 0)
+                        .getTextStyle()?.cl?.rgb;
+                }
+
+                expect(await commandService.executeCommand(SetTextColorCommand.id, { value: '#abcdef' })).toBeTruthy();
+                expect(getFontColor()).toBe('#abcdef');
+            });
+        });
+
+        describe('fault situations', () => {
+            it('will not apply when there is no selected ranges', async () => {
+                const result = await commandService.executeCommand(SetTextColorCommand.id);
+                expect(result).toBeFalsy();
+            });
+        });
+    });
+
+    describe('background color', () => {
+        describe('correct situations', () => {
+            it('will change background color when there is a selected range', async () => {
+                const selectionManager = get(SelectionManagerService);
+                selectionManager.setCurrentSelection({
+                    pluginName: NORMAL_SELECTION_PLUGIN_NAME,
+                    unitId: 'test',
+                    sheetId: 'sheet1',
+                });
+                selectionManager.add([
+                    {
+                        rangeData: { startRow: 0, startColumn: 0, endColumn: 0, endRow: 0 },
+                        cellRange: null,
+                        selectionType: SELECTION_TYPE.NORMAL,
+                        style: null,
+                    },
+                ]);
+
+                function getBackgroundColor(): string | undefined {
+                    return get(ICurrentUniverService)
+                        .getUniverSheetInstance('test')
+                        ?.getWorkBook()
+                        .getSheetBySheetId('sheet1')
+                        ?.getRange(0, 0, 0, 0)
+                        .getBackground();
+                }
+
+                expect(
+                    await commandService.executeCommand(SetBackgroundColorCommand.id, { value: '#abcdef' })
+                ).toBeTruthy();
+                expect(getBackgroundColor()).toBe('#abcdef');
+            });
+        });
+
+        describe('fault situations', () => {
+            it('will not apply when there is no selected ranges', async () => {
+                const result = await commandService.executeCommand(SetBackgroundColorCommand.id);
+                expect(result).toBeFalsy();
+            });
+        });
+    });
+
+    describe('vertical text align', () => {
+        describe('correct situations', () => {
+            it('will change vertical text align when there is a selected range', async () => {
+                const selectionManager = get(SelectionManagerService);
+                selectionManager.setCurrentSelection({
+                    pluginName: NORMAL_SELECTION_PLUGIN_NAME,
+                    unitId: 'test',
+                    sheetId: 'sheet1',
+                });
+                selectionManager.add([
+                    {
+                        rangeData: { startRow: 0, startColumn: 0, endColumn: 0, endRow: 0 },
+                        cellRange: null,
+                        selectionType: SELECTION_TYPE.NORMAL,
+                        style: null,
+                    },
+                ]);
+
+                function getVerticalAlignment(): VerticalAlign | undefined {
+                    return get(ICurrentUniverService)
+                        .getUniverSheetInstance('test')
+                        ?.getWorkBook()
+                        .getSheetBySheetId('sheet1')
+                        ?.getRange(0, 0, 0, 0)
+                        .getVerticalAlignment();
+                }
+
+                expect(
+                    await commandService.executeCommand(SetVerticalTextAlignCommand.id, { value: VerticalAlign.TOP })
+                ).toBeTruthy();
+                expect(getVerticalAlignment()).toBe(VerticalAlign.TOP);
+            });
+        });
+
+        describe('fault situations', () => {
+            it('will not apply when there is no selected ranges', async () => {
+                const result = await commandService.executeCommand(SetVerticalTextAlignCommand.id);
+                expect(result).toBeFalsy();
+            });
+        });
+    });
+
+    describe('horizontal text align', () => {
+        describe('correct situations', () => {
+            it('will change horizontal text align when there is a selected range', async () => {
+                const selectionManager = get(SelectionManagerService);
+                selectionManager.setCurrentSelection({
+                    pluginName: NORMAL_SELECTION_PLUGIN_NAME,
+                    unitId: 'test',
+                    sheetId: 'sheet1',
+                });
+                selectionManager.add([
+                    {
+                        rangeData: { startRow: 0, startColumn: 0, endColumn: 0, endRow: 0 },
+                        cellRange: null,
+                        selectionType: SELECTION_TYPE.NORMAL,
+                        style: null,
+                    },
+                ]);
+
+                function getHorizontalAlignment(): HorizontalAlign | undefined {
+                    return get(ICurrentUniverService)
+                        .getUniverSheetInstance('test')
+                        ?.getWorkBook()
+                        .getSheetBySheetId('sheet1')
+                        ?.getRange(0, 0, 0, 0)
+                        .getHorizontalAlignment();
+                }
+
+                expect(
+                    await commandService.executeCommand(SetHorizontalTextAlignCommand.id, {
+                        value: HorizontalAlign.RIGHT,
+                    })
+                ).toBeTruthy();
+                expect(getHorizontalAlignment()).toBe(HorizontalAlign.RIGHT);
+            });
+        });
+
+        describe('fault situations', () => {
+            it('will not apply when there is no selected ranges', async () => {
+                const result = await commandService.executeCommand(SetHorizontalTextAlignCommand.id);
+                expect(result).toBeFalsy();
+            });
+        });
+    });
+
+    describe('text wrap', () => {
+        describe('correct situations', () => {
+            it('will change text wrap when there is a selected range', async () => {
+                const selectionManager = get(SelectionManagerService);
+                selectionManager.setCurrentSelection({
+                    pluginName: NORMAL_SELECTION_PLUGIN_NAME,
+                    unitId: 'test',
+                    sheetId: 'sheet1',
+                });
+                selectionManager.add([
+                    {
+                        rangeData: { startRow: 0, startColumn: 0, endColumn: 0, endRow: 0 },
+                        cellRange: null,
+                        selectionType: SELECTION_TYPE.NORMAL,
+                        style: null,
+                    },
+                ]);
+
+                function getTextWrap(): BooleanNumber | undefined {
+                    return get(ICurrentUniverService)
+                        .getUniverSheetInstance('test')
+                        ?.getWorkBook()
+                        .getSheetBySheetId('sheet1')
+                        ?.getRange(0, 0, 0, 0)
+                        .getWrap();
+                }
+
+                expect(
+                    await commandService.executeCommand(SetTextWrapCommand.id, {
+                        value: WrapStrategy.WRAP,
+                    })
+                ).toBeTruthy();
+                expect(getTextWrap()).toBe(WrapStrategy.WRAP);
+            });
+        });
+
+        describe('fault situations', () => {
+            it('will not apply when there is no selected ranges', async () => {
+                const result = await commandService.executeCommand(SetTextWrapCommand.id);
+                expect(result).toBeFalsy();
+            });
+        });
+    });
+
+    describe('text rotation', () => {
+        describe('correct situations', () => {
+            it('will change text rotation when there is a selected range', async () => {
+                const selectionManager = get(SelectionManagerService);
+                selectionManager.setCurrentSelection({
+                    pluginName: NORMAL_SELECTION_PLUGIN_NAME,
+                    unitId: 'test',
+                    sheetId: 'sheet1',
+                });
+                selectionManager.add([
+                    {
+                        rangeData: { startRow: 0, startColumn: 0, endColumn: 0, endRow: 0 },
+                        cellRange: null,
+                        selectionType: SELECTION_TYPE.NORMAL,
+                        style: null,
+                    },
+                ]);
+
+                function getTextRotation(): ITextRotation | undefined {
+                    return get(ICurrentUniverService)
+                        .getUniverSheetInstance('test')
+                        ?.getWorkBook()
+                        .getSheetBySheetId('sheet1')
+                        ?.getRange(0, 0, 0, 0)
+                        .getTextRotation();
+                }
+
+                expect(
+                    await commandService.executeCommand(SetTextRotationCommand.id, {
+                        value: 90,
+                    })
+                ).toBeTruthy();
+                expect(getTextRotation()?.a).toBe(90);
+                expect(
+                    await commandService.executeCommand(SetTextRotationCommand.id, {
+                        value: 'v',
+                    })
+                ).toBeTruthy();
+                expect(getTextRotation()?.v).toBe(BooleanNumber.TRUE);
+            });
+        });
+
+        describe('fault situations', () => {
+            it('will not apply when there is no selected ranges', async () => {
+                const result = await commandService.executeCommand(SetTextRotationCommand.id);
                 expect(result).toBeFalsy();
             });
         });

32b3f0dcf92a9f0b8f1af76ca2911386f3a43c64
Dushusir <1414556676@qq.com>
2023-10-13 20:57:21 +0800

    feat(base-ui): input,input number component (#282)

--- packages/base-ui/src/Components/Icon/Text/index.tsx
+++ packages/base-ui/src/Components/Icon/Text/index.tsx
@@ -1,18 +1,18 @@
 import {
     Autowrap24,
-    LeftRotation45Degrees24,
-    LeftRotation90Degrees24,
+    LeftRotationFortyFiveDegrees24,
+    LeftRotationNinetyDegrees24,
     Overflow24,
-    RightRotation45Degrees24,
-    RightRotation90Degrees24,
+    RightRotationFortyFiveDegrees24,
+    RightRotationNinetyDegrees24,
     TextDown24,
-    TextHorizontallyCenter24,
+    TextHorizontallyCentered24,
     TextLeft24,
     TextRight24,
     TextRotation24,
     TextUp24,
     TextVertical24,
-    TextVerticallyCenter24,
+    TextVerticallyCentered24,
     Truncation24,
 } from '@univerjs/icons';
 
@@ -20,22 +20,22 @@ import { BaseIconProps } from '../AddIcon';
 
 export const LeftAlignIcon = (props: BaseIconProps) => <TextLeft24 />;
 
-export const CenterAlignIcon = (props: BaseIconProps) => <TextHorizontallyCenter24 />;
+export const CenterAlignIcon = (props: BaseIconProps) => <TextHorizontallyCentered24 />;
 
 export const RightAlignIcon = (props: BaseIconProps) => <TextRight24 />;
 
 export const TopVerticalIcon = (props: BaseIconProps) => <TextUp24 />;
 
-export const CenterVerticalIcon = (props: BaseIconProps) => <TextVerticallyCenter24 />;
+export const CenterVerticalIcon = (props: BaseIconProps) => <TextVerticallyCentered24 />;
 
 export const BottomVerticalIcon = (props: BaseIconProps) => <TextDown24 />;
 
 export const TextRotateIcon = (props: BaseIconProps) => <TextRotation24 />;
-export const TextRotateAngleUpIcon = (props: BaseIconProps) => <LeftRotation45Degrees24 />;
-export const TextRotateAngleDownIcon = (props: BaseIconProps) => <RightRotation45Degrees24 />;
+export const TextRotateAngleUpIcon = (props: BaseIconProps) => <LeftRotationFortyFiveDegrees24 />;
+export const TextRotateAngleDownIcon = (props: BaseIconProps) => <RightRotationFortyFiveDegrees24 />;
 export const TextRotateVerticalIcon = (props: BaseIconProps) => <TextVertical24 />;
-export const TextRotateRotationUpIcon = (props: BaseIconProps) => <LeftRotation90Degrees24 />;
-export const TextRotateRotationDownIcon = (props: BaseIconProps) => <RightRotation90Degrees24 />;
+export const TextRotateRotationUpIcon = (props: BaseIconProps) => <LeftRotationNinetyDegrees24 />;
+export const TextRotateRotationDownIcon = (props: BaseIconProps) => <RightRotationNinetyDegrees24 />;
 
 export const BrIcon = (props: BaseIconProps) => <Autowrap24 />;
 

efe5781f397934e0515fdc0cd9e9b4f6cd787b91
wzhudev <huwenzhao@univer.ai>
2023-10-16 13:59:28 +0800

    fix: fix clear selection value command

--- packages/ui-plugin-sheets/src/controller/shortcuts/value.shortcut.ts
+++ packages/ui-plugin-sheets/src/controller/shortcuts/value.shortcut.ts
@@ -5,8 +5,7 @@ import { whenEditorNotActivated } from './utils';
 
 export const ClearSelectionValueShortcutItem: IShortcutItem = {
     id: ClearSelectionContentCommand.id,
-    preconditions: (contextService) =>
-        // when focusing on any other input tag do not trigger this shortcut
-        whenEditorNotActivated(contextService) && document.activeElement?.tagName === 'canvas',
+    // when focusing on any other input tag do not trigger this shortcut
+    preconditions: (contextService) => whenEditorNotActivated(contextService),
     binding: KeyCode.BACKSPACE,
 };

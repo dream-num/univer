da1b9875367d24da0a2893f18de18a7cb227f421
白熱 <sonne@asaki.me>
2023-10-30 18:37:49 +0800

    feat(design): add `Radio`

---
+++ packages/design/src/components/radio/Radio.tsx
@@ -0,0 +1,74 @@
+import clsx from 'clsx';
+import { useRef } from 'react';
+
+import styles from './index.module.less';
+
+export interface IRadioIProps {
+    children?: React.ReactNode;
+
+    /**
+     * Used for setting the currently selected value
+     * @default false
+     */
+    checked?: boolean;
+
+    /**
+     * Used for setting the currently selected value
+     */
+    value?: string | number | boolean;
+
+    /**
+     * Specifies whether the radio is disabled
+     * @default false
+     */
+    disabled?: boolean;
+
+    /**
+     * Set the handler to handle `click` event
+     */
+    onChange?: (value: string | number | boolean) => void;
+}
+
+/**
+ * Radio Component
+ */
+export function Radio(props: IRadioIProps) {
+    const { children, checked, value, disabled = false, onChange } = props;
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        e.stopPropagation();
+
+        if (!onChange || disabled) return;
+
+        if (typeof value !== 'undefined') {
+            onChange && onChange(value);
+        } else {
+            const checked = inputRef?.current?.checked!;
+            onChange && onChange(checked);
+        }
+    }
+
+    const _className = clsx(styles.radio, {
+        [styles.radioDisabled]: disabled,
+    });
+
+    return (
+        <label className={_className}>
+            <span className={styles.radioTarget}>
+                <input
+                    ref={inputRef}
+                    className={styles.radioTargetInput}
+                    type="radio"
+                    checked={checked}
+                    disabled={disabled}
+                    onChange={handleChange}
+                />
+                <span className={styles.radioTargetInner} />
+            </span>
+
+            <span>{children}</span>
+        </label>
+    );
+}
---
+++ packages/design/src/components/radio/__tests__/index.spec.tsx
@@ -0,0 +1,18 @@
+import { fireEvent, render } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+
+import { Radio } from '../Radio';
+
+describe('Radio', () => {
+    const component = <Radio value="0">text</Radio>;
+
+    test('click Radio', async () => {
+        const { container } = render(component);
+
+        fireEvent.click(container.querySelector('input')!);
+
+        const $input = container.querySelector('input');
+
+        expect($input?.checked).toBe(true);
+    });
+});

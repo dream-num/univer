9b02303b6cf1b40350d48efadfba61476e9628bc
wzhudev <huwenzhao@univer.ai>
2023-11-01 15:14:20 +0800

    fix: fix type error

--- packages/ui-plugin-sheets/src/controller/navigation/navigation.controller.ts
+++ packages/ui-plugin-sheets/src/controller/navigation/navigation.controller.ts
@@ -1,6 +1,8 @@
-import { IMoveSelectionCommandParams, MoveSelectionCommand, ScrollController } from '@univerjs/base-sheets';
+import { ScrollController } from '@univerjs/base-sheets';
 import { Disposable, ICommandService, LifecycleStages, OnLifecycle } from '@univerjs/core';
 import { Inject } from '@wendellhu/redi';
+
+import { IMoveSelectionCommandParams, MoveSelectionCommand } from '../../commands/commands/set-selection.command';
 
 const SHEET_NAVIGATION_COMMANDS = [MoveSelectionCommand.id];
 

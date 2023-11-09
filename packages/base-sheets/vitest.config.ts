c6320454e13d21f759f0239347fc31218ae09cd8
白熱 <sonne@asaki.me>
2023-11-09 12:35:18 +0800

    chore: using istanbul as a coverage provider

--- packages/base-sheets/vitest.config.ts
+++ packages/base-sheets/vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         environment: 'happy-dom',
+        coverage: {
+            provider: 'istanbul',
+        },
     },
 });

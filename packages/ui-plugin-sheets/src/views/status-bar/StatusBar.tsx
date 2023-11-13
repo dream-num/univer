8d2996526cdee52fd41548453456b02468f8a5ed
YuHong <csuzyh@gmail.com>
2023-11-13 23:19:11 +0800

    feat: status bar ui & flexible layout

--- packages/ui-plugin-sheets/src/views/status-bar/StatusBar.tsx
+++ packages/ui-plugin-sheets/src/views/status-bar/StatusBar.tsx
@@ -1,36 +1,42 @@
-import { FUNCTION_NAMES } from '@univerjs/base-formula-engine';
+import { LocaleService } from '@univerjs/core';
 import { Checkbox, Dropdown } from '@univerjs/design';
 import { MoreSingle } from '@univerjs/icons';
 import { useDependency } from '@wendellhu/redi/react-bindings';
+import clsx from 'clsx';
 import { useEffect, useState } from 'react';
 
 import { IStatusBarService } from '../../services/status-bar.service';
+import { CopyableStatisticItem, formatNumber, functionDisplayNames, IStatisticItem } from './CopyableStatisticItem';
 import styles from './index.module.less';
 
-export interface statisticItem {
-    name: FUNCTION_NAMES;
-    value: number;
-    show: boolean;
-}
+const SINGLE_MODE_WIDTH = 800;
 
 export const StatusBar = () => {
+    const [isSingle, setIsSingle] = useState(window.innerWidth < SINGLE_MODE_WIDTH);
+    const [show, setShow] = useState(true);
+
+    const localeService = useDependency(LocaleService);
     const statusBarService = useDependency(IStatusBarService);
     const items = statusBarService.getFunctions().map((item, index) => ({
         name: item,
         value: 0,
-        show: index < 3,
+        show: isSingle ? index < 1 : index < 3,
     }));
-    const [statistics, setStatistics] = useState<statisticItem[]>(items);
-    const onChange = (item: statisticItem) => {
+    const [statistics, setStatistics] = useState<IStatisticItem[]>(items);
+    const showList = statistics.filter((item) => item.show);
+
+    const onChange = (item: IStatisticItem) => {
         const newStatistics = statistics.map((stat) => {
             if (stat.name === item.name) {
                 stat.show = !stat.show;
+            } else if (isSingle) {
+                stat.show = false;
             }
             return stat;
         });
         setStatistics(newStatistics);
     };
-    const [show, setShow] = useState(true);
+
     useEffect(() => {
         const subscription = statusBarService.state$.subscribe((item) => {
             if (!item) {
@@ -48,43 +54,87 @@ export const StatusBar = () => {
         return () => {
             subscription.unsubscribe();
         };
-    }, [statusBarService]);
+    }, [statusBarService, statistics]);
+
+    useEffect(() => {
+        const handleResize = () => {
+            const newSingleState = window.innerWidth < SINGLE_MODE_WIDTH;
+            if (isSingle !== newSingleState) {
+                if (newSingleState) {
+                    // keep the items hide except the first show item
+                    const firstIndex = statistics.findIndex((item) => item.show);
+                    const newStatistics = statistics.map((stat, index) => {
+                        if (index === firstIndex) {
+                            stat.show = true;
+                        } else {
+                            stat.show = false;
+                        }
+                        return stat;
+                    });
+                    setStatistics(newStatistics);
+                }
+                setIsSingle(newSingleState);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [isSingle, statistics]);
+
     return (
         show && (
-            <div className={styles.statusBar}>
-                <div className={styles.statisticList}>
-                    {statistics
-                        .filter((item) => item.show)
-                        .map((item) => (
-                            <div key={item.name} className={styles.statisticItem}>
-                                {item.name} = {item.value}
+            <div
+                className={clsx(styles.statusBar, {
+                    [styles.singleMode]: isSingle,
+                })}
+            >
+                <div
+                    className={clsx(styles.statisticList, {
+                        [styles.statisticListTwoRow]: showList.length > 3,
+                    })}
+                >
+                    {showList.length > 3 ? (
+                        <>
+                            <div className={styles.statisticListRow}>
+                                {showList.slice(0, 3).map((item) => (
+                                    <CopyableStatisticItem {...item} />
+                                ))}
                             </div>
-                        ))}
+                            <div className={styles.statisticListRow}>
+                                {showList.slice(3).map((item) => (
+                                    <CopyableStatisticItem {...item} />
+                                ))}
+                            </div>
+                        </>
+                    ) : (
+                        showList.map((item) => <CopyableStatisticItem {...item} />)
+                    )}
                 </div>
                 <Dropdown
                     placement="topRight"
                     overlay={
                         <div className={styles.statisticPicker}>
                             {statistics?.map((item) => (
-                                <a
-                                    key={item.name}
-                                    className={`${styles.sliderShortcut}
-                                    ${item.show ? styles.statisticActive : ''}`}
-                                >
+                                <a key={item.name} className={styles.statisticPickerItem}>
                                     <Checkbox
                                         value={item.name}
                                         checked={item.show}
                                         onChange={() => onChange && onChange(item)}
                                     ></Checkbox>
-                                    <span>{`${item.name} = ${item.value}`}</span>
+                                    <span>{`${localeService.t(
+                                        functionDisplayNames?.[item.name] || item.name
+                                    )}: ${formatNumber(item.value)}`}</span>
                                 </a>
                             ))}
                         </div>
                     }
                 >
-                    <a className={styles.statisticMore}>
+                    <div className={styles.statisticMore}>
                         <MoreSingle />
-                    </a>
+                    </div>
                 </Dropdown>
             </div>
         )

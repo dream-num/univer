6b1148eb3bf18da0f823908feb30c686a654a8c3
Wenzhao Hu <huwenzhao@univer.ai>
2023-11-20 22:16:08 +0800

    feat: run formula calculation in worker (#492)
    * feat: run formula calculation in worker
    
    * refactor: base-formula-engine plugin
    
    * fix: fix type export

--- examples/univer-sheet-ts/src/worker.ts
+++ examples/univer-sheet-ts/src/worker.ts
@@ -1,8 +1,10 @@
 // The entry of web worker scripts
 
+import { BaseFormulaEnginePlugin } from '@univerjs/base-formula-engine';
 import { SheetPlugin } from '@univerjs/base-sheets';
 import { LocaleType, Univer } from '@univerjs/core';
 import { UniverRPCWorkerThreadPlugin } from '@univerjs/rpc';
+import { FormulaPlugin } from '@univerjs/sheets-plugin-formula';
 
 // Univer web worker is also a univer application.
 const univer = new Univer({
@@ -10,7 +12,9 @@ const univer = new Univer({
 });
 
 univer.registerPlugin(SheetPlugin);
+univer.registerPlugin(BaseFormulaEnginePlugin);
 univer.registerPlugin(UniverRPCWorkerThreadPlugin);
+univer.registerPlugin(FormulaPlugin);
 
 declare let self: WorkerGlobalScope & typeof globalThis & { univer: Univer };
 self.univer = univer;
--- packages/rpc/src/controllers/data-sync/data-sync-replica.controller.ts
+++ packages/rpc/src/controllers/data-sync/data-sync-replica.controller.ts
@@ -1,6 +1,14 @@
-import { Disposable, IUniverInstanceService, LifecycleStages, OnLifecycle } from '@univerjs/core';
+import {
+    CommandType,
+    Disposable,
+    ICommandService,
+    IMutationInfo,
+    IUniverInstanceService,
+    LifecycleStages,
+    OnLifecycle,
+} from '@univerjs/core';
 
-import { IRemoteInstanceService } from '../../services/remote-instance/remote-instance.service';
+import { IRemoteSyncMutationOptions, IRemoteSyncService } from '../../services/remote-instance/remote-instance.service';
 
 /**
  * This controller is responsible for syncing data from the worker thread to
@@ -9,9 +17,25 @@ import { IRemoteInstanceService } from '../../services/remote-instance/remote-in
 @OnLifecycle(LifecycleStages.Starting, DataSyncReplicaController)
 export class DataSyncReplicaController extends Disposable {
     constructor(
-        @IRemoteInstanceService private readonly: IRemoteInstanceService,
+        @ICommandService private readonly _commandService: ICommandService,
+        @IRemoteSyncService private readonly _remoteInstanceService: IRemoteSyncService,
         @IUniverInstanceService private readonly _univerInstanceService: IUniverInstanceService
     ) {
         super();
+
+        this._init();
+    }
+
+    private _init(): void {
+        this.disposeWithMe(
+            // Mutations executed on the main thread should be synced to the worker thread.
+            this._commandService.onCommandExecuted((commandInfo, options) => {
+                if (commandInfo.type === CommandType.MUTATION && !(options as IRemoteSyncMutationOptions)?.fromSync) {
+                    this._remoteInstanceService.syncMutation({
+                        mutationInfo: commandInfo as IMutationInfo,
+                    });
+                }
+            })
+        );
     }
 }

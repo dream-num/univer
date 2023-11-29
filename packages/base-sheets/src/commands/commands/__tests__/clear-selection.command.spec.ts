59786828490e241b1258421d4832712342fe2d63
gggpound <gpoundLiu@gmail.com>
2023-11-29 14:22:05 +0800

    fix(sheet): a1 should be type 1,string rather than force string

--- packages/base-sheets/src/commands/commands/__tests__/clear-selection.command.spec.ts
+++ packages/base-sheets/src/commands/commands/__tests__/clear-selection.command.spec.ts
@@ -151,7 +151,7 @@ describe('Test clear selection content commands', () => {
                 expect(await commandService.executeCommand(ClearSelectionFormatCommand.id)).toBeTruthy();
                 expect(getValue()).toStrictEqual({
                     v: 'A1',
-                    t: 4,
+                    t: 1,
                 });
 
                 // undo
@@ -163,7 +163,7 @@ describe('Test clear selection content commands', () => {
                 expect(await commandService.executeCommand(RedoCommand.id)).toBeTruthy();
                 expect(getValue()).toStrictEqual({
                     v: 'A1',
-                    t: 4,
+                    t: 1,
                 });
             });
             it('clear formats with merged cells', async () => {
@@ -230,7 +230,7 @@ describe('Test clear selection content commands', () => {
                 // clear formats
                 expect(getValue()).toStrictEqual({
                     v: 'A1',
-                    t: 4,
+                    t: 1,
                 });
                 // remove merge
                 expect(getMerge()).toStrictEqual([]);
@@ -247,7 +247,7 @@ describe('Test clear selection content commands', () => {
                 expect(await commandService.executeCommand(RedoCommand.id)).toBeTruthy();
                 expect(getValue()).toStrictEqual({
                     v: 'A1',
-                    t: 4,
+                    t: 1,
                 });
                 expect(getMerge()).toStrictEqual([]);
             });

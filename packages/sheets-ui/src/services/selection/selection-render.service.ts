1ea59210953dc692f8202fbb5cbf3003436738c3
Univer <68851825+DR-Univer@users.noreply.github.com>
2023-12-04 17:10:42 +0800

    fix(sheet): selection shift bug (#626)
    * fix(sheet): selection shift bug
    
    * fix(sheet): optimized the implementation
    
    * docs(sheet): add comment

--- packages/sheets-ui/src/services/selection/selection-render.service.ts
+++ packages/sheets-ui/src/services/selection/selection-render.service.ts
@@ -493,21 +493,64 @@ export class SelectionRenderService implements ISelectionRenderService {
         const currentCell = selectionControl && selectionControl.model.currentCell;
 
         if (selectionControl && evt.shiftKey && currentCell) {
-            const { actualRow, actualColumn } = currentCell;
+            const { actualRow, actualColumn, mergeInfo: actualMergeInfo } = currentCell;
 
-            // TODO startCell position calculate error
-            const startCell = skeleton.getNoMergeCellPositionByIndex(actualRow, actualColumn, scaleX, scaleY);
-            const endCell = skeleton.getNoMergeCellPositionByIndex(endRow, endColumn, scaleX, scaleY);
+            /**
+             * Get the maximum range selected based on the two cells selected with Shift.
+             */
+
+            const newStartRow = Math.min(actualRow, startSelectionRange.startRow, actualMergeInfo.startRow);
+
+            const newEndRow = Math.max(actualRow, startSelectionRange.endRow, actualMergeInfo.endRow);
+
+            const newStartColumn = Math.min(actualColumn, startSelectionRange.startColumn, actualMergeInfo.startColumn);
+
+            const newEndColumn = Math.max(actualColumn, startSelectionRange.endColumn, actualMergeInfo.endColumn);
+
+            /**
+             * Calculate whether there are merged cells within the range. If there are, recursively expand the selection again.
+             */
+            const bounding = skeleton.getMergeBounding(newStartRow, newStartColumn, newEndRow, newEndColumn);
+
+            const startCell = skeleton.getNoMergeCellPositionByIndex(
+                bounding.startRow,
+                bounding.startColumn,
+                scaleX,
+                scaleY
+            );
+
+            const endCell = skeleton.getNoMergeCellPositionByIndex(bounding.endRow, bounding.endColumn, scaleX, scaleY);
 
             const newSelectionRange = {
-                startColumn: actualColumn,
-                startRow: actualRow,
-                endColumn: startSelectionRange.startColumn,
-                endRow: startSelectionRange.startRow,
-                startY: startCell?.startY || 0,
-                endY: endCell?.endY || 0,
-                startX: startCell?.startX || 0,
-                endX: endCell?.endX || 0,
+                startColumn: bounding.startColumn,
+                startRow: bounding.startRow,
+                endColumn: bounding.endColumn,
+                endRow: bounding.endRow,
+
+                startY: startCell.startY,
+                endY: endCell.endY,
+                startX: startCell.startX,
+                endX: endCell.endX,
+
+                rangeType,
+            };
+
+            /**
+             * When expanding the selection with the Shift key,
+             * the original highlighted cell should remain unchanged.
+             * If the highlighted cell is a merged cell, the selection needs to be expanded.
+             */
+            const activeCell = skeleton.getCellByIndex(actualRow, actualColumn, scaleX, scaleY);
+
+            this._startSelectionRange = {
+                startColumn: activeCell.mergeInfo.startColumn,
+                startRow: activeCell.mergeInfo.startRow,
+                endColumn: activeCell.mergeInfo.endColumn,
+                endRow: activeCell.mergeInfo.endRow,
+                startY: activeCell.mergeInfo.startY || 0,
+                endY: activeCell.mergeInfo.endY || 0,
+                startX: activeCell.mergeInfo.startX || 0,
+                endX: activeCell.mergeInfo.endX || 0,
                 rangeType,
             };
 

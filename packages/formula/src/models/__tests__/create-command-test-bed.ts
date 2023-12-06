03514e8938c7d21450b15f429a550245ea2c5ea8
Dushusir <1414556676@qq.com>
2023-12-06 14:26:31 +0800

    fix(formula): offset formula when delete

---
+++ packages/formula/src/models/__tests__/create-command-test-bed.ts
@@ -0,0 +1,85 @@
+import type { IWorkbookData } from '@univerjs/core';
+import { ILogService, IUniverInstanceService, LocaleType, Plugin, PluginType, Univer } from '@univerjs/core';
+import { FormulaEngineService } from '@univerjs/engine-formula';
+import { SelectionManagerService } from '@univerjs/sheets';
+import type { Dependency } from '@wendellhu/redi';
+import { Inject, Injector } from '@wendellhu/redi';
+
+import { FormulaDataModel } from '../formula-data.model';
+
+const TEST_WORKBOOK_DATA: IWorkbookData = {
+    id: 'test',
+    appVersion: '3.0.0-alpha',
+    sheets: {
+        sheet1: {
+            id: 'sheet1',
+            cellData: {
+                '0': {
+                    '0': {
+                        v: 'A1',
+                    },
+                    '1': {
+                        v: 'B1',
+                    },
+                },
+            },
+        },
+    },
+    locale: LocaleType.EN_US,
+    name: '',
+    sheetOrder: [],
+    styles: {},
+};
+export function createCommandTestBed(workbookConfig?: IWorkbookData, dependencies?: Dependency[]) {
+    const univer = new Univer();
+
+    let get: Injector['get'] | undefined;
+
+    /**
+     * This plugin hooks into Sheet's DI system to expose API to test scripts
+     */
+    class TestSpyPlugin extends Plugin {
+        static override type = PluginType.Sheet;
+
+        constructor(
+            _config: undefined,
+            @Inject(Injector) override readonly _injector: Injector
+        ) {
+            super('test-plugin');
+
+            this._injector = _injector;
+            get = this._injector.get.bind(this._injector);
+        }
+
+        override onStarting(injector: Injector): void {
+            injector.add([SelectionManagerService]);
+            injector.add([FormulaEngineService]);
+            injector.add([FormulaDataModel]);
+
+            dependencies?.forEach((d) => injector.add(d));
+        }
+
+        override onDestroy(): void {
+            get = undefined;
+        }
+    }
+
+    univer.registerPlugin(TestSpyPlugin);
+    const sheet = univer.createUniverSheet(workbookConfig || TEST_WORKBOOK_DATA);
+
+    if (get === undefined) {
+        throw new Error('[TestPlugin]: not hooked on!');
+    }
+
+    const univerInstanceService = get(IUniverInstanceService);
+    univerInstanceService.focusUniverInstance('test');
+
+    const logService = get(ILogService);
+    logService.toggleLogEnabled(false); // change this to `true` to debug tests via logs
+
+    return {
+        univer,
+        get,
+        sheet,
+    };
+}
---
+++ packages/formula/src/models/__tests__/formula-data.model.spec.ts
@@ -0,0 +1,295 @@
+import type { ICellData, IWorkbookData, Nullable, Univer } from '@univerjs/core';
+import { IUniverInstanceService, LocaleType, ObjectMatrix } from '@univerjs/core';
+import type { Injector } from '@wendellhu/redi';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+
+import { FormulaDataModel, initSheetFormulaData } from '../formula-data.model';
+import { createCommandTestBed } from './create-command-test-bed';
+
+const TEST_WORKBOOK_DATA_DEMO: IWorkbookData = {
+    id: 'test',
+    appVersion: '3.0.0-alpha',
+    sheets: {
+        sheet1: {
+            id: 'sheet1',
+            cellData: {
+                '0': {
+                    '3': {
+                        f: '=SUM(A1)',
+                        si: '3e4r5t',
+                    },
+                },
+                '1': {
+                    '3': {
+                        f: '=SUM(A2)',
+                        si: 'OSPtzm',
+                    },
+                },
+                '2': {
+                    '3': {
+                        si: 'OSPtzm',
+                    },
+                },
+                '3': {
+                    '3': {
+                        si: 'OSPtzm',
+                    },
+                },
+            },
+        },
+    },
+    locale: LocaleType.EN_US,
+    name: '',
+    sheetOrder: [],
+    styles: {},
+};
+
+describe('Test formula data model', () => {
+    describe('updateFormulaData', () => {
+        let univer: Univer;
+        let get: Injector['get'];
+        let formulaDataModel: FormulaDataModel;
+        let getValues: (
+            startRow: number,
+            startColumn: number,
+            endRow: number,
+            endColumn: number
+        ) => Array<Array<Nullable<ICellData>>> | undefined;
+
+        beforeEach(() => {
+            const testBed = createCommandTestBed(TEST_WORKBOOK_DATA_DEMO);
+            univer = testBed.univer;
+            get = testBed.get;
+
+            formulaDataModel = get(FormulaDataModel);
+
+            getValues = (
+                startRow: number,
+                startColumn: number,
+                endRow: number,
+                endColumn: number
+            ): Array<Array<Nullable<ICellData>>> | undefined =>
+                get(IUniverInstanceService)
+                    .getUniverSheetInstance('test')
+                    ?.getSheetBySheetId('sheet1')
+                    ?.getRange(startRow, startColumn, endRow, endColumn)
+                    .getValues();
+        });
+
+        afterEach(() => {
+            univer.dispose();
+        });
+
+        it('delete formula with id', () => {
+            formulaDataModel.initFormulaData();
+
+            const unitId = 'test';
+            const sheetId = 'sheet1';
+            const cellValue = {
+                '1': {
+                    '3': {
+                        v: null,
+                        p: null,
+                        f: null,
+                        si: null,
+                    },
+                },
+            };
+
+            const result = {
+                [unitId]: {
+                    [sheetId]: {
+                        '0': {
+                            '3': {
+                                f: '=SUM(A1)',
+                                si: '3e4r5t',
+                            },
+                        },
+                        '2': {
+                            '3': {
+                                f: '=SUM(A3)',
+                                si: 'OSPtzm',
+                            },
+                        },
+                        '3': {
+                            '3': {
+                                f: '=SUM(A3)',
+                                si: 'OSPtzm',
+                                x: 0,
+                                y: 1,
+                            },
+                        },
+                    },
+                },
+            };
+
+            formulaDataModel.updateFormulaData(unitId, sheetId, cellValue);
+
+            const formulaData = formulaDataModel.getFormulaData();
+            expect(formulaData).toStrictEqual(result);
+        });
+
+        it('delete formulas with ids and formulas with only ids', () => {
+            formulaDataModel.initFormulaData();
+
+            const unitId = 'test';
+            const sheetId = 'sheet1';
+            const cellValue = {
+                '0': {
+                    '3': {
+                        v: null,
+                        p: null,
+                        f: null,
+                        si: null,
+                    },
+                },
+                '1': {
+                    '3': {
+                        v: null,
+                        p: null,
+                        f: null,
+                        si: null,
+                    },
+                },
+                '2': {
+                    '3': {
+                        v: null,
+                        p: null,
+                        f: null,
+                        si: null,
+                    },
+                },
+            };
+
+            const result = {
+                [unitId]: {
+                    [sheetId]: {
+                        '3': {
+                            '3': {
+                                f: '=SUM(A4)',
+                                si: 'OSPtzm',
+                            },
+                        },
+                    },
+                },
+            };
+
+            formulaDataModel.updateFormulaData(unitId, sheetId, cellValue);
+
+            const formulaData = formulaDataModel.getFormulaData();
+            expect(formulaData).toStrictEqual(result);
+        });
+
+        it('delete the formula with only id', () => {
+            formulaDataModel.initFormulaData();
+
+            const unitId = 'test';
+            const sheetId = 'sheet1';
+            const cellValue = {
+                '3': {
+                    '3': {
+                        v: null,
+                        p: null,
+                        f: null,
+                        si: null,
+                    },
+                },
+            };
+
+            const result = {
+                [unitId]: {
+                    [sheetId]: {
+                        '0': {
+                            '3': {
+                                f: '=SUM(A1)',
+                                si: '3e4r5t',
+                            },
+                        },
+                        '1': {
+                            '3': {
+                                f: '=SUM(A2)',
+                                si: 'OSPtzm',
+                            },
+                        },
+                        '2': {
+                            '3': {
+                                f: '=SUM(A2)',
+                                si: 'OSPtzm',
+                                x: 0,
+                                y: 1,
+                            },
+                        },
+                    },
+                },
+            };
+
+            formulaDataModel.updateFormulaData(unitId, sheetId, cellValue);
+
+            const formulaData = formulaDataModel.getFormulaData();
+            expect(formulaData).toStrictEqual(result);
+        });
+    });
+
+    describe('function initSheetFormulaData', () => {
+        it('init formula data', () => {
+            const unitId = 'workbook-01';
+            const sheetId = 'sheet-0011';
+
+            const formulaData = {
+                [unitId]: {},
+            };
+
+            const cellValue = {
+                '0': {
+                    '0': {
+                        v: 1,
+                    },
+                    '1': {
+                        v: 2,
+                    },
+                    '2': {
+                        v: 3,
+                    },
+                    '3': {
+                        v: 2,
+                        f: '=SUM(A1)',
+                        si: '3e4r5t',
+                    },
+                },
+                '1': {
+                    '0': {
+                        v: 4,
+                    },
+                },
+                '2': {
+                    '0': {
+                        v: 44,
+                    },
+                },
+                '3': {
+                    '0': {
+                        v: 444,
+                    },
+                },
+            };
+
+            const cellMatrix = new ObjectMatrix(cellValue);
+
+            const result = {
+                [unitId]: {
+                    [sheetId]: {
+                        '0': {
+                            '3': {
+                                f: '=SUM(A1)',
+                                si: '3e4r5t',
+                            },
+                        },
+                    },
+                },
+            };
+
+            initSheetFormulaData(formulaData, unitId, sheetId, cellMatrix);
+            expect(formulaData).toStrictEqual(result);
+        });
+    });
+});

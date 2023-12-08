9a8a523d25c1b8f4942c96db907b191bcff99996
Dushusir <1414556676@qq.com>
2023-12-08 21:32:42 +0800

    fix(formula): auto fill twice (#687)

--- packages/sheets-formula/src/controllers/__tests__/formula-auto-fill.controller.spec.ts
+++ packages/sheets-formula/src/controllers/__tests__/formula-auto-fill.controller.spec.ts
@@ -7,7 +7,6 @@ import {
     ThemeService,
     UndoCommand,
 } from '@univerjs/core';
-import { FormulaEngineService } from '@univerjs/engine-formula';
 import {
     AddWorksheetMergeMutation,
     NORMAL_SELECTION_PLUGIN_NAME,
@@ -15,6 +14,7 @@ import {
     SelectionManagerService,
     SetRangeValuesMutation,
     SetSelectionsOperation,
+    SheetInterceptorService,
 } from '@univerjs/sheets';
 import {
     AutoFillCommand,
@@ -24,6 +24,7 @@ import {
     ISelectionRenderService,
     SelectionRenderService,
 } from '@univerjs/sheets-ui';
+import { DesktopPlatformService, DesktopShortcutService, IPlatformService, IShortcutService } from '@univerjs/ui';
 import type { Injector } from '@wendellhu/redi';
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 
@@ -49,10 +50,12 @@ describe('Test auto fill with formula', () => {
 
     beforeEach(() => {
         const testBed = createCommandTestBed(undefined, [
+            [SheetInterceptorService],
             [ISelectionRenderService, { useClass: SelectionRenderService }],
             [AutoFillController],
-            [FormulaEngineService],
             [IAutoFillService, { useClass: AutoFillService }],
+            [IShortcutService, { useClass: DesktopShortcutService }],
+            [IPlatformService, { useClass: DesktopPlatformService }],
             [FormulaAutoFillController],
         ]);
         univer = testBed.univer;
@@ -114,7 +117,7 @@ describe('Test auto fill with formula', () => {
                 }
             );
 
-            // values will be in the following format
+            // B1:B3 values will be in the following format
             // [
             //     [ { f: '=SUM(A1)' } ],
             //     [ { f: '=SUM(A2)', si: '1ZMZWH' } ],
@@ -134,7 +137,10 @@ describe('Test auto fill with formula', () => {
             B2 = values && values[1][0];
             B3 = values && values[2][0];
 
-            expect(B2).toStrictEqual({});
+            expect(B2).toStrictEqual({
+                t: 2,
+                v: 1,
+            });
             expect(B3).toStrictEqual({});
 
             // redo
@@ -145,6 +151,117 @@ describe('Test auto fill with formula', () => {
 
             expect(B2?.f).toStrictEqual('=SUM(A2)');
             expect(B2?.si).toEqual(B3?.si);
+
+            // drop to right
+            (autoFillController as any)._handleFill(
+                {
+                    startColumn: 1,
+                    endColumn: 1,
+                    startRow: 0,
+                    endRow: 2,
+                },
+                {
+                    startColumn: 1,
+                    endColumn: 3,
+                    startRow: 0,
+                    endRow: 2,
+                }
+            );
+
+            // B1:D3 values will be in the following format
+            // [
+            //     [
+            //         {
+            //             "f": "=SUM(A1)",
+            //         },
+            //         {
+            //             "f": "=SUM(B1)",
+            //             "si": "tF5X2Q"
+            //         },
+            //         {
+            //             "si": "tF5X2Q"
+            //         }
+            //     ],
+            //     [
+            //         {
+            //             "f": "=SUM(A2)",
+            //             "si": "d4_488"
+            //         },
+            //         {
+            //             "f": "=SUM(B2)",
+            //             "si": "d4_488"
+            //         },
+            //         {
+            //             "si": "d4_488"
+            //         }
+            //     ],
+            //     [
+            //         {
+            //             "si": "d4_488"
+            //         },
+            //         {
+            //             "si": "d4_488"
+            //         },
+            //         {
+            //             "si": "d4_488"
+            //         }
+            //     ]
+            // ]
+            values = getValues(0, 1, 2, 3);
+            let C1 = values && values[0][1];
+            let C2 = values && values[1][1];
+            let C3 = values && values[2][1];
+            let D1 = values && values[0][2];
+            let D2 = values && values[1][2];
+            let D3 = values && values[2][2];
+
+            expect(C1?.f).toStrictEqual('=SUM(B1)');
+            expect(C2?.f).toStrictEqual('=SUM(B2)');
+            expect(C1?.si).toEqual(D1?.si);
+
+            expect(B2?.si).toEqual(C2?.si);
+            expect(D2?.si).toEqual(C2?.si);
+
+            expect(B3?.si).toEqual(C3?.si);
+            expect(D3?.si).toEqual(C3?.si);
+
+            // undo
+            expect(await commandService.executeCommand(UndoCommand.id)).toBeTruthy();
+
+            values = getValues(0, 1, 2, 3);
+            C1 = values && values[0][1];
+            C2 = values && values[1][1];
+            C3 = values && values[2][1];
+            D1 = values && values[0][2];
+            D2 = values && values[1][2];
+            D3 = values && values[2][2];
+
+            expect(C1).toStrictEqual({});
+            expect(C2).toStrictEqual({});
+            expect(C3).toStrictEqual({});
+            expect(D1).toStrictEqual({});
+            expect(D2).toStrictEqual({});
+            expect(D3).toStrictEqual({});
+
+            // redo
+            expect(await commandService.executeCommand(RedoCommand.id)).toBeTruthy();
+            values = getValues(0, 1, 2, 3);
+            C1 = values && values[0][1];
+            C2 = values && values[1][1];
+            C3 = values && values[2][1];
+            D1 = values && values[0][2];
+            D2 = values && values[1][2];
+            D3 = values && values[2][2];
+
+            expect(C1?.f).toStrictEqual('=SUM(B1)');
+            expect(C2?.f).toStrictEqual('=SUM(B2)');
+            expect(C1?.si).toEqual(D1?.si);
+
+            expect(B2?.si).toEqual(C2?.si);
+            expect(D2?.si).toEqual(C2?.si);
+
+            expect(B3?.si).toEqual(C3?.si);
+            expect(D3?.si).toEqual(C3?.si);
 
             // Restore the original data
             expect(await commandService.executeCommand(UndoCommand.id)).toBeTruthy();

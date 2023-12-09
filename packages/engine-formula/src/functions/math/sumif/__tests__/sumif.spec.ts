9a17d7257b749ebb3d29abe53a2232f94e94d85f
Dushusir <1414556676@qq.com>
2023-12-09 22:23:55 +0800

    test(formula): test case for sumif (#702)
    * test(formula): test case for sumif
    
    * fix(formula): search and help panel style

--- packages/engine-formula/src/functions/math/sumif/__tests__/sumif.spec.ts
+++ packages/engine-formula/src/functions/math/sumif/__tests__/sumif.spec.ts
@@ -1,43 +1,76 @@
-import type { ICellData, Nullable, Univer } from '@univerjs/core';
-import { ICommandService, IUniverInstanceService } from '@univerjs/core';
+import type { IWorkbookData, Univer } from '@univerjs/core';
+import { LocaleType } from '@univerjs/core';
 import type { Injector } from '@wendellhu/redi';
-import { afterEach, beforeEach, describe, it } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 
-import type { LexerTreeBuilder } from '../../../../engine/analysis/lexer';
-import type { CalculateFormulaService } from '../../../../services/calculate-formula.service';
+import type { ISheetData } from '../../../../basics/common';
+import { RangeReferenceObject } from '../../../../engine/reference-object/range-reference-object';
+import { ValueObjectFactory } from '../../../../engine/value-object/array-value-object';
+import type { BaseValueObject } from '../../../../engine/value-object/base-value-object';
+import { IFunctionService } from '../../../../services/function.service';
 import { createCommandTestBed } from '../../../__tests__/create-command-test-bed';
+import { FUNCTION_NAMES_MATH } from '../../function-names';
+import { Sumif } from '../sumif';
+
+const TEST_WORKBOOK_DATA: IWorkbookData = {
+    id: 'test',
+    appVersion: '3.0.0-alpha',
+    sheets: {
+        sheet1: {
+            id: 'sheet1',
+            cellData: {
+                0: {
+                    0: {
+                        v: 1,
+                    },
+                },
+                1: {
+                    0: {
+                        v: 4,
+                    },
+                },
+                2: {
+                    0: {
+                        v: 44,
+                    },
+                },
+                3: {
+                    0: {
+                        v: 444,
+                    },
+                },
+            },
+        },
+    },
+    locale: LocaleType.ZH_CN,
+    name: '',
+    sheetOrder: [],
+    styles: {},
+};
 
 describe('test sumif', () => {
     let univer: Univer;
+    let injector: Injector;
     let get: Injector['get'];
-    let commandService: ICommandService;
-    let calculateFormulaService: CalculateFormulaService;
-    let lexerTreeBuilder: LexerTreeBuilder;
-    let getValue: (
-        startRow: number,
-        startColumn: number,
-        endRow: number,
-        endColumn: number
-    ) => Nullable<ICellData> | undefined;
+    let functionService: IFunctionService;
+    let unitId: string;
+    let sheetId: string;
+    let sheetData: ISheetData = {};
 
     beforeEach(() => {
-        const testBed = createCommandTestBed();
+        const testBed = createCommandTestBed(TEST_WORKBOOK_DATA);
         univer = testBed.univer;
+        injector = univer.__getInjector();
         get = testBed.get;
+        unitId = testBed.unitId;
+        sheetId = testBed.sheetId;
+        sheetData = testBed.sheetData;
 
-        commandService = get(ICommandService);
+        functionService = get(IFunctionService);
 
-        getValue = (
-            startRow: number,
-            startColumn: number,
-            endRow: number,
-            endColumn: number
-        ): Nullable<ICellData> | undefined =>
-            get(IUniverInstanceService)
-                .getUniverSheetInstance('test')
-                ?.getSheetBySheetId('sheet1')
-                ?.getRange(startRow, startColumn, endRow, endColumn)
-                .getValue();
+        // register sumif
+        const sumif = new Sumif(injector, FUNCTION_NAMES_MATH.SUMIF);
+        functionService.registerExecutors(sumif);
     });
 
     afterEach(() => {
@@ -45,10 +78,26 @@ describe('test sumif', () => {
     });
 
     it('range and criteria', async () => {
-        const params = {
-            value: {
-                f: '=SUMIF(A1:A4, ">40")',
-            },
+        // range A1:A4
+        const range = {
+            startRow: 0,
+            startColumn: 0,
+            endRow: 3,
+            endColumn: 0,
         };
+
+        const rangeRef = new RangeReferenceObject(range, sheetId, unitId);
+        rangeRef.setUnitData({
+            [unitId]: sheetData,
+        });
+
+        // criteria >40
+        const criteriaRef = ValueObjectFactory.create('>40');
+
+        // calculate
+        const executor = functionService.getExecutor(FUNCTION_NAMES_MATH.SUMIF);
+        const resultObject = executor?.calculate(rangeRef, criteriaRef) as BaseValueObject;
+        const value = resultObject?.getValue();
+        expect(value).toBe(488);
     });
 });

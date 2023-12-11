4a96052bed42855b74d92d131f5c5fd674038283
Wenzhao Hu <wzhudev@gmail.com>
2023-12-11 11:53:58 +0800

    chore: change license to apache 2.0

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 const { resolve } = require('node:path');
-const tsConfig = require('./tsconfig.json')
+const tsConfig = require('./tsconfig.json');
 
 /**
  * @type {import('eslint').Linter.Config}
@@ -19,6 +19,7 @@ const config = {
         'unused-imports',
         'simple-import-sort',
         'react',
+        'header',
     ],
     extends: [
         'airbnb-base', // https://www.npmjs.com/package/eslint-config-airbnb-base
@@ -39,6 +40,28 @@ const config = {
     //     ],
     // },
     rules: {
+        'header/header': [
+            2,
+            'block',
+            [
+                '*',
+                ' * Copyright 2023 DreamNum Inc.',
+                ' *',
+                ' * Licensed under the Apache License, Version 2.0 (the "License");',
+                ' * you may not use this file except in compliance with the License.',
+                ' * You may obtain a copy of the License at',
+                ' *',
+                ' *     http://www.apache.org/licenses/LICENSE-2.0',
+                ' *',
+                ' * Unless required by applicable law or agreed to in writing, software',
+                ' * distributed under the License is distributed on an "AS IS" BASIS,',
+                ' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
+                ' * See the License for the specific language governing permissions and',
+                ' * limitations under the License.',
+                ' '
+            ],
+            2,
+        ],
         'no-cond-assign': 'off',
         'no-restricted-globals': 'off',
         'import/prefer-default-export': 'off',
@@ -175,7 +198,7 @@ const config = {
                 parserOptions: {
                     project: resolve(__dirname, './tsconfig.eslint.json'),
                 },
-            }
+            };
         }),
     ],
 };

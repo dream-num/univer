55b6f3de544ccf729dbf0a0e00b4935604d71a21
DR-Univer <wbfsa@qq.com>
2023-12-29 02:02:26 +0800

    test(formula): vlookup

--- packages/engine-formula/src/functions/lookup/vlookup/__tests__/index.spec.ts
+++ packages/engine-formula/src/functions/lookup/vlookup/__tests__/index.spec.ts
@@ -14,52 +14,63 @@
  * limitations under the License.
  */
 
-import type { IRange } from '@univerjs/core';
-import { describe } from 'vitest';
+/* eslint-disable no-magic-numbers */
+import { describe, expect, it } from 'vitest';
 
-import type { ISheetData } from '../../../../basics/common';
-import type { ArrayValueObject } from '../../../../engine/value-object/array-value-object';
+import type { BaseValueObject } from '../../../..';
+import { NumberValueObject } from '../../../..';
+import { ArrayValueObject } from '../../../../engine/value-object/array-value-object';
+import { FUNCTION_NAMES_LOOKUP } from '../../function-names';
+import { Vlookup } from '..';
 
-const cellData = {
-    0: {
-        0: {
-            v: 1,
-        },
-        1: {
-            v: ' ',
-        },
-        2: {
-            v: 1.23,
-        },
-        3: {
-            v: true,
-        },
-        4: {
-            v: false,
-        },
-    },
-    1: {
-        0: {
-            v: 0,
-        },
-        1: {
-            v: '100',
-        },
-        2: {
-            v: '2.34',
-        },
-        3: {
-            v: 'test',
-        },
-        4: {
-            v: -3,
-        },
-    },
-};
+const arrayValueObject1 = new ArrayValueObject(
+    `{
+    1, "甲";
+    2, "乙";
+    3, "丙";
+    4, "丁";
+    5, "戊";
+    6, "己";
+    7, "庚";
+    8, "辛";
+}`
+);
 
-describe('test abs', () => {
-    let unitId: string;
-    let sheetId: string;
-    const sheetData: ISheetData = {};
-    let absCalculate: (range: IRange) => ArrayValueObject;
+const arrayValueObject2 = new ArrayValueObject(
+    `{
+    4, "丁";
+    5, "戊";
+    6, "己";
+    7, "庚";
+    8, "辛";
+    1, "甲";
+    2, "乙";
+    3, "丙";
+}`
+);
+
+describe('Test vlookup', () => {
+    const textFunction = new Vlookup(FUNCTION_NAMES_LOOKUP.VLOOKUP);
+
+    describe('Exact Match', () => {
+        it('normal', async () => {
+            const resultObject = textFunction.calculate(
+                new NumberValueObject(2),
+                arrayValueObject1,
+                new NumberValueObject(2),
+                new NumberValueObject(0)
+            ) as BaseValueObject;
+            expect(resultObject.getValue().toString()).toBe('乙');
+        });
+
+        it('normal2', async () => {
+            const resultObject = textFunction.calculate(
+                new NumberValueObject(8),
+                arrayValueObject1,
+                new NumberValueObject(2),
+                new NumberValueObject(0)
+            ) as BaseValueObject;
+            expect(resultObject.getValue().toString()).toBe('辛');
+        });
+    });
 });

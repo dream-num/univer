bd85df3a3a82918869fdfa25802ae05c776e7696
Dushusir <1414556676@qq.com>
2024-01-02 15:04:32 +0800

    fix(sheet): move row remove undefined

--- packages/core/src/shared/__test__/object-matrix.spec.ts
+++ packages/core/src/shared/__test__/object-matrix.spec.ts
@@ -16,13 +16,14 @@
 
 import { describe, expect, it } from 'vitest';
 
-import { ObjectMatrix } from '../object-matrix';
+import { moveMatrixArray, ObjectMatrix } from '../object-matrix';
 
 describe('test ObjectMatrix', () => {
     const getPrimitiveObj = () => ({
         1: { 1: '123', 2: '222', 3: '333' },
         2: { 1: '111', 2: '121', 3: '313' },
     });
+
     it('test deleteValue', () => {
         const matrix = new ObjectMatrix(getPrimitiveObj());
         matrix.realDeleteValue(1, 1);
@@ -34,5 +35,39 @@ describe('test ObjectMatrix', () => {
         expect(matrix.getValue(1, 3)).toBe(undefined);
         expect(matrix.getRow(1)).toBe(undefined);
         expect(matrix.getSizeOf()).toBe(1);
+    });
+
+    it('test forValue', () => {
+        const matrix = new ObjectMatrix(getPrimitiveObj());
+        matrix.realDeleteValue(1, 1);
+        matrix.realDeleteValue(1, 2);
+        matrix.realDeleteValue(1, 3);
+
+        const rowList: number[] = [];
+        const colList: number[] = [];
+        matrix.forValue((row, col, value) => {
+            rowList.push(row);
+            colList.push(col);
+        });
+
+        expect(rowList).toEqual([2, 2, 2]);
+        expect(colList).toEqual([1, 2, 3]);
+    });
+
+    it('test moveMatrixArray', () => {
+        const primitiveObject = getPrimitiveObj();
+        moveMatrixArray(0, 1, 2, primitiveObject);
+        expect(primitiveObject).toStrictEqual({
+            0: { 1: '123', 2: '222', 3: '333' },
+            2: { 1: '111', 2: '121', 3: '313' },
+        });
+    });
+    it('test moveMatrixArray', () => {
+        const primitiveObject = getPrimitiveObj();
+        moveMatrixArray(2, 1, 0, primitiveObject);
+        expect(primitiveObject).toStrictEqual({
+            0: { 1: '111', 2: '121', 3: '313' },
+            2: { 1: '123', 2: '222', 3: '333' },
+        });
     });
 });

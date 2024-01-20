67f45062fe67dd63d91155af309be6b533ca5126
Gpound.liu <141617023+Gggpound@users.noreply.github.com>
2024-01-20 20:35:07 +0800

    test(cache): wait (#1198)

--- packages/sheets-numfmt/vite.config.ts
+++ packages/sheets-numfmt/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(({ mode }) => ({
         }),
     ],
     css: {
+
         modules: {
             localsConvention: 'camelCaseOnly',
             generateScopedName: 'univer-[local]',

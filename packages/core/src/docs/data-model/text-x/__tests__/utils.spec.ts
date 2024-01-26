d6d2c771823fcb665a9f318cb506f69a2c370db5
jocs <ransixi@gmail.com>
2024-01-26 20:03:51 +0800

    test: utils more

--- packages/core/src/docs/data-model/text-x/__tests__/utils.spec.ts
+++ packages/core/src/docs/data-model/text-x/__tests__/utils.spec.ts
@@ -237,4 +237,41 @@ describe('test text-x utils', () => {
             }],
         });
     });
+
+    it('test composeBody fn both width paragraphs and from retain 2', () => {
+        const thisBody: IDocumentBody = {
+            dataStream: '',
+            paragraphs: [{
+                startIndex: 8,
+            }],
+        };
+
+        const otherBody: IDocumentBody = {
+            dataStream: '',
+            paragraphs: [{
+                startIndex: 5,
+                paragraphStyle: {
+                    spaceAbove: 10,
+                    lineSpacing: 2,
+                    spaceBelow: 0,
+                },
+            }],
+        };
+
+        const composedBody = composeBody(thisBody, otherBody);
+
+        expect(composedBody).toEqual({
+            dataStream: '',
+            paragraphs: [{
+                startIndex: 5,
+                paragraphStyle: {
+                    spaceAbove: 10,
+                    lineSpacing: 2,
+                    spaceBelow: 0,
+                },
+            }, {
+                startIndex: 8,
+            }],
+        });
+    });
 });

99869aa66b9e957561fb902eb6123104fb950dda
gggpound <gpoundLiu@gmail.com>
2024-01-26 19:00:43 +0800

    fix(dev): fix dev config

--- packages/sheets-conditional-format/vite.config.ts
+++ packages/sheets-conditional-format/vite.config.ts
@@ -1,71 +1,48 @@
-import { resolve } from 'node:path';
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
+import createViteConfig from '@univerjs/shared/vite';
+import pkg from './package.json';
 
-import { name } from './package.json';
-
-const libName = name
-    .replace('@univerjs/', 'univer-')
-    .split('-')
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join('');
-
-export default defineConfig(({ mode }) => ({
-    plugins: [
-        react(),
-        dts({
-            entryRoot: 'src',
-            outDir: 'lib/types',
-        }),
-    ],
-    css: {
-        modules: {
-            localsConvention: 'camelCaseOnly',
-            generateScopedName: 'univer-[local]',
-        },
-    },
-    define: {
-        'process.env.NODE_ENV': JSON.stringify(mode),
-    },
+export default ({ mode }) => createViteConfig({
     build: {
-        outDir: 'lib',
-        lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            name: libName,
-            fileName: format => `${format}/index.js`,
-            formats: ['es', 'umd', 'cjs'],
-        },
         rollupOptions: {
             external: [
                 '@univerjs/core',
                 '@univerjs/design',
+                '@univerjs/engine-formula',
+                '@univerjs/engine-render',
+                '@univerjs/sheets',
+                '@univerjs/sheets-ui',
                 '@univerjs/ui',
                 '@wendellhu/redi',
                 '@wendellhu/redi/react-bindings',
-                'clsx',
                 'react',
                 'rxjs',
+                'rxjs/operators',
             ],
             output: {
                 assetFileNames: 'index.css',
                 globals: {
                     '@univerjs/core': 'UniverCore',
                     '@univerjs/design': 'UniverDesign',
+                    '@univerjs/engine-formula': 'UniverEngineFormula',
+                    '@univerjs/engine-render': 'UniverEngineRender',
+                    '@univerjs/sheets': 'UniverSheets',
+                    '@univerjs/sheets-ui': 'UniverSheetsUi',
                     '@univerjs/ui': 'UniverUi',
                     '@wendellhu/redi': '@wendellhu/redi',
                     '@wendellhu/redi/react-bindings': '@wendellhu/redi/react-bindings',
-                    'clsx': 'clsx',
-                    'react': 'React',
-                    'rxjs': 'rxjs',
+                    react: 'React',
+                    rxjs: 'rxjs',
+                    'rxjs/operators': 'rxjs.operators',
                 },
             },
         },
     },
-    test: {
-        environment: 'happy-dom',
-        coverage: {
-            provider: 'istanbul',
-        },
+}, {
+    mode,
+    pkg,
+    features: {
+        react: true,
+        css: true,
+        dom: true,
     },
-}));
+});

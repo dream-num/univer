6822b57b74e3fd4d87f065a41544ccd14a1a0cc7
gggpound <gpoundLiu@gmail.com>
2024-01-31 17:35:17 +0800

    docs(cf): docs

--- packages/sheets-conditional-format/src/controllers/render.controller.ts
+++ packages/sheets-conditional-format/src/controllers/render.controller.ts
@@ -44,12 +44,17 @@ export class RenderController {
             const unitId = this._univerInstanceService.getCurrentUniverSheetInstance().getUnitId();
             this._renderManagerService.getRenderById(unitId)?.mainComponent?.makeDirty();
         };
-        this._conditionalFormatService.ruleComputeStatus$.pipe(bufferTime(16), filter((v) => {
+
+        // After the conditional format is marked dirty to drive a rendering, to trigger the window within the conditional format recalculation
+        this._conditionalFormatViewModel.markDirty$.pipe(bufferTime(16), filter((v) => {
             const workbook = this._univerInstanceService.getCurrentUniverSheetInstance();
             const worksheet = workbook.getActiveSheet();
             return v.filter((item) => item.unitId === workbook.getUnitId() && item.subUnitId === worksheet.getSheetId()).length > 0;
         })).subscribe(markDirtySkeleton);
-        this._conditionalFormatViewModel.markDirty$.pipe(bufferTime(16), filter((v) => {
+
+        // Once the calculation is complete, a view update is triggered
+        // This rendering does not trigger conditional format recalculation,because the rule is not mark dirty
+        this._conditionalFormatService.ruleComputeStatus$.pipe(bufferTime(16), filter((v) => {
             const workbook = this._univerInstanceService.getCurrentUniverSheetInstance();
             const worksheet = workbook.getActiveSheet();
             return v.filter((item) => item.unitId === workbook.getUnitId() && item.subUnitId === worksheet.getSheetId()).length > 0;

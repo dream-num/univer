92c0ce0333243f254aea993814c2c53a32d9b340
gggpound <gpoundLiu@gmail.com>
2024-02-21 20:06:56 +0800

    feat(cf): init menu

---
+++ packages/sheets-conditional-format/src/menu/manage-rule.ts
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2023-present DreamNum Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { ComponentManager, IMenuSelectorItem } from '@univerjs/ui';
+import type { IAccessor } from '@wendellhu/redi';
+import { MenuGroup, MenuItemType, MenuPosition } from '@univerjs/ui';
+import { OpenConditionalFormatOperator } from '../commands/operations/open-conditional-format-panel';
+
+export const FactoryManageConditionalFormatRule = (componentManager: ComponentManager) => {
+    return (_accessor: IAccessor) => {
+        return {
+            id: OpenConditionalFormatOperator.id,
+            type: MenuItemType.SELECTOR,
+            group: MenuGroup.TOOLBAR_FORMULAS_INSERT,
+            positions: [MenuPosition.TOOLBAR_START],
+            label: 'cf',
+            tooltip: 'cf',
+            selections: [{
+                label: '打开cf管理面板',
+                value: '123',
+            }, {
+                label: '2',
+            }],
+        } as IMenuSelectorItem;
+    };
+};

dd11e21a91649a1534195f3c0adfaadc3b06d321
gggpound <gpoundLiu@gmail.com>
2024-02-22 22:42:14 +0800

    feat(cf): support change priority

--- packages/sheets-conditional-format/src/components/panel/rule-list/index.tsx
+++ packages/sheets-conditional-format/src/components/panel/rule-list/index.tsx
@@ -14,15 +14,25 @@
  * limitations under the License.
  */
 
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Select } from '@univerjs/design';
 import { useDependency } from '@wendellhu/redi/react-bindings';
-import { IUniverInstanceService, Rectangle } from '@univerjs/core';
+import { ICommandService, IUniverInstanceService, Rectangle } from '@univerjs/core';
 import { SelectionManagerService } from '@univerjs/sheets';
 import { serializeRange } from '@univerjs/engine-formula';
+import { DeleteSingle, MoreFunctionSingle } from '@univerjs/icons';
+import GridLayout from 'react-grid-layout';
+import type { IDeleteCfCommandParams } from '../../../commands/commands/delete-cf.command';
+import { deleteCfCommand } from '../../../commands/commands/delete-cf.command';
+import type { IMoveCfCommand } from '../../../commands/commands/move-cf.command';
+import { moveCfCommand } from '../../../commands/commands/move-cf.command';
 import { ConditionalFormatRuleModel } from '../../../models/conditional-format-rule-model';
-import styles from '../index.module.less';
+import panelStyle from '../index.module.less';
 import type { IConditionFormatRule } from '../../../models/type';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+
+import styles from './index.module.less';
 
 interface IRuleListProps {
     onClick: (rule: IConditionFormatRule) => void;
@@ -32,12 +42,17 @@ export const RuleList = (props: IRuleListProps) => {
     const conditionalFormatRuleModel = useDependency(ConditionalFormatRuleModel);
     const univerInstanceService = useDependency(IUniverInstanceService);
     const selectionManagerService = useDependency(SelectionManagerService);
+    const commandService = useDependency(ICommandService);
+
     const workbook = univerInstanceService.getCurrentUniverSheetInstance();
     const unitId = workbook.getUnitId();
     const worksheet = workbook.getActiveSheet();
     const subUnitId = worksheet.getSheetId();
     const [selectValue, selectValueSet] = useState('2');
-    const selectOption = [{ label: '所选择单元格', value: '1' }, { label: '整张工作表', value: '2' }];
+    const [fetchRuleListId, fetchRuleListIdSet] = useState(0);
+    const [layoutWidth, layoutWidthSet] = useState(0);
+    const layoutContainerRef = useRef<HTMLDivElement>(null);
+    const selectOption = [{ label: '整张工作表', value: '2' }, { label: '所选择单元格', value: '1' }];
     const ruleList = useMemo(() => {
         const ruleList = conditionalFormatRuleModel.getSubunitRules(unitId, subUnitId);
         if (!ruleList || !ruleList.length) {
@@ -57,31 +72,92 @@ export const RuleList = (props: IRuleListProps) => {
             return ruleList;
         }
         return [];
-    }, [selectValue, subUnitId]);
+    }, [selectValue, subUnitId, fetchRuleListId, selectionManagerService, conditionalFormatRuleModel, unitId]);
 
+    useEffect(() => {
+        const dispose = conditionalFormatRuleModel.$ruleChange.subscribe(() => {
+            fetchRuleListIdSet(Math.random());
+        });
+        return () => dispose.unsubscribe();
+    }, [conditionalFormatRuleModel]);
+
+    useEffect(() => {
+        // 8 is padding-left
+        layoutWidthSet(Math.max(0, (layoutContainerRef.current?.getBoundingClientRect().width || 0) - 8));
+    }, [selectValue, fetchRuleListId]);
+
+    const handleDelete = (rule: IConditionFormatRule) => {
+        const unitId = univerInstanceService.getCurrentUniverSheetInstance().getUnitId();
+        const subUnitId = univerInstanceService.getCurrentUniverSheetInstance().getActiveSheet().getSheetId();
+        commandService.executeCommand(deleteCfCommand.id, { unitId, subUnitId, cfId: rule.cfId } as IDeleteCfCommandParams);
+    };
+
+    const handleDragStop = (_layout: unknown, from: { y: number }, to: { y: number }) => {
+        const unitId = univerInstanceService.getCurrentUniverSheetInstance().getUnitId();
+        const subUnitId = univerInstanceService.getCurrentUniverSheetInstance().getActiveSheet().getSheetId();
+        const getSaveIndex = (index: number) => {
+            const length = ruleList.length;
+            return Math.min(length - 1, Math.max(0, index));
+        };
+        const cfId = ruleList[getSaveIndex(from.y)].cfId;
+        const targetCfId = ruleList[getSaveIndex(to.y)].cfId;
+        commandService.executeCommand(moveCfCommand.id, { unitId, subUnitId, cfId, targetCfId } as IMoveCfCommand);
+    };
+    const layout = ruleList.map((rule, index) => ({ i: rule.cfId, x: 0, w: 12, y: index, h: 1, isResizable: false }));
     return (
         <>
-            <div>
+            <div className={styles.cfRuleList}>
                 <div>
                     管理
-                    <span className={styles.select}>
+                    <span className={panelStyle.select}>
                         <Select options={selectOption} value={selectValue} onChange={(v) => { selectValueSet(v); }} />
                     </span>
-
                     的规则
                 </div>
-                <div>
-                    {ruleList.map((rule) => {
-                        return (
-                            <div className={styles.cfRuleItem} key={`cfId_${rule.cfId}`} onClick={() => onClick(rule)}>
-                                <div>
-                                    <div>rule描述</div>
-                                    <div>{rule.ranges.map((range) => serializeRange(range)).join(',')}</div>
-                                </div>
-                                <div className={styles.preview}>预览 </div>
-                            </div>
-                        );
-                    })}
+                <div ref={layoutContainerRef} className={styles.gridLayoutWrap}>
+                    { layoutWidth
+                        ? (
+                            <GridLayout
+                                onDragStop={handleDragStop}
+                                layout={layout}
+                                cols={12}
+                                rowHeight={42}
+                                width={layoutWidth}
+                                margin={[0, 10]}
+                                draggableHandle=".draggableHandle"
+                            >
+                                { ruleList.map((rule) => {
+                                    return (
+                                        <div key={`${rule.cfId}`} className={styles.reactGridItem}>
+                                            <div className={styles.ruleItem} onClick={() => onClick(rule)}>
+                                                <div
+                                                    className={`${styles.draggableHandle} draggableHandle`}
+                                                    onClick={(e) => e.stopPropagation()}
+                                                >
+                                                    <MoreFunctionSingle />
+                                                </div>
+                                                <div>
+                                                    <div>rule描述</div>
+                                                    <div>{rule.ranges.map((range) => serializeRange(range)).join(',')}</div>
+                                                </div>
+                                                <div className={styles.preview}>预览 </div>
+                                                <div
+                                                    className={styles.deleteItem}
+                                                    onClick={(e) => {
+                                                        e.stopPropagation();
+                                                        handleDelete(rule);
+                                                    }}
+                                                >
+                                                    <DeleteSingle />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                            </GridLayout>
+                        )
+                        : null}
+
                 </div>
             </div>
         </>

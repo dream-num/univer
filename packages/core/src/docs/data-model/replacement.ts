55f4c5258af890365bee29c6e58b43ba84e6d06b
jocs <ransixi@gmail.com>
2024-03-01 15:18:25 +0800

    feat: replacement in document body

---
+++ packages/core/src/docs/data-model/replacement.ts
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2023-present DreamNum Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { IDocumentBody, IDocumentData } from '../../types/interfaces/i-document-data';
+import { DocumentDataModel } from './document-data-model';
+import { TextX } from './text-x/text-x';
+
+export function replaceInDocumentBody(body: IDocumentBody, query: string, target: string): IDocumentBody {
+    if (query === '') {
+        return body;
+    }
+
+    const mockDocumentData: IDocumentData = {
+        id: 'mock-id',
+        body,
+        documentStyle: {},
+    };
+
+    const documentDataModel = new DocumentDataModel(mockDocumentData);
+
+    const queryLen = query.length;
+    let index;
+
+    while ((index = documentDataModel.getBody()!.dataStream.indexOf(query)) >= 0) {
+        const textX = new TextX();
+
+        if (index > 0) {
+            textX.retain(index);
+        }
+
+        if (target.length > 0) {
+            const sliceBody = documentDataModel.sliceBody(index, index + queryLen);
+            const replaceBody: IDocumentBody = {
+                dataStream: target,
+            };
+
+            if (Array.isArray(sliceBody?.textRuns) && sliceBody.textRuns.length) {
+                replaceBody.textRuns = [{
+                    ...sliceBody.textRuns[0],
+                    st: 0,
+                    ed: target.length,
+                }];
+            }
+
+            textX.insert(target.length, replaceBody);
+        }
+
+        textX.delete(queryLen);
+
+        const actions = textX.serialize();
+
+        documentDataModel.apply(actions);
+    }
+
+    const newBody = documentDataModel.getBody()!;
+
+    documentDataModel.dispose();
+
+    return newBody;
+}

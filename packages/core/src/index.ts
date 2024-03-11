d09999276bab35753597d3d1e1f297dc2275f012
Dushusir <1414556676@qq.com>
2024-03-11 20:29:25 +0800

    feat(core): export function to use in exchange

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -141,8 +141,10 @@ export {
     transformSnapshotToDocumentData,
     generateTemporarySnap,
 } from './services/snapshot/snapshot-transform';
-export { textEncoder } from './services/snapshot/snapshot-utils';
+export { textEncoder, textDecoder } from './services/snapshot/snapshot-utils';
 export { type ILogContext } from './services/log/context';
 export { b64DecodeUnicode, b64EncodeUnicode } from './shared/coder';
+export { ClientSnapshotServerService } from './services/snapshot/snapshot-server.service';
+export { getSheetBlocksFromSnapshot } from './services/snapshot/snapshot-transform';
 
 installShims();

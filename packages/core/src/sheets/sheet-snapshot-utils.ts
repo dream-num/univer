39a778e75772e89fb4a85122672f1523a62560ea
zw <zhang9748@foxmail.com>
2024-03-23 20:59:38 +0800

    fix: build error

--- packages/core/src/sheets/sheet-snapshot-utils.ts
+++ packages/core/src/sheets/sheet-snapshot-utils.ts
@@ -78,7 +78,6 @@ export function mergeWorksheetSnapshotWithDefault(snapshot: Partial<IWorksheetDa
         },
         selections: ['A1'],
         rightToLeft: BooleanNumber.FALSE,
-        dataValidation: [],
     };
 
     // Merge the user's snapshot with the default snapshot.

de22d32db13980de0e6fb0e950b4239848efbf4a
DR-Univer <wbfsa@qq.com>
2024-03-27 19:58:24 +0800

    docs(sheet): add comment

--- packages/engine-render/src/components/sheets/sheet-skeleton.ts
+++ packages/engine-render/src/components/sheets/sheet-skeleton.ts
@@ -1632,8 +1632,14 @@ export class SpreadsheetSkeleton extends Skeleton {
             );
 
             if (isMerged && !isMergedMainCell) {
+                /**
+                 * If the cell is merged and is not the main cell, the cell is not rendered.
+                 */
                 return true;
             } else if (!isMergedMainCell) {
+                /**
+                 * If the cell no merged, the cell is not rendered.
+                 */
                 return true;
             }
         }

1876e68d23241f14faf6885cc5e8f2b074ff4c72
#FF0000 <hexf00@gmail.com>
2024-04-02 17:00:44 +0800

    fix(facade): fix set horizontal, vertical, warp (#1766)
    * fix(facade): fix set horizontal and vertical
    
    * fix(facade): fix set warp with range

--- packages/facade/src/apis/sheets/__tests__/f-range.spec.ts
+++ packages/facade/src/apis/sheets/__tests__/f-range.spec.ts
@@ -15,8 +15,8 @@
  */
 
 import type { ICellData, IStyleData, Nullable } from '@univerjs/core';
-import { ICommandService, IUniverInstanceService } from '@univerjs/core';
-import { SetRangeValuesCommand, SetRangeValuesMutation, SetStyleCommand } from '@univerjs/sheets';
+import { HorizontalAlign, ICommandService, IUniverInstanceService, VerticalAlign, WrapStrategy } from '@univerjs/core';
+import { SetHorizontalTextAlignCommand, SetRangeValuesCommand, SetRangeValuesMutation, SetStyleCommand, SetTextWrapCommand, SetVerticalTextAlignCommand } from '@univerjs/sheets';
 import type { Injector } from '@wendellhu/redi';
 import { beforeEach, describe, expect, it } from 'vitest';
 
@@ -49,6 +49,9 @@ describe('Test FRange', () => {
         commandService.registerCommand(SetRangeValuesCommand);
         commandService.registerCommand(SetRangeValuesMutation);
         commandService.registerCommand(SetStyleCommand);
+        commandService.registerCommand(SetVerticalTextAlignCommand);
+        commandService.registerCommand(SetHorizontalTextAlignCommand);
+        commandService.registerCommand(SetTextWrapCommand);
 
         getValueByPosition = (
             startRow: number,
@@ -450,5 +453,22 @@ describe('Test FRange', () => {
         expect(getStyleByPosition(0, 0, 0, 0)?.ff).toBe(undefined);
         expect(getStyleByPosition(0, 0, 0, 0)?.fs).toBe(undefined);
         expect(getStyleByPosition(0, 0, 0, 0)?.cl).toBe(undefined);
+    });
+
+    it('Range set range vertical and Horizontal and Warp', async () => {
+        const activeSheet = univerAPI.getActiveWorkbook()?.getActiveSheet();
+
+        const range = activeSheet?.getRange(0, 0, 10, 10);
+
+        await range!.setVerticalAlignment('middle');
+        await range!.setHorizontalAlignment('center');
+
+        expect(getStyleByPosition(0, 0, 0, 0)?.ht).toBe(HorizontalAlign.CENTER);
+        expect(getStyleByPosition(0, 0, 0, 0)?.vt).toBe(VerticalAlign.MIDDLE);
+
+        await range!.setWrap(true);
+        expect(getStyleByPosition(0, 0, 0, 0)?.tb).toBe(WrapStrategy.WRAP);
+        await range!.setWrapStrategy(WrapStrategy.CLIP);
+        expect(getStyleByPosition(0, 0, 0, 0)?.tb).toBe(WrapStrategy.CLIP);
     });
 });

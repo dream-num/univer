785e141f8b8a311bb1bf55922bac34ecd6765d8e
白熱 <sonne@asaki.me>
2024-04-03 11:53:02 +0800

    feat(condiational-formatting): update enUS locale (#1787)

--- examples/src/sheets/locales.ts
+++ examples/src/sheets/locales.ts
@@ -19,6 +19,9 @@ import { enUS as UniverDesignEnUS } from '@univerjs/design';
 import { enUS as UniverDocsUIEnUS } from '@univerjs/docs-ui';
 import { enUS as UniverSheetsEnUS } from '@univerjs/sheets';
 import { enUS as UniverSheetsUIEnUS } from '@univerjs/sheets-ui';
+import { enUS as UniverSheetsFormulaEnUS } from '@univerjs/sheets-formula';
+import { enUS as UniverSheetsDataValidationEnUS } from '@univerjs/sheets-data-validation';
+import { enUS as UniverSheetsConditionalFormattingUIEnUS } from '@univerjs/sheets-conditional-formatting-ui';
 import { enUS as UniverUiEnUS } from '@univerjs/ui';
 
 export const locales = {
@@ -26,6 +29,9 @@ export const locales = {
         ...UniverSheetsEnUS,
         ...UniverDocsUIEnUS,
         ...UniverSheetsUIEnUS,
+        ...UniverSheetsFormulaEnUS,
+        ...UniverSheetsDataValidationEnUS,
+        ...UniverSheetsConditionalFormattingUIEnUS,
         ...UniverUiEnUS,
         ...UniverDesignEnUS,
     },

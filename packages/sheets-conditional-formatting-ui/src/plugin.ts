d2ba58bc879ae4e1899969f61b2e354a5a53935b
白熱 <sonne@asaki.me>
2024-04-07 12:03:41 +0800

    refactor(conditional-formatting): rename exported plugin name (#1811)

--- packages/sheets-conditional-formatting-ui/src/plugin.ts
+++ packages/sheets-conditional-formatting-ui/src/plugin.ts
@@ -43,7 +43,7 @@ import { RefRangeController } from './controllers/cf.ref-range.controller';
 import { ConditionalFormattingEditorController } from './controllers/cf.editor.controller';
 import { ConditionalFormattingClearController } from './controllers/cf.clear.controller';
 
-export class SheetsConditionalFormattingUIPlugin extends Plugin {
+export class UniverSheetsConditionalFormattingUIPlugin extends Plugin {
     static override type = PluginType.Sheet;
     static commandList = [
         AddAverageCfCommand,
@@ -85,7 +85,7 @@ export class SheetsConditionalFormattingUIPlugin extends Plugin {
     }
 
     _initCommand() {
-        [...SheetsConditionalFormattingPlugin.mutationList, ...SheetsConditionalFormattingUIPlugin.commandList].forEach((m) => {
+        [...SheetsConditionalFormattingPlugin.mutationList, ...UniverSheetsConditionalFormattingUIPlugin.commandList].forEach((m) => {
             this._commandService.registerCommand(m);
         });
     }

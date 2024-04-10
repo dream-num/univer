b6862e0ac25b781f342413b056383cf45da50d5f
白熱 <sonne@asaki.me>
2024-04-10 11:45:35 +0800

    chore(data-validation): fix import path

--- packages/data-validation/src/index.ts
+++ packages/data-validation/src/index.ts
@@ -68,3 +68,4 @@ export { TWO_FORMULA_OPERATOR_COUNT } from './types/const/two-formula-operators'
 export { DataValidationResourceController } from './controllers/dv-resource.controller';
 export { DataValidationSheetController } from './controllers/dv-sheet.controller';
 export { DataValidationFormulaMarkDirty, type IDataValidationFormulaMarkDirtyParams } from './commands/mutations/formula.mutation';
+export { TextLengthErrorTitleMap } from './types/const/operator-text-map';

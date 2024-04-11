5527132bda2b7557cb7b7d64fec3892569c0e9b6
zw <zhang9748@foxmail.com>
2024-04-11 14:52:43 +0800

    feat: subUnitId

--- packages/sheets-data-validation/src/views/formula-input/custom-formula-input.tsx
+++ packages/sheets-data-validation/src/views/formula-input/custom-formula-input.tsx
@@ -38,6 +38,7 @@ export function CustomFormulaInput(props: IFormulaInputProps) {
                 }}
                 onlyInputFormula
                 openForSheetUnitId={unitId}
+                openForSheetSubUnitId={subUnitId}
             />
         </FormLayout>
     );

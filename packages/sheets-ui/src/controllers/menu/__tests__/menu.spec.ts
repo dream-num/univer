8935d14eb8787a8642c32420a18c47eba882af86
Wenzhao Hu <wzhudev@gmail.com>
2024-04-18 13:38:36 +0800

    chore: remove permission test

--- packages/sheets-ui/src/controllers/menu/__tests__/menu.spec.ts
+++ packages/sheets-ui/src/controllers/menu/__tests__/menu.spec.ts
@@ -14,15 +14,12 @@
  * limitations under the License.
  */
 
-import type { Univer, Workbook } from '@univerjs/core';
+import type { Univer } from '@univerjs/core';
 import {
     DisposableCollection,
     ICommandService,
-    IUniverInstanceService,
     RANGE_TYPE,
     toDisposable,
-    UniverInstanceType,
-    UniverPermissionService,
 } from '@univerjs/core';
 import {
     NORMAL_SELECTION_PLUGIN_NAME,
@@ -30,7 +27,6 @@ import {
     SetBoldCommand,
     SetRangeValuesMutation,
     SetStyleCommand,
-    SheetPermissionService,
 } from '@univerjs/sheets';
 import { Injector } from '@wendellhu/redi';
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
@@ -67,11 +63,6 @@ describe('Test menu items', () => {
     it('Test bold menu item', async () => {
         let activated = false;
         let disabled = false;
-        const sheetPermissionService = get(SheetPermissionService);
-        const univerPermissionService = get(UniverPermissionService);
-        const univerInstanceService = get(IUniverInstanceService);
-        const workbook = univerInstanceService.getCurrentUnitForType<Workbook>(UniverInstanceType.SHEET)!;
-        const worksheet = workbook.getActiveSheet();
         const menuItem = get(Injector).invoke(BoldMenuItemFactory);
         disposableCollection.add(toDisposable(menuItem.activated$!.subscribe((v: boolean) => (activated = v))));
         disposableCollection.add(toDisposable(menuItem.disabled$!.subscribe((v: boolean) => (disabled = v))));
@@ -103,16 +94,5 @@ describe('Test menu items', () => {
 
         expect(await commandService.executeCommand(SetBoldCommand.id)).toBeTruthy();
         expect(activated).toBe(true);
-
-        expect(sheetPermissionService.getSheetEditable(workbook.getUnitId(), worksheet.getSheetId())).toBe(true);
-        sheetPermissionService.setSheetEditable(false, workbook.getUnitId(), worksheet.getSheetId());
-        expect(sheetPermissionService.getSheetEditable()).toBe(false);
-        sheetPermissionService.setSheetEditable(true, workbook.getUnitId(), worksheet.getSheetId());
-        univerPermissionService.setEditable(workbook.getUnitId(), false);
-        expect(sheetPermissionService.getSheetEditable()).toBe(false);
-        expect(univerPermissionService.getEditable()).toBe(false);
-        univerPermissionService.setEditable(workbook.getUnitId(), true);
-        expect(univerPermissionService.getEditable()).toBe(true);
-        expect(sheetPermissionService.getSheetEditable()).toBe(true);
     });
 });

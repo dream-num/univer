5f5e6254affbe77aef18e2cdaab2f77c850dde11
Wenzhao Hu <wzhudev@gmail.com>
2024-04-29 21:56:19 +0800

    feat: ui plugin support override dependencies

---
+++ packages/core/src/services/plugin/__tests__/plugin-override.spec.ts
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2023-present DreamNum Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createIdentifier } from '@wendellhu/redi';
+import { describe, expect, it } from 'vitest';
+import { mergeOverrideWithDependencies } from '../plugin-override';
+
+describe('test dependency override', () => {
+    it('should override dependencies', () => {
+        class A {}
+        interface IA {}
+        const IA = createIdentifier<IA>('IA');
+
+        expect(mergeOverrideWithDependencies([[A]], [[A, null]]))
+            .toEqual([]);
+
+        expect(mergeOverrideWithDependencies([[IA, { useClass: A }]], [[IA, null]]))
+            .toEqual([]);
+
+        expect(mergeOverrideWithDependencies([[A], [IA, { useClass: A }]], [[IA, { useValue: {} }]]))
+            .toEqual([[A], [IA, { useValue: {} }]]);
+    });
+});

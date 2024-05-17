15fca4e88b4db8a92d74d3ef0c5691b783817928
白熱 <sonne@asaki.me>
2024-05-17 18:22:23 +0800

    build(debugger): fix issues related to bundling configurations

--- packages/debugger/vite.config.ts
+++ packages/debugger/vite.config.ts
@@ -1,7 +1,9 @@
+import vue from '@vitejs/plugin-vue';
 import createViteConfig from '@univerjs/shared/vite';
 import pkg from './package.json';
 
 export default ({ mode }) => createViteConfig({
+    plugins: [vue()],
 }, {
     mode,
     pkg,

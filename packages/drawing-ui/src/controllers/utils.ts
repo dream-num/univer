3bfd880948500033f85fb8df3b28ca5327d5189c
DR-Univer <wbfsa@qq.com>
2024-05-23 16:10:41 +0800

    fix(drawing): group data load and active sheet

---
+++ packages/drawing-ui/src/controllers/utils.ts
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2023-present DreamNum Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { IDrawingManagerService, IDrawingSearch } from '@univerjs/core';
+import { getDrawingShapeKeyByDrawingSearch } from '@univerjs/drawing';
+import type { BaseObject, Scene } from '@univerjs/engine-render';
+import { DRAWING_OBJECT_LAYER_INDEX, Group } from '@univerjs/engine-render';
+
+export function insertGroupObject(objectParam: IDrawingSearch, object: BaseObject, scene: Scene, drawingManagerService: IDrawingManagerService) {
+    const groupParam = drawingManagerService.getDrawingByParam(objectParam);
+    if (groupParam == null) {
+        return;
+    }
+
+    const groupKey = getDrawingShapeKeyByDrawingSearch(objectParam);
+    const groupObject = scene.getObject(groupKey);
+
+    if (!(groupObject instanceof Group)) {
+        return;
+    }
+
+    if (groupObject != null) {
+        groupObject.addObject(object);
+    }
+
+    const group = new Group(groupKey);
+
+    scene.addObject(group, DRAWING_OBJECT_LAYER_INDEX).attachTransformerTo(groupObject);
+
+    group.addObject(object);
+
+    groupParam.transform && group.transformByState({ left: groupParam.transform.left, top: groupParam.transform.top });
+}

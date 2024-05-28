835bd79076766a933131e6b1fe276d0fddf24061
zw <zhang9748@foxmail.com>
2024-05-28 14:52:30 +0800

    feat: u

--- examples/src/data/sheets/demo/default-workbook-data-demo.ts
+++ examples/src/data/sheets/demo/default-workbook-data-demo.ts
@@ -18,7 +18,7 @@ import type { IDocumentData, IWorkbookData } from '@univerjs/core';
 import { BooleanNumber, DataValidationErrorStyle, DataValidationOperator, DataValidationType, LocaleType } from '@univerjs/core';
 
 import { DATA_VALIDATION_PLUGIN_NAME } from '@univerjs/sheets-data-validation';
-import type { ICellHyperLink } from '@univerjs/sheets-hyper-link/types/interfaces/i-hyper-link.js';
+import type { ICellHyperLink } from '@univerjs/sheets-hyper-link';
 import { PAGE5_RICHTEXT_1 } from '../../slides/rich-text/page5-richtext1';
 
 const richTextDemo: IDocumentData = {

80cba1eac63b4fb316c3f83de41f8a1c7f0e27b6
zw <zhang9748@foxmail.com>
2024-05-31 17:20:07 +0800

    feat: lint

--- packages/sheets/src/controllers/basic-worksheet.controller.ts
+++ packages/sheets/src/controllers/basic-worksheet.controller.ts
@@ -128,7 +128,6 @@ import { SetRangeProtectionCommand } from '../commands/commands/set-range-protec
 import { AddRangeProtectionMutation } from '../commands/mutations/add-range-protection.mutation';
 import { DeleteRangeProtectionMutation } from '../commands/mutations/delete-range-protection.mutation';
 import { SetRangeProtectionMutation } from '../commands/mutations/set-range-protection.mutation';
-import { SetDrawingApplyMutation } from '../commands/mutations/set-drawing-apply.mutation';
 import { AddWorksheetMergeMutation } from '../commands/mutations/add-worksheet-merge.mutation';
 import { MAX_CELL_PER_SHEET_DEFAULT, MAX_CELL_PER_SHEET_KEY } from './config/config';
 import { ONLY_REGISTER_FORMULA_RELATED_MUTATIONS_KEY } from './config';

97cf71a99916eda73b683986f58e35b6bb644b54
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2024-06-03 17:57:45 +0800

    fix: remove filter plugin from worker

--- examples/src/sheets/worker.ts
+++ examples/src/sheets/worker.ts
@@ -18,7 +18,6 @@ import { LocaleType, LogLevel, Univer } from '@univerjs/core';
 import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula';
 import { UniverRPCWorkerThreadPlugin } from '@univerjs/rpc';
 import { UniverSheetsPlugin } from '@univerjs/sheets';
-import { UniverSheetsFilterPlugin } from '@univerjs/sheets-filter';
 
 // Univer web worker is also a univer application.
 const univer = new Univer({
@@ -29,7 +28,6 @@ const univer = new Univer({
 univer.registerPlugin(UniverSheetsPlugin, { onlyRegisterFormulaRelatedMutations: true });
 univer.registerPlugin(UniverFormulaEnginePlugin);
 univer.registerPlugin(UniverRPCWorkerThreadPlugin);
-univer.registerPlugin(UniverSheetsFilterPlugin);
 
 declare let self: WorkerGlobalScope & typeof globalThis & { univer: Univer };
 self.univer = univer;

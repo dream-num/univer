218be36600abbb77c81dd601a3518b05aafab7fb
zw <zhang9748@foxmail.com>
2024-06-05 21:46:30 +0800

    feat: update

--- packages/sheets-drawing-ui/src/controllers/sheet-drawing-printing.controller.ts
+++ packages/sheets-drawing-ui/src/controllers/sheet-drawing-printing.controller.ts
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-import { Disposable, LifecycleStages, OnLifecycle, Tools } from '@univerjs/core';
-import { IDrawingManagerService } from '@univerjs/drawing';
+import { Disposable, IRange, LifecycleStages, OnLifecycle, Tools } from '@univerjs/core';
+import { DrawingTypeEnum, IDrawingManagerService } from '@univerjs/drawing';
 import { DrawingRenderService } from '@univerjs/drawing-ui';
 import { IRenderManagerService } from '@univerjs/engine-render';
 import { SheetPrintInterceptorService, SheetSkeletonManagerService } from '@univerjs/sheets-ui';
@@ -27,7 +27,6 @@ export class SheetDrawingPrintingController extends Disposable {
         @Inject(SheetPrintInterceptorService) private readonly _sheetPrintInterceptorService: SheetPrintInterceptorService,
         @Inject(DrawingRenderService) private readonly _drawingRenderService: DrawingRenderService,
         @IDrawingManagerService private readonly _drawingManagerService: IDrawingManagerService,
-        @Inject(SheetSkeletonManagerService) private readonly _sheetSkeletonManagerService: SheetSkeletonManagerService,
         @IRenderManagerService private readonly _renderManagerService: IRenderManagerService
     ) {
         super();
@@ -61,12 +60,13 @@ export class SheetDrawingPrintingController extends Disposable {
                 this._sheetPrintInterceptorService.interceptor.getInterceptPoints().PRINTING_RANGE,
                 {
                     handler: (range, pos, next) => {
-                        if (!range) {
+                        const { unitId, subUnitId } = pos;
+                        const renderer = this._renderManagerService.getRenderById(unitId);
+                        if (!renderer) {
                             return next(range);
                         }
 
-                        const { unitId, subUnitId } = pos;
-                        const skeleton = this._sheetSkeletonManagerService.getUnitSkeleton(unitId, subUnitId);
+                        const skeleton = renderer.with(SheetSkeletonManagerService).getUnitSkeleton(unitId, subUnitId);
                         if (!skeleton) {
                             return next(range);
                         }
@@ -76,35 +76,35 @@ export class SheetDrawingPrintingController extends Disposable {
                             return next(range);
                         }
 
-                        const renderer = this._renderManagerService.getRenderById(unitId);
-                        if (!renderer) {
-                            return next(range);
-                        }
                         const { scaleX, scaleY } = renderer.scene;
-                        const newRange = { ...range };
-                        subUnitData.order.forEach((id) => {
-                            const param = subUnitData.data[id];
-                            if (!param.groupId && param.transform && Tools.isDefine(param.transform.left) && Tools.isDefine(param.transform.top) && Tools.isDefine(param.transform.width) && Tools.isDefine(param.transform.height)) {
-                                const start = skeleton.skeleton.getCellPositionByOffset(param.transform.left, param.transform.top, scaleX, scaleY, { x: 0, y: 0 });
-                                const end = skeleton.skeleton.getCellPositionByOffset(param.transform.left + param.transform.width, param.transform.top + param.transform.height, scaleX, scaleY, { x: 0, y: 0 });
-                                if (start.column < newRange.startColumn) {
-                                    newRange.startColumn = start.column;
-                                }
+                        const newRange: IRange = range ? { ...range } : { startColumn: 0, endColumn: 0, endRow: 0, startRow: 0 };
+                        const data = subUnitData.order.map(key => subUnitData.data[key]).filter(item => item.drawingType !== DrawingTypeEnum.DRAWING_DOM);
+                        if (data.length) {
+                            data.forEach((param) => {
+                                if (!param.groupId && param.transform && Tools.isDefine(param.transform.left) && Tools.isDefine(param.transform.top) && Tools.isDefine(param.transform.width) && Tools.isDefine(param.transform.height)) {
+                                    const start = skeleton.skeleton.getCellPositionByOffset(param.transform.left, param.transform.top, scaleX, scaleY, { x: 0, y: 0 });
+                                    const end = skeleton.skeleton.getCellPositionByOffset(param.transform.left + param.transform.width, param.transform.top + param.transform.height, scaleX, scaleY, { x: 0, y: 0 });
+                                    if (start.column < newRange.startColumn) {
+                                        newRange.startColumn = start.column;
+                                    }
 
-                                if (start.row < newRange.startRow) {
-                                    newRange.startRow = start.row;
-                                }
+                                    if (start.row < newRange.startRow) {
+                                        newRange.startRow = start.row;
+                                    }
 
-                                if (newRange.endRow < end.row) {
-                                    newRange.endRow = end.row;
-                                }
+                                    if (newRange.endRow < end.row) {
+                                        newRange.endRow = end.row;
+                                    }
 
-                                if (newRange.endColumn < end.column) {
-                                    newRange.endColumn = end.column;
+                                    if (newRange.endColumn < end.column) {
+                                        newRange.endColumn = end.column;
+                                    }
                                 }
-                            }
-                        });
-                        return next(newRange);
+                            });
+                            return next(newRange);
+                        }
+
+                        return next(range);
                     },
                 }
             )

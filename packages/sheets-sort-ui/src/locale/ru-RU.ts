19a7cfafadda18c9d834e9c9f8593c267c1e94e1
YuHong <csuzyh@gmail.com>
2024-06-12 17:48:20 +0800

    feat: add locale

---
+++ packages/sheets-sort-ui/src/locale/ru-RU.ts
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2023-present DreamNum Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default {
+    'sheets-sort': {
+        general: {
+            sort: 'Sort',
+            'sort-asc': 'Ascending',
+            'sort-desc': 'Descending',
+            'sort-custom': 'Custom Sort',
+            'sort-asc-ext': 'Expand Ascending',
+            'sort-desc-ext': 'Expand Descending',
+            'sort-asc-cur': 'Ascending',
+            'sort-desc-cur': 'Descending',
+        },
+        error: {
+            'merge-size': 'The selected range contains merged cells of different sizes, which cannot be sorted.',
+        },
+        dialog: {
+            'sort-reminder': 'Sort Reminder',
+            'sort-reminder-desc': 'Extend range sorting or keep range sorting?',
+            'sort-reminder-ext': 'Extend range sorting',
+            'sort-reminder-no': 'Keep range sorting',
+            'first-row-check': 'First row participates in sorting',
+            'add-condition': 'Add condition',
+            cancel: 'Cancel',
+            confirm: 'Confirm',
+        },
+    },
+};

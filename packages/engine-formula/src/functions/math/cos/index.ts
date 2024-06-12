d1b52c22122f0cf20da9e503072c6ae35bbf2a27
Abdelrahman Zaitoun <78018369+abdelrahman830a@users.noreply.github.com>
2024-06-12 16:44:34 +0300

    Create index.ts

---
+++ packages/engine-formula/src/functions/math/cos/index.ts
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2023-present DreamNum Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ErrorType } from '../../../basics/error-type';
+import { ArrayValueObject } from '../../../engine/value-object/array-value-object';
+import type { BaseValueObject } from '../../../engine/value-object/base-value-object';
+import { ErrorValueObject } from '../../../engine/value-object/base-value-object';
+import { NumberValueObject } from '../../../engine/value-object/primitive-object';
+import { BaseFunction } from '../../base-function';
+
+export class Cos extends BaseFunction {
+    override minParams = 1;
+
+    override maxParams = 1;
+
+    override calculate(variant: BaseValueObject) {
+        if (variant.isArray()) {
+            const resultArray: BaseValueObject[][] = [];
+
+            for (const row of variant.getArrayValue()) {
+                const resultRow: BaseValueObject[] = [];
+
+                for (const cell of row) {
+                    if (cell.isError()) {
+                        resultRow.push(cell);
+                    } else {
+                        resultRow.push(cos(cell as BaseValueObject));
+                    }
+                }
+
+                resultArray.push(resultRow);
+            }
+
+            return ArrayValueObject.create(resultArray);
+        } else {
+            return cos(variant);
+        }
+    }
+}
+
+
+function cos(num: BaseValueObject) {
+    let currentValue = num.getValue();
+
+    if (num.isBoolean()) {
+        currentValue = currentValue ? 1 : 0;
+    }
+
+    if (!Number.isFinite(currentValue)) {
+        return ErrorValueObject.create(ErrorType.VALUE);
+    }
+
+    const result = Math.cos(Number(currentValue));
+
+    if (Number.isNaN(result)) {
+        return ErrorValueObject.create(ErrorType.VALUE);
+    }
+
+    return NumberValueObject.create(result);
+}

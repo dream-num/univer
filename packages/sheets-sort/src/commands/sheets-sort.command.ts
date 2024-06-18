e00a9fa18b99dfc6989b408aa4f015430cf84e78
YuHong <csuzyh@gmail.com>
2024-06-18 15:16:39 +0800

    fix: sort by raw cell data

--- packages/sheets-sort/src/commands/sheets-sort.command.ts
+++ packages/sheets-sort/src/commands/sheets-sort.command.ts
@@ -125,7 +125,7 @@ function getRowCellData(
 ): Nullable<ICellData>[] {
     const result: Nullable<ICellData>[] = [];
     orderRules.forEach(({ colIndex }) => {
-        result.push(worksheet.getCell(rowIndex, colIndex));
+        result.push(worksheet.getCellRaw(rowIndex, colIndex));
     });
     return result;
 }

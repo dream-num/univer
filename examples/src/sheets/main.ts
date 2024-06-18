fab2bdd72ff5ce8f44b14ceaafcbe80cda84039d
Dushusir <1414556676@qq.com>
2024-06-18 15:38:46 +0800

    fix(sheet): disable button when there is only one sheet

--- examples/src/sheets/main.ts
+++ examples/src/sheets/main.ts
@@ -103,7 +103,7 @@ univer.registerPlugin(UniverSheetsDrawingUIPlugin);
 
 // create univer sheet instance
 if (!IS_E2E) {
-    univer.createUnit(UniverInstanceType.UNIVER_SHEET, DEFAULT_WORKBOOK_DATA_DEMO);
+    univer.createUnit(UniverInstanceType.UNIVER_SHEET, {});
 }
 
 const mockUser = {

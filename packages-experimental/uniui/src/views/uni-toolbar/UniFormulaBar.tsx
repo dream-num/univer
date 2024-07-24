2f771ac464d70a1d29a66a8e524411fd9974a74e
YuHong <csuzyh@gmail.com>
2024-07-24 18:46:39 +0800

    feat: uni formula bar

--- packages-experimental/uniui/src/views/uni-toolbar/UniFormulaBar.tsx
+++ packages-experimental/uniui/src/views/uni-toolbar/UniFormulaBar.tsx
@@ -14,17 +14,220 @@
  * limitations under the License.
  */
 
-import React from 'react';
-import { FxSingle } from '@univerjs/icons';
+import React, { useCallback, useLayoutEffect, useState } from 'react';
+import { CheckMarkSingle, CloseSingle, FxSingle } from '@univerjs/icons';
+import type { Nullable, Workbook } from '@univerjs/core';
+import { BooleanNumber, DEFAULT_EMPTY_DOCUMENT_VALUE, DOCS_FORMULA_BAR_EDITOR_UNIT_ID_KEY, DocumentFlavor, HorizontalAlign, IPermissionService, IUniverInstanceService, Rectangle, ThemeService, UniverInstanceType, useDependency, useObservable, VerticalAlign, WrapStrategy } from '@univerjs/core';
+import { IEditorBridgeService, IFormulaEditorManagerService, useActiveWorkbook } from '@univerjs/sheets-ui';
+import { RangeProtectionPermissionEditPoint, RangeProtectionRuleModel, SheetsSelectionsService, WorkbookEditablePermission, WorksheetEditPermission, WorksheetProtectionRuleModel, WorksheetSetCellValuePermission } from '@univerjs/sheets';
+import { EMPTY, merge, switchMap } from 'rxjs';
+import { DeviceInputEventType } from '@univerjs/engine-render';
+import { KeyCode, ProgressBar, TextEditor } from '@univerjs/ui';
+import clsx from 'clsx';
 import styles from './index.module.less';
 
 export const UniFormulaBar = () => {
+    const editorBridgeService = useDependency(IEditorBridgeService);
+    const univerInstanceService = useDependency(IUniverInstanceService);
+
+    const visibleInfo = useObservable(editorBridgeService.visible$);
+    const focusedId = useObservable(univerInstanceService.focused$);
+    const visible = (visibleInfo?.visible && focusedId) || false;
+
+    const handleOpenWrite = useCallback(() => {
+        if (focusedId) {
+            editorBridgeService.changeVisible({
+                visible: true,
+                eventType: DeviceInputEventType.PointerDown,
+                unitId: focusedId,
+            });
+        }
+    }, [editorBridgeService, focusedId]);
+
     return (
-        <div
-            className={styles.uniFormulaBar}
-        >
-            <FxSingle />
-            <span>Write formula</span>
-        </div>
+        <>
+            <div
+                className={styles.uniFormulaBar}
+                onClick={() => handleOpenWrite()}
+            >
+                <FxSingle />
+                <span>Write formula</span>
+            </div>
+            {visible && (
+                <div
+                    className={styles.uniFormulaBarFullInput}
+                >
+                    <FormulaBar />
+                </div>
+            )}
+        </>
     );
 };
+
+export function FormulaBar() {
+    const formulaEditorManagerService = useDependency(IFormulaEditorManagerService);
+    const editorBridgeService = useDependency(IEditorBridgeService);
+    const themeService = useDependency(ThemeService);
+    const progressBarColor = themeService.getCurrentTheme().primaryColor;
+    const [disable, setDisable] = useState<boolean>(false);
+    const univerInstanceService = useDependency(IUniverInstanceService);
+    const selectionManager = useDependency(SheetsSelectionsService);
+    const worksheetProtectionRuleModel = useDependency(WorksheetProtectionRuleModel);
+    const rangeProtectionRuleModel = useDependency(RangeProtectionRuleModel);
+    const permissionService = useDependency(IPermissionService);
+    const currentWorkbook = useActiveWorkbook();
+
+    function getPermissionIds(unitId: string, subUnitId: string): string[] {
+        return [
+            new WorkbookEditablePermission(unitId).id,
+            new WorksheetSetCellValuePermission(unitId, subUnitId).id,
+            new WorksheetEditPermission(unitId, subUnitId).id,
+        ];
+    }
+
+    useLayoutEffect(() => {
+        const workbook = univerInstanceService.getCurrentUnitForType<Workbook>(UniverInstanceType.UNIVER_SHEET)!;
+        const subscription = merge(
+            worksheetProtectionRuleModel.ruleChange$,
+            rangeProtectionRuleModel.ruleChange$,
+            selectionManager.selectionMoveEnd$,
+            workbook.activeSheet$
+        ).pipe(
+            switchMap(() => {
+                const unitId = workbook.getUnitId();
+                const worksheet = workbook.getActiveSheet();
+                if (!worksheet) return EMPTY;
+
+                const subUnitId = worksheet.getSheetId();
+                const range = selectionManager.getCurrentLastSelection()?.range;
+                if (!range) return EMPTY;
+
+                const permissionIds = getPermissionIds(unitId, subUnitId);
+
+                const selectionRanges = selectionManager.getCurrentSelections()?.map((selection) => selection.range);
+                const permissionList = rangeProtectionRuleModel.getSubunitRuleList(unitId, subUnitId).filter((rule) => {
+                    return rule.ranges.some((r) => selectionRanges?.some((selectionRange) => Rectangle.intersects(r, selectionRange)));
+                });
+
+                permissionList.forEach((p) => {
+                    permissionIds.push(new RangeProtectionPermissionEditPoint(unitId, subUnitId, p.permissionId).id);
+                });
+
+                return permissionService.composePermission$(permissionIds);
+            })
+        ).subscribe((permissions) => {
+            if (permissions) {
+                setDisable(!permissions.every((p) => p.value));
+            }
+        });
+
+        return () => {
+            subscription.unsubscribe();
+        };
+    }, []);
+
+    const INITIAL_SNAPSHOT = {
+        id: DOCS_FORMULA_BAR_EDITOR_UNIT_ID_KEY,
+        body: {
+            dataStream: `${DEFAULT_EMPTY_DOCUMENT_VALUE}`,
+            textRuns: [],
+            paragraphs: [
+                {
+                    startIndex: 0,
+                },
+            ],
+        },
+        documentStyle: {
+            pageSize: {
+                width: Number.POSITIVE_INFINITY,
+                height: Number.POSITIVE_INFINITY,
+            },
+            documentFlavor: DocumentFlavor.MODERN,
+            marginTop: 5,
+            marginBottom: 5,
+            marginRight: 0,
+            marginLeft: 0,
+            paragraphLineGapDefault: 0,
+            renderConfig: {
+                horizontalAlign: HorizontalAlign.UNSPECIFIED,
+                verticalAlign: VerticalAlign.TOP,
+                centerAngle: 0,
+                vertexAngle: 0,
+                wrapStrategy: WrapStrategy.WRAP,
+                isRenderStyle: BooleanNumber.FALSE,
+            },
+        },
+    };
+
+    function resizeCallBack(editor: Nullable<HTMLDivElement>) {
+        if (editor == null) {
+            return;
+        }
+
+        const editorRect = editor.getBoundingClientRect();
+
+        formulaEditorManagerService.setPosition(editorRect);
+    }
+
+    // Handle click the "×" button.
+    function handleCloseBtnClick() {
+        const visibleState = editorBridgeService.isVisible();
+        if (visibleState.visible) {
+            editorBridgeService.changeVisible({
+                visible: false,
+                eventType: DeviceInputEventType.Keyboard,
+                keycode: KeyCode.ESC,
+                unitId: currentWorkbook?.getUnitId() ?? '',
+            });
+        }
+    }
+
+    // Handle click the "√" button.
+    function handleConfirmBtnClick() {
+        const visibleState = editorBridgeService.isVisible();
+        if (visibleState.visible) {
+            editorBridgeService.changeVisible({
+                visible: false,
+                eventType: DeviceInputEventType.PointerDown,
+                unitId: currentWorkbook?.getUnitId() ?? '',
+            });
+        }
+    }
+
+    function handlerFxBtnClick() {
+        formulaEditorManagerService.handleFxBtnClick(true);
+    }
+
+    return (
+        <div className={styles.uniFormulaBox}>
+            <div className={styles.uniFormulaInput}>
+                <TextEditor
+                    id={DOCS_FORMULA_BAR_EDITOR_UNIT_ID_KEY}
+                    isSheetEditor={true}
+                    resizeCallBack={resizeCallBack}
+                    cancelDefaultResizeListener={true}
+                    onContextMenu={(e) => e.preventDefault()}
+                    className={styles.formulaContent}
+                    snapshot={INITIAL_SNAPSHOT}
+                    isSingle={true}
+                />
+            </div>
+            <div className={clsx(styles.formulaIcon, { [styles.formulaIconDisable]: disable })}>
+                <span
+                    className={clsx(styles.iconContainer, styles.iconContainerError)}
+                    onClick={handleCloseBtnClick}
+                >
+                    <CloseSingle />
+                </span>
+
+                <span
+                    className={clsx(styles.iconContainer, styles.iconContainerSuccess)}
+                    onClick={handleConfirmBtnClick}
+                >
+                    <CheckMarkSingle />
+                </span>
+            </div>
+            <ProgressBar barColor={progressBarColor} />
+        </div>
+    );
+}

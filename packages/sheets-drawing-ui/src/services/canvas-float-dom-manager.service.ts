0e71243d06b2072e72ab72072582559e2c3ad775
weird94 <zhang9748@foxmail.com>
2024-07-25 12:04:31 +0800

    feat: update

--- packages/sheets-drawing-ui/src/services/canvas-float-dom-manager.service.ts
+++ packages/sheets-drawing-ui/src/services/canvas-float-dom-manager.service.ts
@@ -19,7 +19,7 @@ import { Disposable, DisposableCollection, DrawingTypeEnum, ICommandService, Inj
 import type { IDrawingJsonUndo1 } from '@univerjs/drawing';
 import { getDrawingShapeKeyByDrawingSearch, IDrawingManagerService } from '@univerjs/drawing';
 import type { BaseObject, IBoundRectNoAngle, IRectProps, IRender, Scene, SpreadsheetSkeleton } from '@univerjs/engine-render';
-import { DRAWING_OBJECT_LAYER_INDEX, IRenderManagerService, Rect, SHEET_VIEWPORT_KEY } from '@univerjs/engine-render';
+import { DRAWING_OBJECT_LAYER_INDEX, IRenderManagerService, pxToNum, Rect, SHEET_VIEWPORT_KEY } from '@univerjs/engine-render';
 import type { ISetFrozenMutationParams } from '@univerjs/sheets';
 import { getSheetCommandTarget, SetFrozenMutation } from '@univerjs/sheets';
 import type { IFloatDomData, ISheetDrawingPosition, ISheetFloatDom } from '@univerjs/sheets-drawing';
@@ -134,8 +134,13 @@ const calcPosition = (
     skeleton: SpreadsheetSkeleton,
     worksheet: Worksheet
 ): IFloatDomLayout => {
-    const { scene } = currentRender;
+    const { scene, engine } = currentRender;
     const { left, top, width, height, angle } = targetObject;
+    const canvasElement = engine.getCanvasElement();
+    const canvasClientRect = canvasElement.getBoundingClientRect();
+    const widthOfCanvas = pxToNum(canvasElement.style.width); // declared width
+    const { top: topOffset, left: leftOffset, width: domWidth } = canvasClientRect; // real width affected by scale
+    const scaleAdjust = domWidth / widthOfCanvas;
 
     const bound: IBoundRectNoAngle = {
         left,
@@ -146,13 +151,13 @@ const calcPosition = (
 
     const offsetBound = transformBound2DOMBound(bound, scene, skeleton, worksheet);
     return {
-        startX: offsetBound.left,
-        endX: offsetBound.right,
-        startY: offsetBound.top,
-        endY: offsetBound.bottom,
+        startX: offsetBound.left * scaleAdjust + leftOffset,
+        endX: offsetBound.right * scaleAdjust + leftOffset,
+        startY: offsetBound.top * scaleAdjust + topOffset,
+        endY: offsetBound.bottom * scaleAdjust + topOffset,
         rotate: angle,
-        width,
-        height,
+        width: width * scaleAdjust,
+        height: height * scaleAdjust,
         absolute: offsetBound.absolute,
     };
 };

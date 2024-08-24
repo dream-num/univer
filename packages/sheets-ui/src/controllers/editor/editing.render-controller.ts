d95df53677b310fb24138d81c91346243c9648c1
ybzky <ybzkyfafa@gmail.com>
2024-08-24 19:11:04 +0800

    fix: modify the rich text judgment logic

--- packages/sheets-ui/src/controllers/editor/editing.render-controller.ts
+++ packages/sheets-ui/src/controllers/editor/editing.render-controller.ts
@@ -16,7 +16,7 @@
 
 /* eslint-disable max-lines-per-function */
 
-import type { DocumentDataModel, ICellData, ICommandInfo, IDisposable, IDocumentBody, IDocumentData, IPosition, Nullable, Workbook } from '@univerjs/core';
+import type { DocumentDataModel, ICellData, ICommandInfo, IDisposable, IDocumentBody, IDocumentData, IPosition, IStyleData, Nullable, Workbook } from '@univerjs/core';
 import {
     CellValueType, DEFAULT_EMPTY_DOCUMENT_VALUE, Direction, Disposable, DisposableCollection, DOCS_FORMULA_BAR_EDITOR_UNIT_ID_KEY, EDITOR_ACTIVATED,
     FOCUSING_EDITOR_BUT_HIDDEN,
@@ -1089,21 +1089,45 @@ export function getCellDataByInput(
         cellData.f = null;
         cellData.si = null;
         cellData.p = null;
+        const style = getCellStyleBySnapshot(snapshot);
+        if (style) {
+            cellData.s = style;
+        }
     }
 
     return cellData;
 }
 
-export function isRichText(body: IDocumentBody) {
+export function isRichText(body: IDocumentBody): boolean {
     const { textRuns = [], paragraphs = [], customRanges, customBlocks = [] } = body;
 
     const bodyNoLineBreak = body.dataStream.replace('\r\n', '');
 
     return (
-        textRuns.some((textRun) => textRun.ts && (textRun.st !== 0 || textRun.ed !== bodyNoLineBreak.length)) ||
+        textRuns.some((textRun) => textRun.ts && (textRun.ed - textRun.st < bodyNoLineBreak.length)) ||
         paragraphs.some((paragraph) => paragraph.bullet) ||
         paragraphs.length >= 2 ||
         Boolean(customRanges?.length) ||
         customBlocks.length > 0
     );
 }
+
+export function getCellStyleBySnapshot(snapshot: IDocumentData): Nullable<IStyleData> {
+    const { body } = snapshot;
+    if (!body) return null;
+    const { textRuns = [] } = body;
+
+    let style = {};
+    const bodyNoLineBreak = body.dataStream.replace('\r\n', '');
+    textRuns.forEach((textRun) => {
+        const { st, ed, ts } = textRun;
+        if (ed - st >= bodyNoLineBreak.length) {
+            style = { ...style, ...ts };
+        }
+    });
+    if (Object.keys(style).length) {
+        return style;
+    }
+    return null;
+}
+

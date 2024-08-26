02eda7db8b1762a3a712d5ab22ca2249972a873d
VicKun4937 <mincheng4937@126.com>
2024-08-26 17:51:59 +0800

    feat(sheet): add some comments

--- packages/sheets/src/basics/expand-range.ts
+++ packages/sheets/src/basics/expand-range.ts
@@ -335,7 +335,21 @@ function getExpandedRangeDown(range: IRange, allMatrixWithSpan: ObjectMatrix<IMa
         range,
     };
 }
+// the demo unit=YSvbxFMCTxugbku-IWNyxQ&type=2&subunit=U_wr1DEF84N_mbesFNmxR in pro
+// The excel behavior rules:
+// 1. If the range has a span, the range should expand to whole span range.
+// 2. If range left, right, up, down has value, the range should expand to the cell which has value.
+// 3. If the range has no value, the range should not expand.
+// 4. If the merge has span, the every cell value in span should be the anchor of the span range.
+// 5. The span range should be not part in the result range.
 
+/**
+ *  Expand the range to a continuous range, it uses when Ctrl + A , or only one cell selected to add a pivot table adn so on.
+ * @param {IRange} startRange The start range.
+ * @param {IExpandParams} directions The directions to expand.
+ * @param {Worksheet} worksheet The worksheet working on.
+ * @returns {IRange} The expanded range.
+ */
 export function expandToContinuousRange(startRange: IRange, directions: IExpandParams, worksheet: Worksheet): IRange {
     const maxRow = worksheet.getMaxRows();
     const maxColumn = worksheet.getMaxColumns();

128ef85e91407fa85bdcb8f4aa11c0962de8bc43
jocs <ransixi@gmail.com>
2024-08-27 17:51:17 +0800

    fix(sheets): do not copy cell bg color to rich text bg

--- packages/sheets/src/commands/mutations/set-range-values.mutation.ts
+++ packages/sheets/src/commands/mutations/set-range-values.mutation.ts
@@ -101,7 +101,6 @@ export const SetRangeValuesUndoMutationFactory = (
 
 /**
  * TODO@Dushusir: Excel can display numbers with up to about 15 digits of precision. When the user inputs more than 15 digits, interception is required, but there are unknown performance risks.
-
    Intercept 15-digit number reference function truncateNumber
  */
 export const SetRangeValuesMutation: IMutation<ISetRangeValuesMutationParams, boolean> = {

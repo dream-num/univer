e428b92b638a8a9c2c31a2dfb35d8c02b2d8d7de
YuHong <csuzyh@gmail.com>
2024-08-28 11:33:34 +0800

    fix: filter wont use worker

--- examples/src/sheets/lazy.ts
+++ examples/src/sheets/lazy.ts
@@ -36,7 +36,7 @@ export default function getLazyPlugins(): Array<[PluginCtor<Plugin>] | [PluginCt
 
         // ],
         [UniverSheetsCrosshairHighlightPlugin],
-        [UniverSheetsFilterUIPlugin, { useRemoteFilterValuesGenerator: true }],
+        [UniverSheetsFilterUIPlugin],
         [UniverSheetsFindReplacePlugin],
     ];
 }

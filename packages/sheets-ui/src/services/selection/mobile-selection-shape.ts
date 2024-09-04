d1773ede7979ba6e6ad5ee4da80ed55c10e20912
lumix <lumixraku@gmail.com>
2024-09-04 18:45:23 +0800

    fix: mobile selection did not drawing (#3312)

--- packages/sheets-ui/src/services/selection/mobile-selection-shape.ts
+++ packages/sheets-ui/src/services/selection/mobile-selection-shape.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import type { IRangeWithCoord, Nullable, ThemeService } from '@univerjs/core';
+import type { ThemeService } from '@univerjs/core';
 import { RANGE_TYPE } from '@univerjs/core';
 import type { BaseObject, IRectProps, Scene } from '@univerjs/engine-render';
 import { Rect, SHEET_VIEWPORT_KEY } from '@univerjs/engine-render';
@@ -49,7 +49,6 @@ export class MobileSelectionControl extends SelectionControl {
         protected _rangeType: RANGE_TYPE = RANGE_TYPE.NORMAL
     ) {
         super(_scene, _zIndex, _themeService, _highlightHeader);
-        // window.sp = this;
         this.initControlPoints();
     }
 
@@ -155,12 +154,8 @@ export class MobileSelectionControl extends SelectionControl {
         super.dispose();
     }
 
-    override updateRange(range: IRangeWithCoord): void {
-        this._selectionModel.setValue(range);
-    }
-
-    protected override _updateControlStyleAndLayout(style: Nullable<ISelectionStyle>): void {
-        super._updateControlStyleAndLayout(style);
+    protected override _setSizeAndStyleForSelectionControl(style: ISelectionStyle): void {
+        super._setSizeAndStyleForSelectionControl(style);
 
         // const rangeType = this.rangeType;
         // startX startY shares same coordinate with viewport.(include row & colheader)

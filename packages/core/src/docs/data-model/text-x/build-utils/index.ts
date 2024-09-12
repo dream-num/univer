0525071747398509718e4e2a228acc68082c103d
zw <zhang9748@foxmail.com>
2024-09-12 23:48:00 +0800

    feat: test

--- packages/core/src/docs/data-model/text-x/build-utils/index.ts
+++ packages/core/src/docs/data-model/text-x/build-utils/index.ts
@@ -15,7 +15,7 @@
  */
 
 import { copyCustomRange, getCustomRangesInterestsWithRange, isIntersecting, shouldDeleteCustomRange } from './custom-range';
-import { getDeleteSelection, getInsertSelection, getRetainAndDeleteFromReplace, getSelectionWithNoSymbolSide, getSelectionWithSymbolMax, isSegmentIntersects, makeSelection, normalizeSelection } from './selection';
+import { getDeleteSelection, getInsertSelection, getRetainAndDeleteFromReplace, isSegmentIntersects, makeSelection, normalizeSelection } from './selection';
 import { addCustomRangeTextX, deleteCustomRangeTextX, getRetainAndDeleteAndExcludeLineBreak, replaceSelectionTextX } from './text-x-utils';
 import { fromPlainText, getPlainText } from './transform';
 
@@ -36,10 +36,9 @@ export class BuildTextUtils {
 
         makeSelection,
         normalizeSelection,
-        getSelectionWithSymbolMax,
-        getSelectionWithNoSymbolSide,
         getDeleteSelection,
         getInsertSelection,
+
         getDeleteActions: getRetainAndDeleteFromReplace,
         getDeleteExculdeLastLineBreakActions: getRetainAndDeleteAndExcludeLineBreak,
     };

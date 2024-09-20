de8aa5ca0ac9753060b23f05c6062705f423fd33
jocs <ransixi@gmail.com>
2024-09-20 21:12:48 +0800

    fix: delete content with table row

--- packages/core/src/docs/data-model/text-x/build-utils/text-x-utils.ts
+++ packages/core/src/docs/data-model/text-x/build-utils/text-x-utils.ts
@@ -235,7 +235,7 @@ export function getRetainAndDeleteAndExcludeLineBreak(
     const textStart = startOffset - memoryCursor;
     const textEnd = endOffset - memoryCursor;
 
-    const paragraphInRange = paragraphs?.find(
+    const paragraphInRange = paragraphs?.findLast(
         (p) => p.startIndex - memoryCursor >= textStart && p.startIndex - memoryCursor < textEnd
     );
 

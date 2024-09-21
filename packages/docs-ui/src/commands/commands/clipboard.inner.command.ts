76f6c29b4c6a8068e85ca7137ba5cbcb9bd8a365
zw <zhang9748@foxmail.com>
2024-09-21 14:07:05 +0800

    fix: clipboard

--- packages/docs-ui/src/commands/commands/clipboard.inner.command.ts
+++ packages/docs-ui/src/commands/commands/clipboard.inner.command.ts
@@ -89,9 +89,9 @@ export const InnerPasteCommand: ICommand<IInnerPasteCommandParams> = {
         const commandService = accessor.get(ICommandService);
         const docSelectionManagerService = accessor.get(DocSelectionManagerService);
         const univerInstanceService = accessor.get(IUniverInstanceService);
-        const selections = docSelectionManagerService.getCurrentTextRanges();
+        const _selections = docSelectionManagerService.getCurrentTextRanges();
         const { body, tableSource, drawings } = doc;
-        if (!Array.isArray(selections) || selections.length === 0 || body == null) {
+        if (!Array.isArray(_selections) || _selections.length === 0 || body == null) {
             return false;
         }
 
@@ -100,7 +100,7 @@ export const InnerPasteCommand: ICommand<IInnerPasteCommandParams> = {
         if (docDataModel == null || originBody == null) {
             return false;
         }
-
+        const selections = _selections.map((range) => BuildTextUtils.selection.getInsertSelection(range, originBody));
         const unitId = docDataModel.getUnitId();
 
         const doMutation: IMutationInfo<IRichTextEditingMutationParams> = {

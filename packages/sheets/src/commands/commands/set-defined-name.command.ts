c3a18b0c3a584820128e1ced8681dd0d3e42a54f
Dushusir <1414556676@qq.com>
2024-09-21 17:02:23 +0800

    chore(sheet): add comment for set defined name

--- packages/sheets/src/commands/commands/set-defined-name.command.ts
+++ packages/sheets/src/commands/commands/set-defined-name.command.ts
@@ -26,6 +26,10 @@ import { SheetInterceptorService } from '../../services/sheet-interceptor/sheet-
 
 /**
  * The command to update defined name
+ *
+ * 1. The old defined name can be obtained through IDefinedNamesService, and does not need to be passed in from the outside, making the command input more concise
+
+   2. Unlike InsertDefinedNameCommand, the old defined name needs to be deleted here at the same time. Because the command interception in UpdateDefinedNameController will add SetDefinedNameMutation or RemoveDefinedNameMutation, it results in that in DefinedNameController, only mutations can be listened to to update Function Description (commands cannot be listened to), so it is necessary to ensure that each mutation triggered by the command has completed all work.
  */
 export const SetDefinedNameCommand: ICommand = {
     id: 'sheet.command.set-defined-name',

c2ebc40fc9cc586329df2fcaf8b420fd2ac1fbfa
白熱 <sonne@asaki.me>
2024-09-23 20:54:37 +0800

    chore: fix eslint

--- packages/sheets-data-validation/src/types/index.ts
+++ packages/sheets-data-validation/src/types/index.ts
@@ -14,5 +14,5 @@
  * limitations under the License.
  */
 
-export * from './interface';
 export * from './const';
+export * from './interface';
--- packages/sheets/src/services/permission/permission-point/index.ts
+++ packages/sheets/src/services/permission/permission-point/index.ts
@@ -14,44 +14,44 @@
  * limitations under the License.
  */
 
+export { RangeProtectionPermissionEditPoint } from './range/edit';
+export { RangeProtectionPermissionViewPoint } from './range/view';
+export { WorkbookCommentPermission } from './workbook/comment';
+export { WorkbookCopyPermission } from './workbook/copy';
+export { WorkbookCopySheetPermission } from './workbook/copy-sheet';
+export { WorkbookCreateSheetPermission } from './workbook/create-sheet';
+export { WorkbookDeleteSheetPermission } from './workbook/delete-sheet';
+export { WorkbookDuplicatePermission } from './workbook/duplicate';
+export { WorkbookEditablePermission } from './workbook/editable';
+export { WorkbookExportPermission } from './workbook/export';
+export { WorkbookHideSheetPermission } from './workbook/hide-sheet';
+export { WorkbookHistoryPermission } from './workbook/history';
+export { WorkbookManageCollaboratorPermission } from './workbook/manage-collaborator';
+export { WorkbookMoveSheetPermission } from './workbook/move-sheet';
+export { WorkbookPrintPermission } from './workbook/print';
+export { WorkbookRecoverHistoryPermission } from './workbook/recover-history';
+export { WorkbookRenameSheetPermission } from './workbook/rename-sheet';
+export { WorkbookSharePermission } from './workbook/share';
+export { WorkbookViewPermission } from './workbook/view';
+
+export { WorkbookViewHistoryPermission } from './workbook/view-history';
 export { WorksheetCopyPermission } from './worksheet/copy';
+export { WorksheetDeleteColumnPermission } from './worksheet/delete-column';
+export { WorksheetDeleteRowPermission } from './worksheet/delete-row';
+export { WorksheetEditPermission } from './worksheet/edit';
+export { WorksheetEditExtraObjectPermission } from './worksheet/edit-extra-object';
+export { WorksheetFilterPermission } from './worksheet/filter';
+export { WorksheetInsertColumnPermission } from './worksheet/insert-column';
+export { WorksheetInsertHyperlinkPermission } from './worksheet/insert-hyperlink';
+export { WorksheetInsertRowPermission } from './worksheet/insert-row';
+export { WorksheetManageCollaboratorPermission } from './worksheet/manage-collaborator';
+export { WorksheetPivotTablePermission } from './worksheet/pivot-table';
 export { WorksheetSelectProtectedCellsPermission } from './worksheet/select-protected-cells';
 export { WorksheetSelectUnProtectedCellsPermission } from './worksheet/select-un-protected-cells';
 export { WorksheetSetCellStylePermission } from './worksheet/set-cell-style';
 export { WorksheetSetCellValuePermission } from './worksheet/set-cell-value';
-export { WorksheetViewPermission } from './worksheet/view';
-export { WorksheetSetRowStylePermission } from './worksheet/set-row-style';
 export { WorksheetSetColumnStylePermission } from './worksheet/set-column-style';
-export { WorksheetInsertRowPermission } from './worksheet/insert-row';
-export { WorksheetInsertColumnPermission } from './worksheet/insert-column';
-export { WorksheetInsertHyperlinkPermission } from './worksheet/insert-hyperlink';
-export { WorksheetDeleteRowPermission } from './worksheet/delete-row';
-export { WorksheetDeleteColumnPermission } from './worksheet/delete-column';
+export { WorksheetSetRowStylePermission } from './worksheet/set-row-style';
+
 export { WorksheetSortPermission } from './worksheet/sort';
-export { WorksheetFilterPermission } from './worksheet/filter';
-export { WorksheetPivotTablePermission } from './worksheet/pivot-table';
-export { WorksheetEditExtraObjectPermission } from './worksheet/edit-extra-object';
-export { WorksheetManageCollaboratorPermission } from './worksheet/manage-collaborator';
-export { WorksheetEditPermission } from './worksheet/edit';
-
-export { WorkbookCommentPermission } from './workbook/comment';
-export { WorkbookEditablePermission } from './workbook/editable';
-export { WorkbookDuplicatePermission } from './workbook/duplicate';
-export { WorkbookPrintPermission } from './workbook/print';
-export { WorkbookExportPermission } from './workbook/export';
-export { WorkbookMoveSheetPermission } from './workbook/move-sheet';
-export { WorkbookDeleteSheetPermission } from './workbook/delete-sheet';
-export { WorkbookHideSheetPermission } from './workbook/hide-sheet';
-export { WorkbookRenameSheetPermission } from './workbook/rename-sheet';
-export { WorkbookCreateSheetPermission } from './workbook/create-sheet';
-export { WorkbookHistoryPermission } from './workbook/history';
-export { WorkbookViewPermission } from './workbook/view';
-export { WorkbookSharePermission } from './workbook/share';
-export { WorkbookCopyPermission } from './workbook/copy';
-export { WorkbookCopySheetPermission } from './workbook/copy-sheet';
-export { WorkbookManageCollaboratorPermission } from './workbook/manage-collaborator';
-export { WorkbookViewHistoryPermission } from './workbook/view-history';
-export { WorkbookRecoverHistoryPermission } from './workbook/recover-history';
-
-export { RangeProtectionPermissionEditPoint } from './range/edit';
-export { RangeProtectionPermissionViewPoint } from './range/view';
+export { WorksheetViewPermission } from './worksheet/view';

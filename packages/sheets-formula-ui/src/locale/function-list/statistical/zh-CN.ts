2e6fa712e78ee1621eaa27d6654b388acee46a9e
wpxp123456 <Wpxp1223456>
2024-09-28 21:16:23 +0800

    feat(formula): add mode.mult/mode.sngl function

--- packages/sheets-formula-ui/src/locale/function-list/statistical/zh-CN.ts
+++ packages/sheets-formula-ui/src/locale/function-list/statistical/zh-CN.ts
@@ -1011,8 +1011,8 @@ export default {
             },
         ],
         functionParameter: {
-            number1: { name: 'number1', detail: 'first' },
-            number2: { name: 'number2', detail: 'second' },
+            number1: { name: '数值 1', detail: '要计算众数的第一个数字、单元格引用或单元格区域。' },
+            number2: { name: '数值 2', detail: '要计算众数的其他数字、单元格引用或单元格区域，最多可包含 255 个。' },
         },
     },
     MODE_SNGL: {
@@ -1025,8 +1025,8 @@ export default {
             },
         ],
         functionParameter: {
-            number1: { name: 'number1', detail: 'first' },
-            number2: { name: 'number2', detail: 'second' },
+            number1: { name: '数值 1', detail: '要计算众数的第一个数字、单元格引用或单元格区域。' },
+            number2: { name: '数值 2', detail: '要计算众数的其他数字、单元格引用或单元格区域，最多可包含 255 个。' },
         },
     },
     NEGBINOM_DIST: {

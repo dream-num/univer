f3cc6d9c6d233b59fd16025d7c302320f5e2d0c3
zhangw <zhang9748@foxmail.com>
2024-09-29 21:57:30 +0800

    fix: remove useless mark dirty

--- packages/sheets-hyper-link-ui/src/plugin.ts
+++ packages/sheets-hyper-link-ui/src/plugin.ts
@@ -15,26 +15,25 @@
  */
 
 import type { Dependency } from '@univerjs/core';
+import type { IUniverSheetsHyperLinkUIConfig } from './controllers/config.schema';
 import { DependentOn, IConfigService, Inject, Injector, Plugin, UniverInstanceType } from '@univerjs/core';
-import { UniverSheetsHyperLinkPlugin } from '@univerjs/sheets-hyper-link';
-import { IRenderManagerService } from '@univerjs/engine-render';
 import { UniverDocsUIPlugin } from '@univerjs/docs-ui';
+import { UniverSheetsHyperLinkPlugin } from '@univerjs/sheets-hyper-link';
+import { SheetsHyperLinkAutoFillController } from './controllers/auto-fill.controller';
+import { defaultPluginConfig, PLUGIN_CONFIG_KEY } from './controllers/config.schema';
+import { SheetsHyperLinkCopyPasteController } from './controllers/copy-paste.controller';
+import { SheetsHyperLinkPermissionController } from './controllers/hyper-link-permission.controller';
+import { SheetsHyperLinkPopupController } from './controllers/popup.controller';
 import { SheetsHyperLinkRemoveSheetController } from './controllers/remove-sheet.controller';
-import { SheetsHyperLinkRenderController, SheetsHyperLinkRenderManagerController } from './controllers/render-controllers/render.controller';
+import { SheetsHyperLinkRenderManagerController } from './controllers/render-controllers/render.controller';
+import { SheetsHyperLinkRichTextRefRangeController } from './controllers/rich-text-ref-range.controller';
+import { SheetHyperLinkSetRangeController } from './controllers/set-range.controller';
+import { SheetsHyperLinkUIController } from './controllers/ui.controller';
+import { SheetHyperLinkUrlController } from './controllers/url.controller';
 import { SheetsHyperLinkPopupService } from './services/popup.service';
 import { SheetsHyperLinkResolverService } from './services/resolver.service';
-import { SheetHyperLinkSetRangeController } from './controllers/set-range.controller';
-import { SheetsHyperLinkPopupController } from './controllers/popup.controller';
-import { SheetsHyperLinkUIController } from './controllers/ui.controller';
-import { SHEET_HYPER_LINK_UI_PLUGIN } from './types/const';
-import { SheetsHyperLinkAutoFillController } from './controllers/auto-fill.controller';
-import { SheetsHyperLinkCopyPasteController } from './controllers/copy-paste.controller';
-import { SheetHyperLinkUrlController } from './controllers/url.controller';
-import { SheetsHyperLinkPermissionController } from './controllers/hyper-link-permission.controller';
 import { SheetsHyperLinkSidePanelService } from './services/side-panel.service';
-import type { IUniverSheetsHyperLinkUIConfig } from './controllers/config.schema';
-import { defaultPluginConfig, PLUGIN_CONFIG_KEY } from './controllers/config.schema';
-import { SheetsHyperLinkRichTextRefRangeController } from './controllers/rich-text-ref-range.controller';
+import { SHEET_HYPER_LINK_UI_PLUGIN } from './types/const';
 
 @DependentOn(UniverSheetsHyperLinkPlugin, UniverDocsUIPlugin)
 export class UniverSheetsHyperLinkUIPlugin extends Plugin {
@@ -44,7 +43,6 @@ export class UniverSheetsHyperLinkUIPlugin extends Plugin {
     constructor(
         private readonly _config: Partial<IUniverSheetsHyperLinkUIConfig> = defaultPluginConfig,
         @Inject(Injector) protected override _injector: Injector,
-        @IRenderManagerService private readonly _renderManagerService: IRenderManagerService,
         @IConfigService private readonly _configService: IConfigService
     ) {
         super();
@@ -76,13 +74,5 @@ export class UniverSheetsHyperLinkUIPlugin extends Plugin {
         ];
 
         dependencies.forEach((dep) => this._injector.add(dep));
-    }
-
-    override onReady(): void {
-        const renderDependencies: Dependency[] = [
-            [SheetsHyperLinkRenderController],
-        ];
-
-        renderDependencies.forEach((d) => this._renderManagerService.registerRenderModule(UniverInstanceType.UNIVER_SHEET, d));
     }
 }

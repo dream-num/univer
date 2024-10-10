e36529acf9dbcb98f1eb9c92a133ff3755a94463
lumixraku <lumixraku@gmail.com>
2024-10-10 11:36:13 +0800

    test: visual e2e test after scrolling (aim for merged cells)

--- e2e/visual-comparison/sheets/sheets-visual-comparison.spec.ts
+++ e2e/visual-comparison/sheets/sheets-visual-comparison.spec.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { expect, test } from '@playwright/test';
+import { chromium, expect, test } from '@playwright/test';
 import { generateSnapshotName } from '../const';
 
 test('diff default sheet content', async ({ page }) => {
@@ -39,8 +39,93 @@ test('diff demo sheet content', async ({ page }) => {
     await page.waitForTimeout(2000);
 
     await page.evaluate(() => window.E2EControllerAPI.loadDemoSheet());
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(2000);
 
     await expect(page).toHaveScreenshot(generateSnapshotName('demo-sheet'), { maxDiffPixels: 5 });
     expect(errored).toBeFalsy();
+});
+
+/**
+ * Aim for merged cells rendering.
+ */
+test('diff merged cells rendering', async () => {
+    const browser = await chromium.launch({
+        headless: true, //!!CI, // Set to false to see the browser window
+    });
+    const context = await browser.newContext({
+        viewport: { width: 1280, height: 1280 },
+        deviceScaleFactor: 1, // Set your desired DPR
+    });
+    const page = await context.newPage();
+    await page.goto('http://localhost:3000/sheets/');
+    await page.waitForTimeout(2000);
+
+    await page.evaluate(() => window.E2EControllerAPI.loadMergeCellSheet());
+    await page.waitForTimeout(2000);
+
+    await expect(page).toHaveScreenshot(generateSnapshotName('mergedCellsRendering'), { maxDiffPixels: 5 });
+
+    await browser.close();
+});
+const isCI = !!process.env.CI;
+test('diff merged cells rendering after scrolling', async ({}) => {
+    const browser = await chromium.launch({
+        headless: true, //!!isCI, // Set to false to see the browser window in local
+    });
+    const context = await browser.newContext({
+        viewport: { width: 1280, height: 1280 },
+        deviceScaleFactor: 1, // Set your desired DPR
+    });
+    const page = await context.newPage();
+    await page.goto('http://localhost:3000/sheets/');
+    await page.waitForTimeout(2000);
+
+    await page.evaluate(() => window.E2EControllerAPI.loadMergeCellSheet());
+    await page.waitForTimeout(2000);
+
+    await page.evaluate(async () => {
+        const dispatchWheelEvent = (deltaX: number, deltaY: number, element: HTMLElement, interval: number = 30, lastFor: number = 1000) => {
+            // const canvasElements = document.querySelectorAll('canvas.univer-render-canvas') as unknown as HTMLElement[];
+            // const filteredCanvasElements = Array.from(canvasElements).filter((canvas) => canvas.offsetHeight > 500);
+
+            const dispatchSimulateWheelEvent = (element) => {
+                const event = new WheelEvent('wheel', {
+                    bubbles: true,
+                    cancelable: true,
+                    deltaY,
+                    deltaX,
+                    clientX: 580,
+                    clientY: 580,
+                });
+                element.dispatchEvent(event);
+            };
+
+            // mock wheel event.
+            let intervalID;
+            const continuousWheelSimulation = (element, interval) => {
+                intervalID = setInterval(function () {
+                    dispatchSimulateWheelEvent(element);
+                }, interval);
+            };
+
+            // start mock wheel event.
+            continuousWheelSimulation(element, interval);
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    clearInterval(intervalID);
+                    resolve(1);
+                }, lastFor);
+            });
+        };
+        const canvasElements = document.querySelectorAll('canvas.univer-render-canvas') as unknown as HTMLElement[];
+        const filteredCanvasElements = Array.from(canvasElements).filter((canvas) => canvas.offsetHeight > 500);
+        const element = filteredCanvasElements[0];
+        await dispatchWheelEvent(0, 100, element);
+        await dispatchWheelEvent(0, -100, element);
+    });
+    await page.waitForTimeout(4000);
+
+    await expect(page).toHaveScreenshot(generateSnapshotName('mergedCellsRenderingScrolling'), { maxDiffPixels: 5 });
+
+    await browser.close();
 });

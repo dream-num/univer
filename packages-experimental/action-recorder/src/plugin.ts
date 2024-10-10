7759b35bd3d0d063eee82a6ac013501fc6695f64
YuHong <csuzyh@gmail.com>
2024-10-10 18:21:37 +0800

    feat: add replay-only mode to actionRecordPlugin

--- packages-experimental/action-recorder/src/plugin.ts
+++ packages-experimental/action-recorder/src/plugin.ts
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-import { IConfigService, Inject, Injector, Plugin } from '@univerjs/core';
 import type { Dependency } from '@univerjs/core';
+import type { IUniverActionRecorderConfig } from './controllers/config.schema';
+import { IConfigService, Inject, Injector, Plugin } from '@univerjs/core';
 import { ActionRecorderController } from './controllers/action-recorder.controller';
 import { defaultPluginConfig, PLUGIN_CONFIG_KEY } from './controllers/config.schema';
 import { ActionRecorderService } from './services/action-recorder.service';
 import { ActionReplayService } from './services/replay.service';
-import type { IUniverActionRecorderConfig } from './controllers/config.schema';
 
 /**
  * This plugin provides a recorder for user's interactions with Univer,
@@ -53,6 +53,9 @@ export class UniverActionRecorderPlugin extends Plugin {
     }
 
     override onSteady(): void {
+        if (this._config.replayOnly) {
+            return;
+        }
         this._injector.get(ActionRecorderController);
     }
 }

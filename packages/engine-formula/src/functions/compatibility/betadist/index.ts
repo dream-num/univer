4acfab96d003894a490269ce12f7d6c6da403676
wpxp123456 <Wpxp1223456>
2024-10-10 20:19:34 +0800

    fix(formula): fix bug

--- packages/engine-formula/src/functions/compatibility/betadist/index.ts
+++ packages/engine-formula/src/functions/compatibility/betadist/index.ts
@@ -22,7 +22,6 @@ import { checkVariantsErrorIsStringToNumber } from '../../../engine/utils/check-
 import { type BaseValueObject, ErrorValueObject } from '../../../engine/value-object/base-value-object';
 import { NumberValueObject } from '../../../engine/value-object/primitive-object';
 import { BaseFunction } from '../../base-function';
-import type { ArrayValueObject } from '../../../engine/value-object/array-value-object';
 
 export class Betadist extends BaseFunction {
     override minParams = 3;

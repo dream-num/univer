7ed42126154f1c08af8eabffce9deb65a3efaaab
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2024-10-11 19:13:38 +0800

    chore(node-sdk): add computing process

--- examples-node/src/sdk/index.ts
+++ examples-node/src/sdk/index.ts
@@ -14,12 +14,17 @@
  * limitations under the License.
  */
 
+import type { IMessageProtocol } from '@univerjs/rpc';
+import type { Serializable } from 'node:child_process';
+import { fork } from 'node:child_process';
+import path from 'node:path';
 import { Univer } from '@univerjs/core';
 import { UniverDataValidationPlugin } from '@univerjs/data-validation';
 import { UniverDocsPlugin } from '@univerjs/docs';
 import { UniverDocsDrawingPlugin } from '@univerjs/docs-drawing';
 import { UniverDrawingPlugin } from '@univerjs/drawing';
 import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula';
+import { UniverRPCNodeMainPlugin } from '@univerjs/rpc-node';
 import { UniverSheetsPlugin } from '@univerjs/sheets';
 import { UniverSheetsConditionalFormattingPlugin } from '@univerjs/sheets-conditional-formatting';
 import { UniverSheetsDataValidationPlugin } from '@univerjs/sheets-data-validation';
@@ -28,12 +33,15 @@ import { UniverSheetsFilterPlugin } from '@univerjs/sheets-filter';
 import { UniverSheetsHyperLinkPlugin } from '@univerjs/sheets-hyper-link';
 import { UniverSheetsSortPlugin } from '@univerjs/sheets-sort';
 import { UniverThreadCommentPlugin } from '@univerjs/thread-comment';
+import { Observable, shareReplay } from 'rxjs';
 
 export function createUniverOnNode(): Univer {
     const univer = new Univer();
 
     registerBasicPlugins(univer);
     reigsterSharedPlugins(univer);
+    registerRPCPlugin(univer);
+
     registerDocPlugins(univer);
     registerSheetPlugins(univer);
 
@@ -63,4 +71,23 @@ function registerSheetPlugins(univer: Univer): void {
     univer.registerPlugin(UniverSheetsHyperLinkPlugin);
     univer.registerPlugin(UniverSheetsDrawingPlugin);
     univer.registerPlugin(UniverSheetsSortPlugin);
+}
+
+function registerRPCPlugin(univer: Univer): void {
+    const childFork = fork(path.join(__dirname, './worker.js'));
+    const messageProtocol: IMessageProtocol = {
+        send(message: unknown): void {
+            childFork.send(message as Serializable);
+        },
+        onMessage: new Observable<any>((subscriber) => {
+            const handler = (message: unknown) => {
+                subscriber.next(message);
+            };
+
+            childFork.on('message', handler);
+            return () => childFork.off('message', handler);
+        }).pipe(shareReplay(1)),
+    };
+
+    univer.registerPlugin(UniverRPCNodeMainPlugin, { messageProtocol });
 }

c50742c5c0aed457d776cd54297f25b9b361bafa
zhangw <zhang9748@foxmail.com>
2024-10-11 15:46:15 +0800

    fix: editor resize

--- packages/sheets-ui/src/controllers/render-controllers/editor-bridge.render-controller.ts
+++ packages/sheets-ui/src/controllers/render-controllers/editor-bridge.render-controller.ts
@@ -14,24 +14,23 @@
  * limitations under the License.
  */
 
+import type { ICommandInfo, IDisposable, IExecutionOptions, Nullable, Workbook } from '@univerjs/core';
+import type { IRenderContext, IRenderModule } from '@univerjs/engine-render';
+import type { ISelectionWithStyle } from '@univerjs/sheets';
+import type { ICurrentEditCellParam, IEditorBridgeServiceVisibleParam } from '../../services/editor-bridge.service';
 import { DisposableCollection, ICommandService, Inject, IUniverInstanceService, RxDisposable, UniverInstanceType } from '@univerjs/core';
 import { IRangeSelectorService } from '@univerjs/docs-ui';
 import { DeviceInputEventType } from '@univerjs/engine-render';
 import {
-    COMMAND_LISTENER_SKELETON_CHANGE,
     SetWorksheetActiveOperation,
     SheetsSelectionsService,
 } from '@univerjs/sheets';
 import { merge } from 'rxjs';
-import type { ICommandInfo, IDisposable, IExecutionOptions, Nullable, Workbook } from '@univerjs/core';
-import type { IRenderContext, IRenderModule } from '@univerjs/engine-render';
-import type { ISelectionWithStyle } from '@univerjs/sheets';
 import { SetZoomRatioCommand } from '../../commands/commands/set-zoom-ratio.command';
 import { SetActivateCellEditOperation } from '../../commands/operations/activate-cell-edit.operation';
 import { SetCellEditVisibleOperation } from '../../commands/operations/cell-edit.operation';
 import { IEditorBridgeService } from '../../services/editor-bridge.service';
 import { getSheetObject } from '../utils/component-tools';
-import type { ICurrentEditCellParam, IEditorBridgeServiceVisibleParam } from '../../services/editor-bridge.service';
 
 // TODO: wzhudev: this should be merged with Edit EditingRenderController.
 
@@ -207,10 +206,10 @@ export class EditorBridgeRenderController extends RxDisposable implements IRende
                 this._editorBridgeService.refreshEditCellState();
             }
 
-            if (options?.fromCollab) return;
-            if (command.id !== SetWorksheetActiveOperation.id && COMMAND_LISTENER_SKELETON_CHANGE.includes(command.id)) {
-                this._hideEditor();
-            }
+            // if (options?.fromCollab) return;
+            // if (command.id !== SetWorksheetActiveOperation.id && COMMAND_LISTENER_SKELETON_CHANGE.includes(command.id)) {
+            //     this._hideEditor();
+            // }
         }));
 
         d.add(this._commandService.beforeCommandExecuted((command: ICommandInfo, options?: IExecutionOptions) => {

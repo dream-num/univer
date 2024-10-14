f3182db1477deb3d120782a727f1c5e45528d8d9
ybzky <ybzkyfafa@gmail.com>
2024-10-14 20:35:20 +0800

    chore: bugfix

--- packages/engine-render/src/context.ts
+++ packages/engine-render/src/context.ts
@@ -438,10 +438,10 @@ export class UniverRenderingContext2D implements CanvasRenderingContext2D {
         this._context.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
     }
 
-     /**
-      * bezierCurveTo function precision.
-      * @method
-      */
+    /**
+     * bezierCurveTo function precision.
+     * @method
+     */
     bezierCurveToByPrecision(cp1x: number, cp1y: number, cp2x: number, cp2y: number, x: number, y: number) {
         const { scaleX, scaleY } = this._getScale();
         x = fixLineWidthByScale(x, scaleX);
@@ -673,10 +673,10 @@ export class UniverRenderingContext2D implements CanvasRenderingContext2D {
         this._context.fillRect(x, y, width, height);
     }
 
-     /**
-      * fillRect function precision.
-      * @method
-      */
+    /**
+     * fillRect function precision.
+     * @method
+     */
     fillRectByPrecision(x: number, y: number, width: number, height: number) {
         const { scaleX, scaleY } = this._getScale();
         x = fixLineWidthByScale(x, scaleX);
@@ -1009,6 +1009,15 @@ export class UniverRenderingContextWebGL { }
 export class UniverRenderingContextWebGPU { }
 
 export class UniverRenderingContext extends UniverRenderingContext2D {
+    private _id: string;
+
+    getId() {
+        return this._id;
+    }
+
+    setId(id: string) {
+        this._id = id;
+    }
 }
 
 export class UniverPrintingContext extends UniverRenderingContext2D {

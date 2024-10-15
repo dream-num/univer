2031d01627401a4382d1baafd2a84b1aec7ee4c0
lumixraku <lumixraku@gmail.com>
2024-10-15 11:21:51 +0800

    fix: font render bounds

--- packages/engine-render/src/components/sheets/extensions/font.ts
+++ packages/engine-render/src/components/sheets/extensions/font.ts
@@ -150,8 +150,8 @@ export class Font extends SheetExtension {
         ctx.restore();
     }
 
-    renderFontEachCell(renderFontContext: IRenderFontContext, row: number, col: number, fontMatrix: ObjectMatrix<IFontCacheItem>) {
-        const { ctx, viewRanges, diffRanges, spreadsheetSkeleton, cellInfo } = renderFontContext;
+    renderFontEachCell(renderFontCtx: IRenderFontContext, row: number, col: number, fontMatrix: ObjectMatrix<IFontCacheItem>) {
+        const { ctx, viewRanges, diffRanges, spreadsheetSkeleton, cellInfo } = renderFontCtx;
 
         //#region merged cell
         let { startY, endY, startX, endX } = cellInfo;
@@ -203,23 +203,30 @@ export class Font extends SheetExtension {
         ctx.beginPath();
 
         //#region text overflow
-        renderFontContext.overflowRectangle = overflowRange;
-        renderFontContext.cellData = cellData;
-        renderFontContext.startX = startX;
-        renderFontContext.startY = startY;
-        renderFontContext.endX = endX;
-        renderFontContext.endY = endY;
-        this._clipTextOverflow(renderFontContext, row, col, fontMatrix);
+        renderFontCtx.overflowRectangle = overflowRange;
+        renderFontCtx.cellData = cellData;
+        renderFontCtx.startX = startX;
+        renderFontCtx.startY = startY;
+        renderFontCtx.endX = endX;
+        renderFontCtx.endY = endY;
+        this._setFontRenderBounds(renderFontCtx, row, col, fontMatrix);
         //#endregion
 
         ctx.translate(startX + FIX_ONE_PIXEL_BLUR_OFFSET, startY + FIX_ONE_PIXEL_BLUR_OFFSET);
-        this._renderDocuments(ctx, fontsConfig, startX, startY, endX, endY, row, col, spreadsheetSkeleton.overflowCache);
+        this._renderDocuments(ctx, fontsConfig, renderFontCtx.startX, renderFontCtx.startY, renderFontCtx.endX, renderFontCtx.endY, row, col, spreadsheetSkeleton.overflowCache);
 
         ctx.closePath();
         ctx.restore();
     };
 
-    private _clipTextOverflow(renderFontContext: IRenderFontContext, row: number, col: number, fontMatrix: ObjectMatrix<IFontCacheItem>) {
+    /**
+     * Change font render bounds, for overflow and filter icon & custom render.
+     * @param renderFontContext
+     * @param row
+     * @param col
+     * @param fontMatrix
+     */
+    private _setFontRenderBounds(renderFontContext: IRenderFontContext, row: number, col: number, fontMatrix: ObjectMatrix<IFontCacheItem>) {
         const { ctx, scale, overflowRectangle, rowHeightAccumulation, columnWidthAccumulation, cellData } = renderFontContext;
         let { startX, endX, startY, endY } = renderFontContext;
 
@@ -305,6 +312,10 @@ export class Font extends SheetExtension {
             // for normal cell, forbid text overflow cellarea
             ctx.clip();
         }
+        renderFontContext.startX = startX;
+        renderFontContext.startY = startY;
+        renderFontContext.endX = endX;
+        renderFontContext.endY = endY;
     }
 
     private _renderDocuments(

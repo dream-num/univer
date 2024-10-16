af6af6c70653af66b3f1cf77bb584c26d3b38ed8
Dushusir <1414556676@qq.com>
2024-10-16 09:35:43 +0000

    fix(sheet): conflict error

--- packages/sheets/src/commands/commands/__tests__/set-row-custom.command.spec.ts
+++ packages/sheets/src/commands/commands/__tests__/set-row-custom.command.spec.ts
@@ -21,22 +21,6 @@ import { SetRowCustomMutation } from '../../mutations/set-row-custom.mutation';
 import { SetRowCustomCommand } from '../set-row-custom.command';
 import { createCommandTestBed } from './create-command-test-bed';
 
-/**
- * Copyright 2023-present DreamNum Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 describe('test set row custom commands', () => {
     let univer: Univer;
     let get: Injector['get'];

0aab33bb8c2c791c88a1c5a45454ac808c6e980f
ybzky <ybzkyfafa@gmail.com>
2024-10-17 17:04:57 +0800

    chore: add menu hidden mark

--- packages/watermark/src/controllers/menu.ts
+++ packages/watermark/src/controllers/menu.ts
@@ -15,44 +15,17 @@
  */
 
 import type { IMenuButtonItem } from '@univerjs/ui';
-import { DOCS_ZEN_EDITOR_UNIT_ID_KEY, type IAccessor, IUniverInstanceService } from '@univerjs/core';
 import { MenuItemType } from '@univerjs/ui';
-import { of, switchMap } from 'rxjs';
 import { OpenWatermarkPanelOperation } from '../commands/operations/OpenWatermarkPanelOperation';
 import { UNIVER_WATERMARK_MENU } from '../common/const';
-import { UniverWatermarkService } from '../services/watermarkService';
 
-export function WatermarkMenuItemFactory(accessor: IAccessor): IMenuButtonItem {
+export function WatermarkMenuItemFactory(): IMenuButtonItem {
     return {
         id: OpenWatermarkPanelOperation.id,
         title: 'univer-watermark.title',
         tooltip: 'univer-watermark.title',
         icon: UNIVER_WATERMARK_MENU,
         type: MenuItemType.BUTTON,
-        hidden$: getWatermarkMenuHiddenObservable(accessor),
     };
-}
-
-function getWatermarkMenuHiddenObservable(accessor: IAccessor) {
-    const univerWatermarkService = accessor.get(UniverWatermarkService);
-    // return univerWatermarkService.menuHidden$.pipe(map((hidden) => hidden));
-
-    return univerWatermarkService.menuHidden$.pipe(
-        switchMap((menuHidden) => {
-            if (menuHidden) {
-                return of(true);
-            }
-            const univerInstanceService = accessor.get(IUniverInstanceService);
-            return univerInstanceService.focused$.pipe(
-                switchMap((id) => {
-                    if (id === DOCS_ZEN_EDITOR_UNIT_ID_KEY) {
-                        return of(true);
-                    } else {
-                        return of(false);
-                    }
-                })
-            );
-        })
-    );
 }
 

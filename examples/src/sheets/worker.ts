8c755e2c4cf65dc21f64e3153d38d97524c9c48b
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2024-10-29 11:58:27 +0800

    chore: temply remove filter ui from worker

--- examples/src/sheets/worker.ts
+++ examples/src/sheets/worker.ts
@@ -18,7 +18,6 @@ import { LocaleType, LogLevel, Univer } from '@univerjs/core';
 import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula';
 import { UniverRPCWorkerThreadPlugin } from '@univerjs/rpc';
 import { UniverSheetsPlugin } from '@univerjs/sheets';
-import { UniverSheetsFilterUIWorkerPlugin } from '@univerjs/sheets-filter-ui';
 import { UniverRemoteSheetsFormulaPlugin } from '@univerjs/sheets-formula';
 import { enUS, faIR, ruRU, viVN, zhCN, zhTW } from '../locales';
 
@@ -40,7 +39,6 @@ univer.registerPlugin(UniverSheetsPlugin, { onlyRegisterFormulaRelatedMutations:
 univer.registerPlugin(UniverFormulaEnginePlugin);
 univer.registerPlugin(UniverRPCWorkerThreadPlugin);
 univer.registerPlugin(UniverRemoteSheetsFormulaPlugin);
-univer.registerPlugin(UniverSheetsFilterUIWorkerPlugin);
 
 declare let self: WorkerGlobalScope & typeof globalThis & { univer: Univer };
 self.univer = univer;

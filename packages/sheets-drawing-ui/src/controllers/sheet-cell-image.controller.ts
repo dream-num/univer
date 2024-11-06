4be77cf4799cb4f04a3b43843ee9367bae21da2f
zhangw <zhang9748@foxmail.com>
2024-11-06 18:45:53 +0800

    feat: update

--- packages/sheets-drawing-ui/src/controllers/sheet-cell-image.controller.ts
+++ packages/sheets-drawing-ui/src/controllers/sheet-cell-image.controller.ts
@@ -14,139 +14,20 @@
  * limitations under the License.
  */
 
-import type { ICellData, IDisposable, IDocDrawingBase, Nullable, UnitModel, Workbook } from '@univerjs/core';
+import type { IDocDrawingBase, Nullable } from '@univerjs/core';
 import type { IImageData } from '@univerjs/drawing';
-import type { ISetRangeValuesMutationParams, ISetWorksheetColWidthMutationParams, ISetWorksheetRowAutoHeightMutationParams, ISetWorksheetRowHeightMutationParams, ISetWorksheetRowIsAutoHeightMutationParams } from '@univerjs/sheets';
-import { Disposable, DOCS_NORMAL_EDITOR_UNIT_ID_KEY, DOCS_ZEN_EDITOR_UNIT_ID_KEY, ICommandService, Inject, Injector, IUniverInstanceService, ObjectMatrix, toDisposable, UniverInstanceType } from '@univerjs/core';
+import type { ISetWorksheetColWidthMutationParams, ISetWorksheetRowAutoHeightMutationParams, ISetWorksheetRowHeightMutationParams, ISetWorksheetRowIsAutoHeightMutationParams } from '@univerjs/sheets';
+import { Disposable, DOCS_NORMAL_EDITOR_UNIT_ID_KEY, DOCS_ZEN_EDITOR_UNIT_ID_KEY, ICommandService, Inject, Injector, IUniverInstanceService, UniverInstanceType } from '@univerjs/core';
 import { DocDrawingController } from '@univerjs/docs-drawing';
 import { ReplaceSnapshotCommand } from '@univerjs/docs-ui';
-import { IDrawingManagerService, IImageIoService, ImageSourceType } from '@univerjs/drawing';
+import { IDrawingManagerService, IImageIoService } from '@univerjs/drawing';
 import { IRenderManagerService } from '@univerjs/engine-render';
-import { AFTER_CELL_EDIT, getSheetCommandTarget, INTERCEPTOR_POINT, RefRangeService, SetRangeValuesMutation, SetWorksheetColWidthMutation, SetWorksheetRowAutoHeightMutation, SetWorksheetRowHeightMutation, SetWorksheetRowIsAutoHeightMutation, SheetInterceptorService } from '@univerjs/sheets';
+import { AFTER_CELL_EDIT, getSheetCommandTarget, INTERCEPTOR_POINT, RefRangeService, SetWorksheetColWidthMutation, SetWorksheetRowAutoHeightMutation, SetWorksheetRowHeightMutation, SetWorksheetRowIsAutoHeightMutation, SheetInterceptorService } from '@univerjs/sheets';
 import { IEditorBridgeService } from '@univerjs/sheets-ui';
+import { SheetCellCacheManagerService } from '../services/sheet-cell-cache-manager.service';
 import { getDrawingSizeByCell } from './sheet-drawing-update.controller';
 
-interface IImageCache {
-    image: HTMLImageElement;
-    disposable: IDisposable;
-    row: number;
-    col: number;
-    key: string;
-    status: 'loading' | 'loaded' | 'error';
-}
-
-function makeImageRecord(imageCache: IImageCache[]) {
-    const record: Record<string, HTMLImageElement> = {};
-
-    imageCache.forEach((item) => {
-        record[item.key] = item.image;
-    });
-
-    return record;
-}
-
-function makeRecord(imageCache: IImageCache[]) {
-    const record: Record<string, IImageCache> = {};
-
-    imageCache.forEach((item) => {
-        record[item.key] = item;
-    });
-
-    return record;
-}
-
-class ImageCacheMap {
-    private _map = new Map<string, IImageCache>();
-    private _poistionMap = new Map<number, Map<number, Set<string>>>();
-
-    set(key: string, imageCache: IImageCache) {
-        this._map.set(key, imageCache);
-
-        this.update(key, imageCache.row, imageCache.col);
-    }
-
-    update(key: string, row: number, col: number) {
-        const item = this._map.get(key);
-        if (item) {
-            const rowMap = this._poistionMap.get(row);
-            if (rowMap) {
-                const colMap = rowMap.get(col);
-                if (colMap) {
-                    colMap.delete(key);
-                }
-            }
-
-            item.row = row;
-            item.col = col;
-        }
-
-        if (row === -1 || col === -1) {
-            return;
-        }
-
-        let rowMap = this._poistionMap.get(row);
-        if (!rowMap) {
-            rowMap = new Map();
-        }
-
-        let colMap = rowMap.get(col);
-        if (!colMap) {
-            colMap = new Set();
-        }
-        colMap.add(key);
-        rowMap.set(col, colMap);
-        this._poistionMap.set(row, rowMap);
-    }
-
-    delete(key: string) {
-        this.update(key, -1, -1);
-        const item = this._map.get(key);
-        if (item) {
-            item.disposable.dispose();
-        }
-        this._map.delete(key);
-    }
-
-    deleteByPosition(row: number, col: number) {
-        const colMap = this._poistionMap.get(row)?.get(col);
-        if (colMap) {
-            colMap.forEach((key) => {
-                const item = this._map.get(key);
-                if (item) {
-                    item.disposable.dispose();
-                }
-                this._map.delete(key);
-            });
-
-            this._poistionMap.get(row)!.delete(col);
-        }
-    }
-
-    getByKey(key: string) {
-        return this._map.get(key);
-    }
-
-    private _getByPosition(row: number, col: number) {
-        const rowMap = this._poistionMap.get(row);
-        if (rowMap) {
-            return [...(rowMap.get(col) || [])].map((key) => this._map.get(key)).filter(Boolean) as IImageCache[];
-        }
-        return [];
-    }
-
-    getByPosition(row: number, col: number) {
-        return makeRecord(this._getByPosition(row, col));
-    }
-
-    getImageCahce(row: number, col: number) {
-        return makeImageRecord(this._getByPosition(row, col));
-    }
-}
-
 export class SheetCellImageController extends Disposable {
-    private _imageMaps = new Map<string, Map<string, ImageCacheMap>>();
-    private _initedMap = new Map<string, Map<string, boolean>>();
-
     constructor(
         @ICommandService private readonly _commandService: ICommandService,
         @IImageIoService private readonly _imageIoService: IImageIoService,
@@ -157,158 +38,22 @@ export class SheetCellImageController extends Disposable {
         @Inject(Injector) private readonly _injector: Injector,
         @IDrawingManagerService private readonly _drawingManagerService: IDrawingManagerService,
         @Inject(DocDrawingController) private readonly _docDrawingController: DocDrawingController,
-        @Inject(IEditorBridgeService) private readonly _editorBridgeService: IEditorBridgeService
+        @Inject(IEditorBridgeService) private readonly _editorBridgeService: IEditorBridgeService,
+        @Inject(SheetCellCacheManagerService) private readonly _sheetCellCacheManagerService: SheetCellCacheManagerService
     ) {
         super();
 
-        this._initSetRangeValuesCommandListener();
         this._initInterceptor();
-        this._initSheetChange();
         this._initHandleResize();
         this._handleInitEditor();
         this._handleWriteCell();
-    }
-
-    private _reRender(unitId: string) {
-        const render = this._renderManagerService.getCurrentTypeOfRenderer(UniverInstanceType.UNIVER_SHEET);
-
-        if (render && render.unitId === unitId) {
-            render.mainComponent?.makeForceDirty();
-        }
-    }
-
-    private _ensureMap(unitId: string, subUnitId: string) {
-        let unitMap = this._imageMaps.get(unitId);
-        if (!unitMap) {
-            unitMap = new Map();
-            this._imageMaps.set(unitId, unitMap);
-        }
-
-        let subUnitMap = unitMap.get(subUnitId);
-        if (!subUnitMap) {
-            subUnitMap = new ImageCacheMap();
-            unitMap.set(subUnitId, subUnitMap);
-        }
-        return subUnitMap;
-    }
-
-    private _initSubSheet(unitId: string, subUnitId: string) {
-        if (this._initedMap.get(unitId)?.get(subUnitId)) {
-            return;
-        }
-
-        const workbook = this._univerInstanceService.getUnit<Workbook>(unitId, UniverInstanceType.UNIVER_SHEET);
-        if (!workbook) {
-            return;
-        }
-
-        const subSheet = workbook.getSheetBySheetId(subUnitId);
-        if (!subSheet) {
-            return;
-        }
-        const unitMap = this._initedMap.get(unitId) || new Map<string, boolean>();
-        unitMap.set(subUnitId, true);
-        this._initedMap.set(unitId, unitMap);
-
-        const matrix = subSheet.getCellMatrix();
-        this._handleMatrix(unitId, subUnitId, matrix);
-    }
-
-    private _handleMatrix(unitId: string, subUnitId: string, matrix: ObjectMatrix<Nullable<ICellData>>) {
-        const map = this._ensureMap(unitId, subUnitId);
-        matrix.forValue((row, col, cell) => {
-            if (cell?.p && cell.p.drawingsOrder?.length) {
-                const imageCache = map.getByPosition(row, col);
-                cell.p.drawingsOrder.forEach(async (key) => {
-                    const image = imageCache[key];
-                    if (!image) {
-                        const drawing = cell.p!.drawings![key]! as IImageData & IDocDrawingBase;
-                        const disposable = this._refRangeService.watchRange(
-                            unitId,
-                            subUnitId,
-                            {
-                                startRow: row,
-                                endRow: row,
-                                startColumn: col,
-                                endColumn: col,
-                            },
-                            (_, after) => {
-                                if (after) {
-                                    map.update(key, after.startRow, after.startColumn);
-                                } else {
-                                    map.delete(key);
-                                }
-                            });
-                        const imageElement = new Image();
-
-                        const item: IImageCache = {
-                            image: imageElement,
-                            disposable,
-                            row,
-                            col,
-                            key,
-                            status: 'loading',
-                        };
-                        map.set(key, item);
-
-                        imageElement.width = drawing.transform?.width || 0;
-                        imageElement.height = drawing.transform?.height || 0;
-                        if (drawing.imageSourceType === ImageSourceType.UUID) {
-                            try {
-                                imageElement.src = await this._imageIoService.getImage(drawing.source);
-                            } catch (error) {
-                                console.error(error);
-                            }
-                        } else {
-                            imageElement.src = drawing.source;
-                        }
-
-                        imageElement.onload = () => {
-                            item.status = 'loaded';
-                            this._reRender(unitId);
-                        };
-                        imageElement.onerror = () => {
-                            item.status = 'error';
-                        };
-                    }
-                });
-
-                return;
-            }
-
-            map.deleteByPosition(row, col);
-        });
-    }
-
-    private _initSetRangeValuesCommandListener() {
-        this.disposeWithMe(this._commandService.onCommandExecuted((commandInfo) => {
-            if (commandInfo.id === SetRangeValuesMutation.id) {
-                const params = commandInfo.params as ISetRangeValuesMutationParams;
-                const { cellValue, unitId, subUnitId } = params;
-                if (!cellValue || !this._initedMap.get(unitId)?.get(subUnitId)) {
-                    return;
-                }
-
-                const cellMatrux = new ObjectMatrix(cellValue);
-                const workbook = this._univerInstanceService.getUnit<Workbook>(unitId, UniverInstanceType.UNIVER_SHEET);
-                const worksheet = workbook?.getSheetBySheetId(subUnitId);
-                if (!worksheet) {
-                    return;
-                }
-                cellMatrux.forValue((row, col) => {
-                    const value = worksheet.getCellRaw(row, col);
-                    cellMatrux.setValue(row, col, value);
-                });
-                this._handleMatrix(unitId, subUnitId, cellMatrux);
-            }
-        }));
     }
 
     private _initInterceptor() {
         this.disposeWithMe(this._sheetInterceptorService.intercept(INTERCEPTOR_POINT.CELL_CONTENT, {
             handler: (cell, context, next) => {
                 const { row, col } = context;
-                const imageCache = this._imageMaps.get(context.unitId)?.get(context.subUnitId)?.getImageCahce(row, col);
+                const imageCache = this._sheetCellCacheManagerService.getImageElementByPosition(context.unitId, context.subUnitId, row, col);
 
                 if (imageCache && cell) {
                     cell.images = imageCache;
@@ -317,34 +62,6 @@ export class SheetCellImageController extends Disposable {
                 return next(cell);
             },
         }));
-    }
-
-    private _initSheetChange() {
-        const map = new Map<string, IDisposable>();
-
-        const hanldeUnit = (unit: UnitModel<object, number>) => {
-            if (unit.type === UniverInstanceType.UNIVER_SHEET) {
-                const workbook = unit as Workbook;
-                this._initSubSheet(workbook.getUnitId(), workbook.getActiveSheet().getSheetId());
-
-                map.set(
-                    workbook.getUnitId(),
-                    toDisposable(workbook.activeSheet$.subscribe((sheet) => {
-                        if (sheet) {
-                            this._initSubSheet(workbook.getUnitId(), sheet.getSheetId());
-                        }
-                    }))
-                );
-            }
-        };
-        this._univerInstanceService.getAllUnitsForType<Workbook>(UniverInstanceType.UNIVER_SHEET).forEach(hanldeUnit);
-        this.disposeWithMe(this._univerInstanceService.unitAdded$.subscribe(hanldeUnit));
-
-        this.disposeWithMe(() => {
-            map.forEach((disposable) => {
-                disposable.dispose();
-            });
-        });
     }
 
     private _initHandleResize() {
@@ -389,7 +106,7 @@ export class SheetCellImageController extends Disposable {
                 const cellMatrix = sheetTarget.worksheet.getCellMatrix();
                 cellMatrix.forValue((row, col, cellData) => {
                     if (rows.has(row) || cols.has(col)) {
-                        const imageCache = this._imageMaps.get(sheetTarget.unitId)?.get(sheetTarget.subUnitId)?.getByPosition(row, +col);
+                        const imageCache = this._sheetCellCacheManagerService.getImageCache(sheetTarget.unitId, sheetTarget.subUnitId, row, +col);
                         // single image need to reisze by cell
                         if (cellData?.p && cellData.p.drawingsOrder?.length === 1 && cellData.p.body?.dataStream.length === 3) {
                             const image = cellData.p.drawings![cellData.p.drawingsOrder[0]]! as IImageData & IDocDrawingBase;
--- packages/sheets-drawing-ui/src/controllers/sheet-celll-image-hover.controller.ts
+++ packages/sheets-drawing-ui/src/controllers/sheet-celll-image-hover.controller.ts
@@ -15,25 +15,33 @@
  */
 
 import { Disposable, Inject, UniverInstanceType } from '@univerjs/core';
+import { DrawingRenderService } from '@univerjs/drawing-ui';
 import { CURSOR_TYPE, IRenderManagerService } from '@univerjs/engine-render';
+import { SheetsSelectionsService } from '@univerjs/sheets';
 import { HoverManagerService } from '@univerjs/sheets-ui';
+import { SheetCellCacheManagerService } from '../services/sheet-cell-cache-manager.service';
 
 export class SheetCellImageHoverController extends Disposable {
     constructor(
         @Inject(HoverManagerService) private _hoverManagerService: HoverManagerService,
-        @Inject(IRenderManagerService) private _renderManagerService: IRenderManagerService
+        @Inject(IRenderManagerService) private _renderManagerService: IRenderManagerService,
+        @Inject(SheetsSelectionsService) private _selectionsService: SheetsSelectionsService,
+        @Inject(SheetCellCacheManagerService) private _sheetCellCacheManagerService: SheetCellCacheManagerService,
+        @Inject(DrawingRenderService) private _drawingRenderService: DrawingRenderService
     ) {
         super();
         this._initHover();
+        this._initImageClick();
     }
 
     private _initHover() {
         let isSetCursor = false;
         this.disposeWithMe(this._hoverManagerService.currentRichText$.subscribe((hover) => {
             if (hover) {
+                const currentSelection = this._selectionsService.getWorkbookSelections(hover.unitId).getSelectionOfWorksheet(hover.subUnitId)[0];
                 const renderer = this._renderManagerService.getRenderById(hover.unitId);
                 if (renderer) {
-                    if (hover.drawing) {
+                    if (hover.drawing && currentSelection.primary?.actualColumn === hover.col && currentSelection.primary.actualRow === hover.row) {
                         renderer.mainComponent?.setCursor(CURSOR_TYPE.ZOOM_IN);
                         isSetCursor = true;
                     } else {
@@ -47,6 +55,23 @@ export class SheetCellImageHoverController extends Disposable {
                     if (renderer) {
                         renderer.mainComponent?.setCursor(CURSOR_TYPE.DEFAULT);
                         isSetCursor = false;
+                    }
+                }
+            }
+        }));
+    }
+
+    private _initImageClick() {
+        this.disposeWithMe(this._hoverManagerService.currentClickedCell$.subscribe((clickedCell) => {
+            if (clickedCell.drawing) {
+                const currentSelection = this._selectionsService.getWorkbookSelections(clickedCell.location.unitId).getSelectionOfWorksheet(clickedCell.location.subUnitId)[0];
+
+                if (currentSelection.primary?.actualColumn === clickedCell.location.col && currentSelection.primary?.actualRow === clickedCell.location.row) {
+                    // const drawings = this
+                    const imageElement = this._sheetCellCacheManagerService.getImageElementByKey(clickedCell.location.unitId, clickedCell.location.subUnitId, clickedCell.drawing);
+
+                    if (imageElement) {
+                        this._drawingRenderService.previewImage(`${clickedCell.drawing}-preview-image`, imageElement.src, imageElement.width, imageElement.height);
                     }
                 }
             }

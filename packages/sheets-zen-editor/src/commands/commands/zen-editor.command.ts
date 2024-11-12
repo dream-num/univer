3cc646acb84548237451cd351f62b9241a04fabe
zhangw <zhang9748@foxmail.com>
2024-11-12 20:38:56 +0800

    feat: update

--- packages/sheets-zen-editor/src/commands/commands/zen-editor.command.ts
+++ packages/sheets-zen-editor/src/commands/commands/zen-editor.command.ts
@@ -19,7 +19,7 @@ import { CommandType, DOCS_ZEN_EDITOR_UNIT_ID_KEY, DocumentDataModel, DocumentFl
 import { IEditorService } from '@univerjs/docs-ui';
 import { IRenderManagerService } from '@univerjs/engine-render';
 import { EditingRenderController, IEditorBridgeService } from '@univerjs/sheets-ui';
-import { IZenZoneService } from '@univerjs/ui';
+import { ISidebarService, IZenZoneService } from '@univerjs/ui';
 
 export const OpenZenEditorCommand: ICommand = {
     id: 'zen-editor.command.open-zen-editor',
@@ -29,7 +29,9 @@ export const OpenZenEditorCommand: ICommand = {
         const editorService = accessor.get(IEditorService);
         const editorBridgeService = accessor.get(IEditorBridgeService);
         const univerInstanceService = accessor.get(IUniverInstanceService);
+        const sideBarService = accessor.get(ISidebarService);
 
+        sideBarService.close();
         zenZoneService.open();
 
         const editor = editorService.getEditor(DOCS_ZEN_EDITOR_UNIT_ID_KEY);
@@ -87,7 +89,9 @@ export const CancelZenEditCommand: ICommand = {
         const zenZoneEditorService = accessor.get(IZenZoneService);
         const editorBridgeService = accessor.get(IEditorBridgeService);
         const univerInstanceManager = accessor.get(IUniverInstanceService);
+        const sideBarService = accessor.get(ISidebarService);
 
+        sideBarService.close();
         zenZoneEditorService.close();
 
         const currentSheetInstance = univerInstanceManager.getCurrentUnitForType<Workbook>(UniverInstanceType.UNIVER_SHEET);
@@ -112,7 +116,9 @@ export const ConfirmZenEditCommand: ICommand = {
         const editorBridgeService = accessor.get(IEditorBridgeService);
         const univerInstanceManager = accessor.get(IUniverInstanceService);
         const editorService = accessor.get(IEditorService);
+        const sideBarService = accessor.get(ISidebarService);
 
+        sideBarService.close();
         zenZoneEditorService.close();
 
         const editor = editorService.getEditor(DOCS_ZEN_EDITOR_UNIT_ID_KEY);

d11cbb1e0104da7625bb849e009f007e3d2b8643
白熱 <sonne@asaki.me>
2024-11-14 18:09:07 +0800

    chore: update generation rules for globalName in UMD builds (#4061)

--- common/shared/vite/utils.ts
+++ common/shared/vite/utils.ts
@@ -19,7 +19,9 @@ import JavaScriptObfuscator from 'javascript-obfuscator';
 
 export function convertLibNameFromPackageName(name: string) {
     return name
-        .replace(/^@univerjs(?:-[^/]+)?\//, 'univer-')
+        .replace(/^@(univerjs(?:-pro)?)\//, (_, matchedPrefix) => {
+            return matchedPrefix === 'univerjs-pro' ? 'univer-pro-' : 'univer-';
+        })
         .replace('/facade', '-facade')
         .split('-')
         .map((word) => word.charAt(0).toUpperCase() + word.slice(1))

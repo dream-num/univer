e50c1e36ab253e9e309d45fcaff84bebbad48994
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2024-11-20 17:10:24 +0800

    test: change formula integration test playground

--- tests/formula-integration/src/__testing__/univer.ts
+++ tests/formula-integration/src/__testing__/univer.ts
@@ -14,8 +14,19 @@
  * limitations under the License.
  */
 
-import { FUniver } from '@univerjs/core';
-import { createUniverOnNode } from 'examples-node';
+import { FUniver, Univer } from '@univerjs/core';
+import { UniverDataValidationPlugin } from '@univerjs/data-validation';
+import { UniverDocsPlugin } from '@univerjs/docs';
+import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula';
+import { UniverSheetsPlugin } from '@univerjs/sheets';
+import { UniverSheetsConditionalFormattingPlugin } from '@univerjs/sheets-conditional-formatting';
+import { UniverSheetsDataValidationPlugin } from '@univerjs/sheets-data-validation';
+import { UniverSheetsFormulaPlugin } from '@univerjs/sheets-formula';
+
+import '@univerjs/sheets/facade';
+import '@univerjs/sheets-data-validation/facade';
+import '@univerjs/engine-formula/facade';
+import '@univerjs/sheets-formula/facade';
 
 export function createFormulaTestBed() {
     const univer = createUniverOnNode();
@@ -26,4 +37,30 @@ export function createFormulaTestBed() {
         get: injector.get.bind(injector),
         api: FUniver.newAPI(univer),
     };
+}
+
+function createUniverOnNode(): Univer {
+    const univer = new Univer();
+
+    registerBasicPlugins(univer);
+    registerDocPlugins(univer);
+    registerSheetPlugins(univer);
+
+    return univer;
+}
+
+function registerBasicPlugins(univer: Univer): void {
+    univer.registerPlugin(UniverFormulaEnginePlugin, { notExecuteFormula: false });
+}
+
+function registerDocPlugins(univer: Univer): void {
+    univer.registerPlugin(UniverDocsPlugin);
+}
+
+function registerSheetPlugins(univer: Univer): void {
+    univer.registerPlugin(UniverSheetsPlugin);
+    univer.registerPlugin(UniverSheetsFormulaPlugin);
+    univer.registerPlugin(UniverSheetsConditionalFormattingPlugin);
+    univer.registerPlugin(UniverDataValidationPlugin);
+    univer.registerPlugin(UniverSheetsDataValidationPlugin);
 }

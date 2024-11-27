ed574540fd0d09d5067e39e7834935baf4612e19
jocs <ransixi@gmail.com>
2024-11-27 20:57:13 +0800

    fix: 4139

--- packages/docs-ui/src/services/doc-auto-format.service.ts
+++ packages/docs-ui/src/services/doc-auto-format.service.ts
@@ -112,7 +112,8 @@ export class DocAutoFormatService extends Disposable {
     onAutoFormat(id: string, params: Nullable<object>): ICommandInfo[] {
         const autoFormats = this._matches.get(id) ?? [];
         const unit = this._univerInstanceService.getCurrentUnitForType<DocumentDataModel>(UniverInstanceType.UNIVER_DOC);
-        const selection = this._textSelectionManagerService.getActiveTextRange();
+        const docRanges = this._textSelectionManagerService.getDocRanges();
+        const selection = docRanges.find((range) => range.isActive) ?? docRanges[0];
 
         if (unit && selection) {
             const doc = unit.getSelfOrHeaderFooterModel(selection.segmentId);

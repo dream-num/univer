7f607ddbe5c767131bd423e99b901cdb8a18975b
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2024-12-13 11:23:35 +0800

    docs: add documents

--- packages/ui/src/controllers/ui/ui.controller.ts
+++ packages/ui/src/controllers/ui/ui.controller.ts
@@ -19,11 +19,26 @@ import { createIdentifier } from '@univerjs/core';
 export interface IWorkbenchOptions {
     container?: string | HTMLElement;
 
+    /**
+     * If Univer should make the header bar visible.
+     */
     header?: boolean;
+
+    /**
+     * If Univer should make the toolbar bar visible.
+     */
     toolbar?: boolean;
+
+    /**
+     * If Univer should make the footer bar visible.
+     */
     footer?: boolean;
+
+    /**
+     * If Univer should make the context menu usable.
+     */
     contextMenu?: boolean;
 }
 
-export interface IUIController {}
+export interface IUIController { }
 export const IUIController = createIdentifier<IUIController>('univer.ui.ui-controller');

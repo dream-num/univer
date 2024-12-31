4ea3d862468ac85da1b498f0ec85a8479a248c18
白熱 <sonne@asaki.me>
2024-12-31 14:37:32 +0800

    chore: cleanup package.json & update dependencies

--- common/shared/postcss/postcss.config.mjs
+++ common/shared/postcss/postcss.config.mjs
@@ -1,9 +1,12 @@
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
+    plugins: [
+        tailwindcss,
+        autoprefixer,
+    ],
 };
 
 export default config;

5f8e8e22ec7fe21a2f562ae74c031429b6d074a4
zhangw <zhang9748@foxmail.com>
2025-01-15 19:11:20 +0800

    feat: add event for row header & col header

--- packages/sheets-ui/src/controllers/hover-render.controller.ts
+++ packages/sheets-ui/src/controllers/hover-render.controller.ts
@@ -18,6 +18,7 @@ import type { Nullable, Workbook } from '@univerjs/core';
 import type { IRenderContext, IRenderModule } from '@univerjs/engine-render';
 import type { ISheetSkeletonManagerParam } from '../services/sheet-skeleton-manager.service';
 import { Disposable, DisposableCollection, fromEventSubject, Inject } from '@univerjs/core';
+import { SHEET_VIEW_KEY } from '../common/keys';
 import { HoverManagerService } from '../services/hover-manager.service';
 import { SheetScrollManagerService } from '../services/scroll-manager.service';
 import { SheetSkeletonManagerService } from '../services/sheet-skeleton-manager.service';
@@ -41,6 +42,7 @@ export class HoverRenderController extends Disposable implements IRenderModule {
         this._initScrollEvent();
     }
 
+    // eslint-disable-next-line max-lines-per-function
     private _initPointerEvent() {
         const disposeSet = new DisposableCollection();
         const handleSkeletonChange = (skeletonParam: Nullable<ISheetSkeletonManagerParam>) => {
@@ -50,7 +52,7 @@ export class HoverRenderController extends Disposable implements IRenderModule {
                 return;
             }
 
-            const { mainComponent, unitId } = this._context;
+            const { mainComponent, unitId, components } = this._context;
             if (!mainComponent) {
                 return;
             }
@@ -73,10 +75,53 @@ export class HoverRenderController extends Disposable implements IRenderModule {
                 this._hoverManagerService.triggerClick(unitId, evt.offsetX, evt.offsetY);
             }));
 
+            disposeSet.add(mainComponent.onDblclick$.subscribeEvent((evt) => {
+                this._hoverManagerService.triggerDbClick(unitId, evt.offsetX, evt.offsetY);
+            }));
+
             disposeSet.add(mainComponent.onPointerLeave$.subscribeEvent(() => {
-                // this._hoverManagerService.triggerMouseLeave(unitId);
                 this._active = false;
             }));
+
+            const rowHeader = components.get(SHEET_VIEW_KEY.ROW);
+            const colHeader = components.get(SHEET_VIEW_KEY.COLUMN);
+            if (rowHeader) {
+                disposeSet.add(rowHeader.onPointerMove$.subscribeEvent((evt) => {
+                    this._hoverManagerService.triggerRowHeaderMouseMove(unitId, evt.offsetX, evt.offsetY);
+                }));
+
+                disposeSet.add(rowHeader.onPointerDown$.subscribeEvent((evt) => {
+                    this._hoverManagerService.triggerRowHeaderPoniterDown(unitId, evt.offsetX, evt.offsetY);
+                }));
+
+                disposeSet.add(rowHeader.onPointerUp$.subscribeEvent((evt) => {
+                    this._hoverManagerService.triggerRowHeaderPoniterUp(unitId, evt.offsetX, evt.offsetY);
+                    this._hoverManagerService.triggerRowHeaderClick(unitId, evt.offsetX, evt.offsetY);
+                }));
+
+                disposeSet.add(rowHeader.onDblclick$.subscribeEvent((evt) => {
+                    this._hoverManagerService.triggerRowHeaderDbClick(unitId, evt.offsetX, evt.offsetY);
+                }));
+            }
+
+            if (colHeader) {
+                disposeSet.add(colHeader.onPointerMove$.subscribeEvent((evt) => {
+                    this._hoverManagerService.triggerColHeaderMouseMove(unitId, evt.offsetX, evt.offsetY);
+                }));
+
+                disposeSet.add(colHeader.onPointerDown$.subscribeEvent((evt) => {
+                    this._hoverManagerService.triggerColHeaderPoniterDown(unitId, evt.offsetX, evt.offsetY);
+                }));
+
+                disposeSet.add(colHeader.onPointerUp$.subscribeEvent((evt) => {
+                    this._hoverManagerService.triggerColHeaderPoniterUp(unitId, evt.offsetX, evt.offsetY);
+                    this._hoverManagerService.triggerColHeaderClick(unitId, evt.offsetX, evt.offsetY);
+                }));
+
+                disposeSet.add(colHeader.onDblclick$.subscribeEvent((evt) => {
+                    this._hoverManagerService.triggerColHeaderDbClick(unitId, evt.offsetX, evt.offsetY);
+                }));
+            }
         };
 
         handleSkeletonChange(this._sheetSkeletonManagerService.getCurrent());

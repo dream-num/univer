644640dd752e28eedcaff965835e9fb7552da3c1
lumixraku <lumixraku@gmail.com>
2025-01-22 12:45:18 +0800

    fix: update freeze handle in left top viewport size when column header size changed

--- packages/sheets-ui/src/controllers/render-controllers/freeze.render-controller.ts
+++ packages/sheets-ui/src/controllers/render-controllers/freeze.render-controller.ts
@@ -14,6 +14,22 @@
  * limitations under the License.
  */
 
+import type { ICommandInfo, IFreeze, IRange, IStyleSheet, IWorksheetData, Nullable, Workbook } from '@univerjs/core';
+import type { IMouseEvent, IPointerEvent, IRenderContext, IRenderModule, Viewport } from '@univerjs/engine-render';
+import type {
+    IInsertColCommandParams,
+    IInsertRowCommandParams,
+    IMoveColsCommandParams,
+    IMoveRowsCommandParams,
+    IRemoveRowColCommandParams,
+    ISetColHiddenMutationParams,
+    ISetFrozenMutationParams,
+    ISetRowHiddenMutationParams,
+    ISetWorksheetColWidthMutationParams,
+    ISetWorksheetRowAutoHeightMutationParams,
+    ISetWorksheetRowHeightMutationParams,
+} from '@univerjs/sheets';
+import type { IViewportScrollState } from '../../services/scroll-manager.service';
 import {
     ColorKit,
     createInterceptorKey,
@@ -28,6 +44,7 @@ import {
     toDisposable,
 } from '@univerjs/core';
 import { CURSOR_TYPE, Rect, SHEET_VIEWPORT_KEY, TRANSFORM_CHANGE_OBSERVABLE_TYPE, Vector2 } from '@univerjs/engine-render';
+
 import {
     InsertColCommand,
     InsertRangeMoveDownCommand,
@@ -52,30 +69,13 @@ import {
     SheetsSelectionsService,
 } from '@univerjs/sheets';
 import { Subscription } from 'rxjs';
-import type { ICommandInfo, IFreeze, IRange, IStyleSheet, IWorksheetData, Nullable, Workbook } from '@univerjs/core';
-import type { IMouseEvent, IPointerEvent, IRenderContext, IRenderModule, Viewport } from '@univerjs/engine-render';
-
-import type {
-    IInsertColCommandParams,
-    IInsertRowCommandParams,
-    IMoveColsCommandParams,
-    IMoveRowsCommandParams,
-    IRemoveRowColCommandParams,
-    ISetColHiddenMutationParams,
-    ISetFrozenMutationParams,
-    ISetRowHiddenMutationParams,
-    ISetWorksheetColWidthMutationParams,
-    ISetWorksheetRowAutoHeightMutationParams,
-    ISetWorksheetRowHeightMutationParams,
-} from '@univerjs/sheets';
 import { ScrollCommand } from '../../commands/commands/set-scroll.command';
 import { SetZoomRatioOperation } from '../../commands/operations/set-zoom-ratio.operation';
-import { SHEET_COMPONENT_HEADER_LAYER_INDEX } from '../../common/keys';
 
+import { SHEET_COMPONENT_HEADER_LAYER_INDEX } from '../../common/keys';
 import { SheetScrollManagerService } from '../../services/scroll-manager.service';
 import { SheetSkeletonManagerService } from '../../services/sheet-skeleton-manager.service';
 import { getCoordByOffset, getSheetObject } from '../utils/component-tools';
-import type { IViewportScrollState } from '../../services/scroll-manager.service';
 
 enum FREEZE_DIRECTION_TYPE {
     ROW,
@@ -89,13 +89,13 @@ enum ResetScrollType {
     ALL = 3,
 }
 
-const FREEZE_ROW_MAIN_NAME = '__SpreadsheetFreezeRowMainName__';
+export const FREEZE_ROW_MAIN_NAME = '__SpreadsheetFreezeRowMainName__';
 
-const FREEZE_ROW_HEADER_NAME = '__SpreadsheetFreezeRowHeaderName__';
+export const FREEZE_ROW_HEADER_NAME = '__SpreadsheetFreezeRowHeaderName__';
 
-const FREEZE_COLUMN_MAIN_NAME = '__SpreadsheetFreezeColumnMainName__';
+export const FREEZE_COLUMN_MAIN_NAME = '__SpreadsheetFreezeColumnMainName__';
 
-const FREEZE_COLUMN_HEADER_NAME = '__SpreadsheetFreezeColumnHeaderName__';
+export const FREEZE_COLUMN_HEADER_NAME = '__SpreadsheetFreezeColumnHeaderName__';
 
 const FREEZE_SIZE_NORMAL = 2;
 

d8ce92b665ab13d9d5c17eebe82dd688c315dfc1
zhangw <zhang9748@foxmail.com>
2025-02-14 00:51:01 +0800

    feat: update

--- packages/sheets/src/facade/f-univer.ts
+++ packages/sheets/src/facade/f-univer.ts
@@ -139,13 +139,12 @@ export interface IFUniverSheetsMixin {
 export class FUniverSheetsMixin extends FUniver implements IFUniverSheetsMixin {
     override getCommandSheetTarget(commandInfo: ICommandInfo<object>): Nullable<{ workbook: FWorkbook; worksheet: FWorksheet }> {
         const params = commandInfo.params as { unitId: string; subUnitId: string; sheetId: string };
-        if (!params) return;
-        const workbook = params.unitId ? this.getUniverSheet(params.unitId) : this.getActiveWorkbook?.();
+        const workbook = params?.unitId ? this.getUniverSheet(params.unitId) : this.getActiveWorkbook?.();
         if (!workbook) {
             return;
         }
 
-        const worksheet = workbook.getSheetBySheetId(params.subUnitId || params.sheetId) || workbook.getActiveSheet();
+        const worksheet = workbook.getSheetBySheetId(params?.subUnitId || params?.sheetId) || workbook.getActiveSheet();
         if (!worksheet) {
             return;
         }

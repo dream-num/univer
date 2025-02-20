b23375aea6dea605c50d4b14cf2b973dee9dfd5c
wpxp123456 <2677556700@qq.com>
2025-02-20 19:33:19 +0800

    docs(facade): supplement notes and example

--- packages/sheets-find-replace/src/facade/f-univer.ts
+++ packages/sheets-find-replace/src/facade/f-univer.ts
@@ -28,11 +28,34 @@ export interface IFUniverFindReplaceMixin {
      * @returns {Promise<FTextFinder | null>} A promise that resolves to the text-finder instance.
      * @example
      * ```typescript
-     * const textFinder = await univerAPI.createTextFinderAsync('Hello');
+     * // Assume the current sheet is empty sheet.
+     * const fWorkbook = univerAPI.getActiveWorkbook();
+     * const fWorksheet = fWorkbook.getActiveSheet();
+     * const fRange = fWorksheet.getRange('A1:D10');
+     * fRange.setValues([
+     *   [1, 2, 3, 4],
+     *   [2, 3, 4, 5],
+     *   [3, 4, 5, 6],
+     *   [4, 5, 6, 7],
+     *   [5, 6, 7, 8],
+     *   [6, 7, 8, 9],
+     *   [7, 8, 9, 10],
+     *   [8, 9, 10, 11],
+     *   [9, 10, 11, 12],
+     *   [10, 11, 12, 13]
+     * ]);
+     *
+     * // Create a text-finder to find the text '5'.
+     * const textFinder = await univerAPI.createTextFinderAsync('5');
+     *
+     * // Find all cells that contain the text '5'.
+     * const matchCells = textFinder.findAll();
+     * matchCells.forEach((cell) => {
+     *   console.log(cell.getA1Notation()); // D2, C3, B4, A5
+     * });
      * ```
      */
     createTextFinderAsync(text: string): Promise<FTextFinder | null>;
-
 }
 
 export class FUniverFindReplaceMixin extends FUniver implements IFUniverFindReplaceMixin {

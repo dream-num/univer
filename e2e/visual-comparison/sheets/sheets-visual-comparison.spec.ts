35145ff183c8c62528ea58f284da039484cd348d
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2025-02-27 08:53:35 +0800

    chore: update threshold

--- e2e/visual-comparison/sheets/sheets-visual-comparison.spec.ts
+++ e2e/visual-comparison/sheets/sheets-visual-comparison.spec.ts
@@ -285,7 +285,7 @@ test('diff set text format number cell', async () => {
 
     const filename = generateSnapshotName('set-text-format-number-cell');
     const screenshot = await page.locator(SHEET_MAIN_CANVAS_ID).screenshot();
-    await expect(screenshot).toMatchSnapshot(filename, { maxDiffPixels: 150 });
+    await expect(screenshot).toMatchSnapshot(filename, { maxDiffPixelRatio: 0.005 });
 
     await page.waitForTimeout(2000);
     await browser.close();

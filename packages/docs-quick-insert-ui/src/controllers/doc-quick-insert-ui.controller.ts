240546f43b83919fb6b80fd8db7f58fcb922b4ea
siam <31573022+siam-ese@users.noreply.github.com>
2025-03-17 17:07:46 +0800

    chore: remove unused variables in quick insert (#4844)

--- packages/docs-quick-insert-ui/src/controllers/doc-quick-insert-ui.controller.ts
+++ packages/docs-quick-insert-ui/src/controllers/doc-quick-insert-ui.controller.ts
@@ -18,7 +18,7 @@ import type { ITextRangeWithStyle } from '@univerjs/engine-render';
 import type { IDocPopup } from '../services/doc-quick-insert-popup.service';
 import { Disposable, ICommandService, Inject } from '@univerjs/core';
 import { DividerSingle, TextSingle } from '@univerjs/icons';
-import { ComponentManager, IMenuManagerService } from '@univerjs/ui';
+import { ComponentManager } from '@univerjs/ui';
 import { of } from 'rxjs';
 import { DeleteSearchKeyCommand } from '../commands/commands/doc-quick-insert.command';
 import { CloseQuickInsertPopupOperation, ShowQuickInsertPopupOperation } from '../commands/operations/quick-insert-popup.operation';
@@ -31,8 +31,7 @@ export class DocQuickInsertUIController extends Disposable {
     constructor(
         @ICommandService private readonly _commandService: ICommandService,
         @Inject(DocQuickInsertPopupService) private readonly _docQuickInsertPopupService: DocQuickInsertPopupService,
-        @Inject(ComponentManager) private readonly _componentManager: ComponentManager,
-        @Inject(IMenuManagerService) private readonly _menuManagerService: IMenuManagerService
+        @Inject(ComponentManager) private readonly _componentManager: ComponentManager
     ) {
         super();
 

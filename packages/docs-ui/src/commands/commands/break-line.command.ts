3e2d8d6a2141eaadba0fa42e8736415634fb2ea4
zhangw <zhang9748@foxmail.com>
2025-03-28 21:02:43 +0800

    feat: update

--- packages/docs-ui/src/commands/commands/break-line.command.ts
+++ packages/docs-ui/src/commands/commands/break-line.command.ts
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-import type { DocumentDataModel, ICommand, IDocumentBody, IParagraph, IParagraphBorder } from '@univerjs/core';
-import { CommandType, DataStreamTreeTokenType, ICommandService, IUniverInstanceService, PresetListType, Tools, UniverInstanceType } from '@univerjs/core';
-import { DocSelectionManagerService } from '@univerjs/docs';
+import type { DocumentDataModel, ICommand, IDocumentBody, IMutationInfo, IParagraph, IParagraphBorder } from '@univerjs/core';
+import type { IRichTextEditingMutationParams } from '@univerjs/docs';
+import { BuildTextUtils, CommandType, DataStreamTreeTokenType, ICommandService, IUniverInstanceService, JSONX, PresetListType, TextX, TextXActionType, Tools, UniverInstanceType, UpdateDocsAttributeType } from '@univerjs/core';
+import { DocSelectionManagerService, RichTextEditingMutation } from '@univerjs/docs';
 import { getTextRunAtPosition } from '../../basics/paragraph';
 import { DocMenuStyleService } from '../../services/doc-menu-style.service';
-import { InsertCommand } from './core-editing.command';
-import { ToggleCheckListCommand } from './list.command';
+import { getRichTextEditPath } from '../util';
 
 export function generateParagraphs(
     dataStream: string,
@@ -102,9 +102,9 @@ export const BreakLineCommand: ICommand<IBreakLineCommandParams> = {
         const { horizontalLine } = params ?? {};
         const { segmentId } = activeTextRange;
         const docDataModel = univerInstanceService.getCurrentUnitForType<DocumentDataModel>(UniverInstanceType.UNIVER_DOC);
-        const body = docDataModel?.getSelfOrHeaderFooterModel(segmentId ?? '').getBody();
+        const originBody = docDataModel?.getSelfOrHeaderFooterModel(segmentId ?? '').getBody();
 
-        if (docDataModel == null || body == null) {
+        if (docDataModel == null || originBody == null) {
             return false;
         }
 
@@ -112,7 +112,7 @@ export const BreakLineCommand: ICommand<IBreakLineCommandParams> = {
 
         const { startOffset, endOffset } = activeTextRange;
 
-        const paragraphs = body.paragraphs ?? [];
+        const paragraphs = originBody.paragraphs ?? [];
         const prevParagraph = paragraphs.find((p) => p.startIndex >= startOffset);
 
         if (!prevParagraph) {
@@ -120,10 +120,9 @@ export const BreakLineCommand: ICommand<IBreakLineCommandParams> = {
         }
 
         const prevParagraphIndex = prevParagraph.startIndex;
-
         const defaultTextStyle = docMenuStyleService.getDefaultStyle();
         const styleCache = docMenuStyleService.getStyleCache();
-        const curTextRun = getTextRunAtPosition(body, endOffset, defaultTextStyle, styleCache);
+        const curTextRun = getTextRunAtPosition(originBody, endOffset, defaultTextStyle, styleCache);
 
         const insertBody: IDocumentBody = {
             dataStream: DataStreamTreeTokenType.PARAGRAPH,
@@ -141,33 +140,106 @@ export const BreakLineCommand: ICommand<IBreakLineCommandParams> = {
             }],
         };
 
-        const deleteRange = {
-            startOffset,
-            endOffset,
-            collapsed: startOffset === endOffset,
-        };
-
-        const result = await commandService.executeCommand(InsertCommand.id, {
-            unitId,
-            body: insertBody,
-            range: deleteRange,
-            segmentId,
-        });
-
-        if (prevParagraph.bullet?.listType === PresetListType.CHECK_LIST_CHECKED) {
-            const params = {
-                index: prevParagraphIndex + 1 - (endOffset - startOffset),
-                segmentId,
-                textRanges: [{
-                    startOffset: startOffset + 1,
-                    endOffset: startOffset + 1,
-                }],
-            };
-            const toggleCheckListResult = await commandService.executeCommand(ToggleCheckListCommand.id, params);
-
-            return Boolean(toggleCheckListResult) && result;
+        if (docDataModel == null) {
+            return false;
         }
 
-        return result;
+        const activeRange = docSelectionManagerService.getActiveTextRange();
+
+        if (originBody == null) {
+            return false;
+        }
+        const { collapsed } = activeTextRange;
+        const cursorMove = insertBody.dataStream.length;
+        const textRanges = [
+            {
+                startOffset: startOffset + cursorMove,
+                endOffset: startOffset + cursorMove,
+                style: activeRange?.style,
+                collapsed,
+            },
+        ];
+
+        const doMutation: IMutationInfo<IRichTextEditingMutationParams> = {
+            id: RichTextEditingMutation.id,
+            params: {
+                unitId,
+                actions: [],
+                textRanges,
+                debounce: true,
+            },
+        };
+
+        const textX = new TextX();
+        const jsonX = JSONX.getInstance();
+
+        if (collapsed) {
+            if (startOffset > 0) {
+                textX.push({
+                    t: TextXActionType.RETAIN,
+                    len: startOffset,
+                });
+            }
+
+            textX.push({
+                t: TextXActionType.INSERT,
+                body: insertBody,
+                len: insertBody.dataStream.length,
+            });
+        } else {
+            const dos = BuildTextUtils.selection.delete([activeTextRange], originBody, 0, insertBody);
+            textX.push(...dos);
+        }
+
+        if (prevParagraph.bullet?.listType === PresetListType.CHECK_LIST_CHECKED || prevParagraph.paragraphStyle?.headingId) {
+            if (activeTextRange.endOffset < prevParagraphIndex) {
+                textX.push({
+                    t: TextXActionType.RETAIN,
+                    len: prevParagraphIndex - activeTextRange.endOffset,
+                });
+            }
+
+            textX.push({
+                t: TextXActionType.RETAIN,
+                len: 1,
+                body: {
+                    dataStream: '',
+                    paragraphs: [
+                        {
+                            ...prevParagraph,
+                            paragraphStyle: {
+                                ...prevParagraph.paragraphStyle,
+                                headingId: undefined,
+                                namedStyleType: undefined,
+                            },
+                            startIndex: 0,
+                            bullet: prevParagraph.paragraphStyle?.headingId
+                                ? undefined
+                                : {
+                                    ...prevParagraph.bullet!,
+                                    listType: PresetListType.CHECK_LIST,
+                                },
+                        },
+                    ],
+                },
+                coverType: UpdateDocsAttributeType.REPLACE,
+            });
+        }
+
+        doMutation.params.textRanges = [{
+            startOffset: startOffset + cursorMove,
+            endOffset: startOffset + cursorMove,
+            collapsed,
+        }];
+
+        const path = getRichTextEditPath(docDataModel, segmentId);
+        doMutation.params.actions = jsonX.editOp(textX.serialize(), path);
+
+        const result = commandService.syncExecuteCommand<
+            IRichTextEditingMutationParams,
+            IRichTextEditingMutationParams
+        >(doMutation.id, doMutation.params);
+
+        return Boolean(result);
     },
 };

1240f83bbc42e1e79d0133aa2154b08e68e2a441
raku <lumixraku@gmail.com>
2025-04-08 21:05:28 +0800

    test: try to rm disable scene event

--- packages/engine-render/src/scene.ts
+++ packages/engine-render/src/scene.ts
@@ -984,7 +984,9 @@ export class Scene extends Disposable {
 
         const scrollBarRect = pickedViewport.pickScrollBar(coord);
         if (scrollBarRect) {
-            return scrollBarRect;
+            if (!scrollBarRect._eventPass === true) {
+                return scrollBarRect;
+            }
         }
 
         const vecFromSheetContent = pickedViewport.transformVector2SceneCoord(coord);

110bc278bfabd5d2e325d7242e125ef2dbc88bcc
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2025-04-25 13:51:17 +0800

    refactor: make more plugins lazy loaded

--- examples/src/sheets/lazy.ts
+++ examples/src/sheets/lazy.ts
@@ -15,13 +15,27 @@
  */
 
 import type { Plugin, PluginCtor } from '@univerjs/core';
+import { UniverDocsDrawingUIPlugin } from '@univerjs/docs-drawing-ui';
+import { UniverDocsMentionUIPlugin } from '@univerjs/docs-mention-ui';
 import { UniverSheetsConditionalFormattingUIPlugin } from '@univerjs/sheets-conditional-formatting-ui';
 import { UniverSheetsDataValidationUIPlugin } from '@univerjs/sheets-data-validation-ui';
 import { UniverSheetsDrawingUIPlugin } from '@univerjs/sheets-drawing-ui';
 import { UniverSheetsFilterUIPlugin } from '@univerjs/sheets-filter-ui';
+import { UniverSheetsFormulaUIPlugin } from '@univerjs/sheets-formula-ui';
+import { UniverSheetsNumfmtUIPlugin } from '@univerjs/sheets-numfmt-ui';
+import { UniverSheetTableUIPlugin } from '@univerjs/sheets-table-ui';
+import { UniverSheetsThreadCommentUIPlugin } from '@univerjs/sheets-thread-comment-ui';
+import { UniverThreadCommentUIPlugin } from '@univerjs/thread-comment-ui';
 
 export default function getLazyPlugins(): Array<[PluginCtor<Plugin>] | [PluginCtor<Plugin>, unknown]> {
     return [
+        [UniverDocsDrawingUIPlugin],
+        [UniverDocsMentionUIPlugin],
+        [UniverSheetsNumfmtUIPlugin],
+        [UniverThreadCommentUIPlugin],
+        [UniverSheetsThreadCommentUIPlugin],
+        [UniverSheetTableUIPlugin],
+        [UniverSheetsFormulaUIPlugin],
         [UniverSheetsDataValidationUIPlugin],
         [UniverSheetsConditionalFormattingUIPlugin],
         [UniverSheetsFilterUIPlugin, { useRemoteFilterValuesGenerator: false }],

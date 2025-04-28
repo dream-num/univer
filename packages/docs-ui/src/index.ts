786c5a76cc370976742fe8f63d48531bcdad0506
ZhangWei <zhang9748@foxmail.com>
2025-04-28 12:38:07 +0800

    feat(docs-ui): export switch mode command (#5108)

--- packages/docs-ui/src/index.ts
+++ packages/docs-ui/src/index.ts
@@ -59,6 +59,7 @@ export { whenDocAndEditorFocused } from './shortcuts/utils';
 export { DOC_VERTICAL_PADDING } from './types/const/padding';
 
 // #region - all commands
+export { SwitchDocModeCommand } from './commands/commands/switch-doc-mode.command.js';
 export { HorizontalLineCommand } from './commands/commands/doc-horizontal-line.command';
 export { AfterSpaceCommand, EnterCommand, type ITabCommandParams, TabCommand } from './commands/commands/auto-format.command';
 export { BreakLineCommand } from './commands/commands/break-line.command';

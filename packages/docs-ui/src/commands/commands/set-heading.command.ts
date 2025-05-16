69457270fa438f82bd4921e4a9410a4eceeea802
zhangw <zhang9748@foxmail.com>
2025-05-16 23:13:11 +0800

    feat: reset line-spacing when set heading

--- packages/docs-ui/src/commands/commands/set-heading.command.ts
+++ packages/docs-ui/src/commands/commands/set-heading.command.ts
@@ -54,6 +54,7 @@ export const SetParagraphNamedStyleCommand: ICommand<ISetParagraphNamedStyleComm
                 headingId: !params.value || params.value === NamedStyleType.NORMAL_TEXT ? undefined : generateRandomId(6),
                 spaceAbove: undefined,
                 spaceBelow: undefined,
+                lineSpacing: undefined,
             },
             paragraphTextRun: {},
 

499a44af48d65b08b61cbdbacf26d28c5013612f
wpxp123456 <2677556700@qq.com>
2025-06-03 11:23:39 +0800

    fix(formula): fix INDEX formula calculation error (#5311)

--- packages/engine-formula/src/functions/lookup/index/index.ts
+++ packages/engine-formula/src/functions/lookup/index/index.ts
@@ -16,10 +16,13 @@
 
 import type { BaseReferenceObject, FunctionVariantType } from '../../../engine/reference-object/base-reference-object';
 import type { ArrayValueObject } from '../../../engine/value-object/array-value-object';
+import type { BaseValueObject } from '../../../engine/value-object/base-value-object';
+import type { CubeValueObject } from '../../../engine/value-object/cube-value-object';
 import { ErrorType } from '../../../basics/error-type';
 import { expandArrayValueObject } from '../../../engine/utils/array-object';
-import { type BaseValueObject, ErrorValueObject } from '../../../engine/value-object/base-value-object';
-import { NullValueObject, NumberValueObject } from '../../../engine/value-object/primitive-object';
+import { checkVariantsErrorIsStringToNumber } from '../../../engine/utils/check-variant-error';
+import { ErrorValueObject } from '../../../engine/value-object/base-value-object';
+import { NumberValueObject } from '../../../engine/value-object/primitive-object';
 import { BaseFunction } from '../../base-function';
 
 /**
@@ -31,104 +34,90 @@ import { BaseFunction } from '../../base-function';
  *
  */
 export class Index extends BaseFunction {
-    override minParams = 1;
+    override minParams = 2;
 
     override maxParams = 4;
 
     override needsReferenceObject = true;
 
     // eslint-disable-next-line max-lines-per-function, complexity
-    override calculate(reference: FunctionVariantType, rowNum?: FunctionVariantType, columnNum?: FunctionVariantType, areaNum?: FunctionVariantType) {
+    override calculate(
+        reference: FunctionVariantType,
+        rowNum: FunctionVariantType,
+        columnNum?: FunctionVariantType,
+        areaNum?: FunctionVariantType
+    ): BaseValueObject | BaseReferenceObject {
         if (reference.isError()) {
             return reference;
         }
 
-        if (rowNum?.isError()) {
-            return rowNum;
+        const referenceRowColumnCounts = this._getReferenceCounts(reference);
+
+        let _rowNum = rowNum;
+
+        if (rowNum.isReferenceObject()) {
+            _rowNum = (rowNum as BaseReferenceObject).toArrayValueObject();
         }
 
-        if (columnNum?.isError()) {
-            return columnNum;
-        }
+        let _columnNum = columnNum;
 
-        if (areaNum?.isError()) {
-            return areaNum;
-        }
-
-        let referenceRowCount = 0;
-        let referenceColumnCount = 0;
-
-        if (reference.isValueObject()) {
-            referenceRowCount = 1;
-            referenceColumnCount = 1;
-        } else if (reference.isReferenceObject()) {
-            const { startRow, endRow, startColumn, endColumn } = (reference as BaseReferenceObject).getRangePosition();
-            referenceRowCount = endRow - startRow + 1;
-            referenceColumnCount = endColumn - startColumn + 1;
-        } else {
-            return ErrorValueObject.create(ErrorType.VALUE);
-        }
-
-        let _rowNum, _columnNum;
-        // When there is only one row, the rowNum is considered to be the column number.
-        // =INDEX(A6:B6,2) equals =INDEX(A6:B6,1,2)
-        if (referenceRowCount === 1 && referenceColumnCount > 1 && columnNum == null) {
-            _columnNum = rowNum ?? NumberValueObject.create(0);
-            _rowNum = NumberValueObject.create(0);
-        } else {
-            _rowNum = rowNum ?? NumberValueObject.create(0);
-            _columnNum = columnNum ?? NumberValueObject.create(0);
+        if (_columnNum?.isReferenceObject()) {
+            _columnNum = (columnNum as BaseReferenceObject).toArrayValueObject();
         }
 
         let _areaNum = areaNum ?? NumberValueObject.create(1);
 
-        if (_rowNum.isReferenceObject()) {
-            _rowNum = (_rowNum as BaseReferenceObject).toArrayValueObject();
-        }
-
-        if (_columnNum.isReferenceObject()) {
-            _columnNum = (_columnNum as BaseReferenceObject).toArrayValueObject();
-        }
-
         if (_areaNum.isReferenceObject()) {
-            _areaNum = (_areaNum as BaseReferenceObject).toArrayValueObject();
+            _areaNum = (areaNum as BaseReferenceObject).toArrayValueObject();
         }
 
         // get max row length
         const maxRowLength = Math.max(
             _rowNum.isArray() ? (_rowNum as ArrayValueObject).getRowCount() : 1,
-            _columnNum.isArray() ? (_columnNum as ArrayValueObject).getRowCount() : 1,
+            _columnNum?.isArray() ? (_columnNum as ArrayValueObject).getRowCount() : 1,
             _areaNum.isArray() ? (_areaNum as ArrayValueObject).getRowCount() : 1
         );
 
         // get max column length
         const maxColumnLength = Math.max(
             _rowNum.isArray() ? (_rowNum as ArrayValueObject).getColumnCount() : 1,
-            _columnNum.isArray() ? (_columnNum as ArrayValueObject).getColumnCount() : 1,
+            _columnNum?.isArray() ? (_columnNum as ArrayValueObject).getColumnCount() : 1,
             _areaNum.isArray() ? (_areaNum as ArrayValueObject).getColumnCount() : 1
         );
-
-        _rowNum = _rowNum as BaseValueObject;
-        _columnNum = _columnNum as BaseValueObject;
-        _areaNum = _areaNum as BaseValueObject;
 
         // If maxRowLength and maxColumnLength are both 1, pick the value from the reference array
         // Otherwise, filter the results from the reference according to the specified rowNum/columnNum/areaNum, take the upper left corner cell value of each result array, and then form an array with maxRowLength row number and maxColumnLength column number.
         if (maxRowLength === 1 && maxColumnLength === 1) {
-            return this._calculateSingleCell(reference, _rowNum, _columnNum, _areaNum);
+            return this._handleSingleObject(
+                reference,
+                _rowNum.isArray()
+                    ? (_rowNum as ArrayValueObject).get(0, 0) as BaseValueObject
+                    : _rowNum as BaseValueObject,
+                _columnNum
+                    ? (
+                        _columnNum.isArray()
+                            ? (_columnNum as ArrayValueObject).get(0, 0) as BaseValueObject
+                            : _columnNum as BaseValueObject
+                    )
+                    : undefined,
+                _areaNum.isArray()
+                    ? (_areaNum as ArrayValueObject).get(0, 0) as BaseValueObject
+                    : _areaNum as BaseValueObject,
+                referenceRowColumnCounts
+            );
         } else {
-            const rowNumArray = expandArrayValueObject(maxRowLength, maxColumnLength, _rowNum, ErrorValueObject.create(ErrorType.NA));
-            const columnNumArray = expandArrayValueObject(maxRowLength, maxColumnLength, _columnNum, ErrorValueObject.create(ErrorType.NA));
-            const areaNumArray = expandArrayValueObject(maxRowLength, maxColumnLength, _areaNum, ErrorValueObject.create(ErrorType.NA));
+            const rowNumArray = expandArrayValueObject(maxRowLength, maxColumnLength, _rowNum as BaseValueObject, ErrorValueObject.create(ErrorType.NA));
+            const columnNumArray = _columnNum ? expandArrayValueObject(maxRowLength, maxColumnLength, _columnNum as BaseValueObject, ErrorValueObject.create(ErrorType.NA)) : [];
+            const areaNumArray = expandArrayValueObject(maxRowLength, maxColumnLength, _areaNum as BaseValueObject, ErrorValueObject.create(ErrorType.NA));
 
-            return rowNumArray.map((rowNumValue, rowIndex, columnIndex) => {
-                const columnNumValue = columnNumArray.get(rowIndex, columnIndex) || NullValueObject.create();
-                const areaNumValue = areaNumArray.get(rowIndex, columnIndex) || NullValueObject.create();
+            return rowNumArray.mapValue((rowNumObject, rowIndex, columnIndex) => {
+                const columnNumObject = _columnNum ? (columnNumArray as ArrayValueObject).get(rowIndex, columnIndex) as BaseValueObject : undefined;
+                const areaNumObject = areaNumArray.get(rowIndex, columnIndex) as BaseValueObject;
 
-                const result = this._calculateSingleCell(reference, rowNumValue, columnNumValue, areaNumValue);
+                const result = this._handleSingleObject(reference, rowNumObject, columnNumObject, areaNumObject, referenceRowColumnCounts);
 
                 if (result.isReferenceObject()) {
-                    return (result as BaseReferenceObject).toArrayValueObject().getFirstCell();
+                    return (result as BaseReferenceObject).getCellByPosition();
                 }
 
                 return result as BaseValueObject;
@@ -136,94 +125,121 @@ export class Index extends BaseFunction {
         }
     }
 
-    private _calculateSingleCell(reference: FunctionVariantType, rowNum: BaseValueObject, columnNum: BaseValueObject, areaNum: BaseValueObject) {
-        if (rowNum.isError()) {
-            return rowNum;
-        }
+    private _handleSingleObject(
+        reference: FunctionVariantType,
+        rowNum: BaseValueObject,
+        columnNum: BaseValueObject | undefined,
+        areaNum: BaseValueObject,
+        referenceRowColumnCounts: { rowCount: number; columnCount: number }[]
+    ): BaseReferenceObject | BaseValueObject {
+        let _rowNum;
+        let _columnNum;
+        let _areaNum;
 
-        const rowNumberValue = this._getNumberValue(rowNum);
+        if (columnNum) {
+            const { isError, errorObject, variants } = checkVariantsErrorIsStringToNumber(rowNum, columnNum, areaNum);
 
-        if (rowNumberValue === undefined || rowNumberValue < 0) {
-            return ErrorValueObject.create(ErrorType.VALUE);
-        }
+            if (isError) {
+                return errorObject as ErrorValueObject;
+            }
 
-        if (columnNum.isError()) {
-            return columnNum;
-        }
-
-        const columnNumberValue = this._getNumberValue(columnNum);
-
-        if (columnNumberValue === undefined || columnNumberValue < 0) {
-            return ErrorValueObject.create(ErrorType.VALUE);
-        }
-
-        // TODO areaNum
-        if (areaNum.isError()) {
-            return areaNum;
-        }
-
-        const areaNumberValue = this._getAreaNumberValue(areaNum);
-
-        if (areaNumberValue === undefined || areaNumberValue < 1) {
-            return ErrorValueObject.create(ErrorType.VALUE);
-        }
-
-        if (reference.isReferenceObject()) {
-            return this._getReferenceObject(reference as BaseReferenceObject, rowNumberValue, columnNumberValue, areaNumberValue);
-        } else if (reference.isValueObject() && rowNumberValue === 1 && columnNumberValue === 1) {
-            return reference;
+            _rowNum = (variants as BaseValueObject[])[0];
+            _columnNum = (variants as BaseValueObject[])[1];
+            _areaNum = (variants as BaseValueObject[])[2];
         } else {
+            const { isError, errorObject, variants } = checkVariantsErrorIsStringToNumber(rowNum, areaNum);
+
+            if (isError) {
+                return errorObject as ErrorValueObject;
+            }
+
+            _rowNum = (variants as BaseValueObject[])[0];
+            _areaNum = (variants as BaseValueObject[])[1];
+        }
+
+        const areaNumValue = Math.floor(+_areaNum.getValue());
+
+        if (areaNumValue < 0) {
+            return ErrorValueObject.create(ErrorType.VALUE);
+        }
+
+        if (areaNumValue > referenceRowColumnCounts.length) {
             return ErrorValueObject.create(ErrorType.REF);
         }
-    }
 
-    private _getNumberValue(numberValueObject?: BaseValueObject) {
-        if (numberValueObject == null) {
-            return 0;
-        }
+        const referenceRowColumnCount = referenceRowColumnCounts[areaNumValue - 1];
 
-        let logicValue = 0;
+        let rowNumValue = 0;
+        let columnNumValue = 0;
 
-        if (numberValueObject.isBoolean()) {
-            const logicV = numberValueObject.getValue() as boolean;
-            if (logicV === true) {
-                logicValue = 1;
+        // When there is only one row, the rowNum is considered to be the column number.
+        // =INDEX(A6:B6,2) equals =INDEX(A6:B6,1,2)
+        if (referenceRowColumnCount.rowCount === 1 && !columnNum) {
+            columnNumValue = Math.floor(+_rowNum.getValue());
+        } else {
+            rowNumValue = Math.floor(+_rowNum.getValue());
+
+            if (columnNum) {
+                columnNumValue = Math.floor(+(_columnNum as BaseValueObject).getValue());
             }
-        } else if (numberValueObject.isString()) {
-            return;
-        } else if (numberValueObject.isNumber()) {
-            logicValue = Math.floor(numberValueObject.getValue() as number);
-        } else if (numberValueObject.isNull()) {
-            logicValue = 0;
         }
 
-        return logicValue;
+        if (rowNumValue < 0 || columnNumValue < 0) {
+            return ErrorValueObject.create(ErrorType.VALUE);
+        }
+
+        if (rowNumValue > referenceRowColumnCount.rowCount || columnNumValue > referenceRowColumnCount.columnCount) {
+            return ErrorValueObject.create(ErrorType.REF);
+        }
+
+        let _reference = reference;
+
+        if (areaNumValue > 1) {
+            _reference = (reference as CubeValueObject).getCubeValues()[areaNumValue - 1];
+        }
+
+        if (_reference.isReferenceObject()) {
+            return this._calculateReferenceObject(_reference as BaseReferenceObject, rowNumValue, columnNumValue);
+        }
+
+        return this._calculateArrayObject(_reference as BaseValueObject, rowNumValue, columnNumValue);
     }
 
-    private _getAreaNumberValue(numberValueObject?: BaseValueObject) {
-        if (numberValueObject == null) {
-            return 1;
+    private _getReferenceCounts(reference: FunctionVariantType): { rowCount: number; columnCount: number }[] {
+        let referenceRowColumnCounts = [
+            {
+                rowCount: 1,
+                columnCount: 1,
+            },
+        ];
+
+        if (reference.isReferenceObject()) {
+            referenceRowColumnCounts = [
+                {
+                    rowCount: (reference as BaseReferenceObject).getRowCount(),
+                    columnCount: (reference as BaseReferenceObject).getColumnCount(),
+                },
+            ];
+        } else if ((reference as BaseValueObject).isCube()) {
+            referenceRowColumnCounts = (reference as CubeValueObject).getCubeValues().map((value) => {
+                return {
+                    rowCount: value.getRowCount(),
+                    columnCount: value.getColumnCount(),
+                };
+            });
+        } else if ((reference as BaseValueObject).isArray()) {
+            referenceRowColumnCounts = [
+                {
+                    rowCount: (reference as ArrayValueObject).getRowCount(),
+                    columnCount: (reference as ArrayValueObject).getColumnCount(),
+                },
+            ];
         }
 
-        let logicValue = 0;
-
-        if (numberValueObject.isBoolean()) {
-            const logicV = numberValueObject.getValue() as boolean;
-            if (logicV === true) {
-                logicValue = 1;
-            }
-        } else if (numberValueObject.isString()) {
-            return;
-        } else if (numberValueObject.isNumber()) {
-            logicValue = Math.floor(numberValueObject.getValue() as number);
-        } else if (numberValueObject.isNull()) {
-            logicValue = 0;
-        }
-
-        return logicValue;
+        return referenceRowColumnCounts;
     }
 
-    private _getReferenceObject(reference: BaseReferenceObject, rowNumberValue: number, columnNumberValue: number, areaNumberValue: number) {
+    private _calculateReferenceObject(reference: BaseReferenceObject, rowNum: number, columnNum: number): BaseReferenceObject | ErrorValueObject {
         const { startRow, endRow, startColumn, endColumn } = reference.getRangePosition();
 
         let referenceStartRow = 0;
@@ -231,18 +247,18 @@ export class Index extends BaseFunction {
         let referenceStartColumn = 0;
         let referenceEndColumn = 0;
 
-        if (rowNumberValue === 0) {
+        if (rowNum === 0) {
             referenceStartRow = startRow;
             referenceEndRow = endRow;
         } else {
-            referenceStartRow = referenceEndRow = startRow + rowNumberValue - 1;
+            referenceStartRow = referenceEndRow = startRow + rowNum - 1;
         }
 
-        if (columnNumberValue === 0) {
+        if (columnNum === 0) {
             referenceStartColumn = startColumn;
             referenceEndColumn = endColumn;
         } else {
-            referenceStartColumn = referenceEndColumn = startColumn + columnNumberValue - 1;
+            referenceStartColumn = referenceEndColumn = startColumn + columnNum - 1;
         }
 
         if (referenceStartRow > endRow || referenceStartColumn > endColumn) {
@@ -257,5 +273,21 @@ export class Index extends BaseFunction {
         };
 
         return this.createReferenceObject(reference, range);
+    }
+
+    private _calculateArrayObject(reference: BaseValueObject, rowNum: number, columnNum: number): BaseValueObject {
+        if (!reference.isArray() || (rowNum === 0 && columnNum === 0)) {
+            return reference;
+        }
+
+        if (rowNum === 0) {
+            return (reference as ArrayValueObject).slice(undefined, [columnNum - 1, columnNum]) as ArrayValueObject;
+        }
+
+        if (columnNum === 0) {
+            return (reference as ArrayValueObject).slice([rowNum - 1, rowNum], undefined) as ArrayValueObject;
+        }
+
+        return (reference as ArrayValueObject).get(rowNum - 1, columnNum - 1) as BaseValueObject;
     }
 }

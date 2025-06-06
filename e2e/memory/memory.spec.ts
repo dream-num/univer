5feb3d136e3e98f7b13989590dfedf4a25314d1d
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2025-06-06 09:49:11 +0800

    test: ensure no error on disposing tests

--- e2e/memory/memory.spec.ts
+++ e2e/memory/memory.spec.ts
@@ -160,8 +160,7 @@ test('memory', async ({ page }) => {
     expect(instanceMemoryOverflow).toBeLessThanOrEqual(MAX_SECOND_INSTANCE_OVERFLOW);
     expect(univerMemoryOverflow).toBeLessThanOrEqual(MAX_UNIVER_MEMORY_OVERFLOW);
 
-    // TODO: enable this when the memory leak is fixed
-    // expect(errored).toBeFalsy();
+    expect(errored).toBeFalsy();
 });
 
 declare global {

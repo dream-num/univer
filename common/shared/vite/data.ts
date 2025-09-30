8fc2dc4da64b954c5edf1dbbd5e6512e5fd56c50
白熱 <sonne@asaki.me>
2025-09-30 12:20:25 +0800

    chore: update dependencies (#5933)

--- common/shared/vite/data.ts
+++ common/shared/vite/data.ts
@@ -48,7 +48,7 @@ export const peerDepsMap = {
     '@wendellhu/redi': {
         global: '@wendellhu/redi',
         name: '@wendellhu/redi',
-        version: '1.0.0',
+        version: '1.0.1',
     },
     '@wendellhu/redi/react-bindings': {
         global: '@wendellhu/redi/react-bindings',
--- packages/uniscript/src/services/script-editor.service.ts
+++ packages/uniscript/src/services/script-editor.service.ts
@@ -49,8 +49,10 @@ export class ScriptEditorService extends Disposable {
     }
 
     requireVscodeEditor(): void {
+        // @ts-expect-error monaco editor global
         if (!window.MonacoEnvironment) {
             const config = this._configService.getConfig<IUniverUniscriptConfig>(UNISCRIPT_PLUGIN_CONFIG_KEY);
+            // @ts-expect-error monaco editor global
             window.MonacoEnvironment = { getWorkerUrl: config?.getWorkerUrl };
         }
     }

6547330811080c43ef23aced71d98b3e3d4cc076
Roman Sergeev <109460313+kenny-not-dead@users.noreply.github.com>
2025-11-01 05:36:37 +0300

    feat(docs-hyper-link-ui): add ru lang to comment-ui (#6072)
    Co-authored-by: r.sergeev <r.sergeev@yadro.com>

--- packages/thread-comment-ui/src/locale/ru-RU.ts
+++ packages/thread-comment-ui/src/locale/ru-RU.ts
@@ -14,8 +14,41 @@
  * limitations under the License.
  */
 
-import enUS from './en-US';
+import type enUS from './en-US';
 
-const locale: typeof enUS = enUS;
+const locale: typeof enUS = {
+    threadCommentUI: {
+        panel: {
+            title: 'Управление комментариями',
+            empty: 'Комментариев пока нет',
+            filterEmpty: 'Нет совпадений',
+            reset: 'Сбросить фильтр',
+            addComment: 'Добавить комментарий',
+            solved: 'Решено',
+        },
+        editor: {
+            placeholder: 'Отметьте пользователей через @',
+            reply: 'Добавить комментарий',
+            cancel: 'Отмена',
+            save: 'Сохранить',
+        },
+        item: {
+            edit: 'Редактировать',
+            delete: 'Удалить',
+        },
+        filter: {
+            sheet: {
+                all: 'Все листы',
+                current: 'Текущий лист',
+            },
+            status: {
+                all: 'Все комментарии',
+                resolved: 'Решено',
+                unsolved: 'Не решено',
+                concernMe: 'Относящиеся ко мне',
+            },
+        },
+    },
+};
 
 export default locale;

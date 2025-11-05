fc1ffca626310e31ac15d3611040defa5ae3db8a
白熱 <sonne@asaki.me>
2025-11-05 20:07:17 +0800

    feat: new ribbon

---
+++ packages/ui/src/views/components/ribbon/ribbon-menu/ClassicMenu.tsx
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2023-present DreamNum Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { IMenuSchema } from '../../../../services/menu/menu-manager.service';
+import { LocaleService } from '@univerjs/core';
+import { useDependency } from '../../../../utils/di';
+
+export function ClassicMenu({
+    ribbon,
+    activatedTab,
+    onSelectTab,
+}: {
+    ribbon: IMenuSchema[];
+    activatedTab: string;
+    onSelectTab: (tab: IMenuSchema) => void;
+}) {
+    const localeService = useDependency(LocaleService);
+
+    return (
+        <div className="univer-flex univer-w-full univer-items-center univer-justify-center">
+            {ribbon.map((group) => (
+                <button
+                    key={group.key}
+                    className="univer-appearance-none univer-border-none univer-bg-none"
+                    type="button"
+                    onClick={() => onSelectTab(group)}
+                >
+                    {localeService.t(group.key)}
+                </button>
+            ))}
+        </div>
+    );
+}

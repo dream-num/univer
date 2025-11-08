a6063afb5421cee3bb68d35a46f57426da579075
Univer <68851825+DR-Univer@users.noreply.github.com>
2025-11-08 09:01:41 +0000

    fix(export): finished alg

--- packages/engine-formula/src/engine/analysis/lexer-tree-builder.ts
+++ packages/engine-formula/src/engine/analysis/lexer-tree-builder.ts
@@ -63,6 +63,9 @@ enum NewExcelFunctionNodeType {
     PARAMETER,
     LAMBDA_PARAMETER,
     FUNCTION,
+    SUFFIX,
+    COLON,
+    MINUS,
     OTHER,
 }
 
@@ -1919,7 +1922,10 @@ export class LexerTreeBuilder extends Disposable {
 
         const tokenTrim = token.trim();
         const tokenTrimUpper = tokenTrim.toUpperCase();
-        const isFunctionNode = hasFunction(tokenTrimUpper);
+
+        const tokenForFunction = this._clearFunctionString(tokenTrimUpper);
+        const isFunctionNode = hasFunction(tokenForFunction);
+
         let curNodeType = NewExcelFunctionNodeType.NORMAL;
 
         if (token === DEFAULT_TOKEN_TYPE_ROOT) {
@@ -1928,7 +1934,7 @@ export class LexerTreeBuilder extends Disposable {
             curNodeType = NewExcelFunctionNodeType.PARAMETER;
         } else if (token === DEFAULT_TOKEN_TYPE_LAMBDA_PARAMETER) {
             curNodeType = NewExcelFunctionNodeType.LAMBDA_PARAMETER;
-        } else if (NEW_EXCEL_FUNCTIONS.has(tokenTrimUpper)) {
+        } else if (NEW_EXCEL_FUNCTIONS.has(tokenForFunction)) {
             formulaStrings.push(`_xlfn.${tokenTrim}`);
             this._hasNewExcelFunction = true;
         } else if (tokenTrimUpper === DEFAULT_TOKEN_LAMBDA_FUNCTION_NAME) {
@@ -1939,6 +1945,15 @@ export class LexerTreeBuilder extends Disposable {
             formulaStrings.push(`_xlfn.${tokenTrim}`);
             this._hasNewExcelFunction = true;
             curNodeType = NewExcelFunctionNodeType.LET;
+        } else if (tokenTrimUpper === matchToken.COLON) {
+            curNodeType = NewExcelFunctionNodeType.COLON;
+        } else if (SUFFIX_TOKEN_SET.has(tokenTrimUpper)) {
+            curNodeType = NewExcelFunctionNodeType.SUFFIX;
+        } else if (tokenTrimUpper === prefixToken.MINUS) {
+            if (this._checkAddBracketForMinus(lexerNode)) {
+                curNodeType = NewExcelFunctionNodeType.MINUS;
+            }
+            formulaStrings.push(token);
         } else {
             formulaStrings.push(token);
             curNodeType = NewExcelFunctionNodeType.OTHER;
@@ -1980,32 +1995,119 @@ export class LexerTreeBuilder extends Disposable {
         }
 
         if (isFunctionNode) {
-            curNodeType = NewExcelFunctionNodeType.FUNCTION;
+            if (curNodeType !== NewExcelFunctionNodeType.LAMBDA && curNodeType !== NewExcelFunctionNodeType.LET) {
+                curNodeType = NewExcelFunctionNodeType.FUNCTION;
+            }
+            formulaStrings.push('(');
+        } else if (curNodeType === NewExcelFunctionNodeType.MINUS) {
             formulaStrings.push('(');
         }
 
         const children = lexerNode.getChildren();
         const childrenCount = children.length;
+        let firstChild: string | LexerNode | null = null;
+
+        // lambda表达式的第一个子节点存放了入参，得把它处理到参数后并加括号，例如：LAMBDA(a,b, a*b)(x, y)中得 x,y
+        if (curNodeType === NewExcelFunctionNodeType.LAMBDA) {
+            firstChild = children[0];
+        } else if (curNodeType === NewExcelFunctionNodeType.COLON) {
+            const firstNode = children[0];
+            const secondNode = children[1];
+
+            this._handleNewFunctionChild(firstNode, formulaStrings, hasFunction, curNodeType, 0, 2, firstChild);
+            formulaStrings.push(token);
+            this._handleNewFunctionChild(secondNode, formulaStrings, hasFunction, curNodeType, 1, 2, firstChild);
+
+            return;
+        }
+
         for (let i = 0; i < childrenCount; i++) {
             const item = children[i];
-            if (item instanceof LexerNode) {
-                this._generateNewFunctionString(item, formulaStrings, hasFunction, curNodeType, i, childrenCount);
-            } else {
-                if (this._lambdaFunctionParameterSet.has(item)) {
-                    formulaStrings.push(`_xlpm.${item}`);
-                    this._hasNewExcelFunction = true;
-                } else {
-                    formulaStrings.push(item);
+            this._handleNewFunctionChild(item, formulaStrings, hasFunction, curNodeType, i, childrenCount, firstChild);
+
+            if (item instanceof LexerNode && !(firstChild && i === 0)) {
+                const nextItem = children[i + 1];
+                if (nextItem && nextItem instanceof LexerNode) {
+                    formulaStrings.push(',');
                 }
             }
+        }
 
-            if (isFunctionNode && i !== childrenCount - 1) {
-                formulaStrings.push(',');
-            }
+        if (curNodeType === NewExcelFunctionNodeType.SUFFIX) {
+            formulaStrings.push(token);
         }
 
         if (isFunctionNode) {
             formulaStrings.push(')');
+        } else if (curNodeType === NewExcelFunctionNodeType.MINUS) {
+            formulaStrings.push(')');
         }
+
+        if (firstChild) {
+            formulaStrings.push('(');
+            this._generateNewFunctionString(firstChild as LexerNode, formulaStrings, hasFunction, curNodeType, 0, childrenCount);
+            formulaStrings.push(')');
+        }
+    }
+
+    private _handleNewFunctionChild(item: string | LexerNode, formulaStrings: (string | number | boolean)[], hasFunction: (functionToken: IFunctionNames) => boolean, curNodeType = NewExcelFunctionNodeType.NORMAL, i?: number, childrenCount?: number, firstChild?: string | LexerNode | null) {
+        if (item instanceof LexerNode) {
+            if (!(firstChild && i === 0)) {
+                this._generateNewFunctionString(item, formulaStrings, hasFunction, curNodeType, i, childrenCount);
+            }
+        } else {
+            if (this._lambdaFunctionParameterSet.has(item)) {
+                formulaStrings.push(`_xlpm.${item}`);
+                this._hasNewExcelFunction = true;
+            } else {
+                formulaStrings.push(item);
+            }
+        }
+    }
+
+    private _clearFunctionString(token: string): string {
+        let t = token.trim();
+        if (!t) return t;
+
+        const firstChar = t[0];
+        if (
+            firstChar === prefixToken.AT ||
+            firstChar === prefixToken.MINUS ||
+            firstChar === prefixToken.PLUS
+        ) {
+            t = t.slice(1);
+        }
+
+        if (!t) return t;
+
+        const lastChar = t[t.length - 1];
+        if (SUFFIX_TOKEN_SET.has(lastChar)) {
+            t = t.slice(0, -1);
+        }
+
+        return t;
+    }
+
+    private _checkAddBracketForMinus(node: LexerNode) {
+        const childrenFirst = node.getChildren()[0];
+        if (!childrenFirst || !(childrenFirst instanceof LexerNode) || node.getChildren().length > 1) {
+            return false;
+        }
+        const children = childrenFirst.getChildren();
+        const childrenCount = children.length;
+
+        if (childrenCount === 1) {
+            return false;
+        }
+
+        for (let i = 0; i < childrenCount; i++) {
+            const item = children[i];
+
+            if (!(item instanceof LexerNode) && OPERATOR_TOKEN_SET.has(item)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }

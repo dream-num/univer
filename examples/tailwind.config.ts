7b7f53b22a6ed4bc18c404056a42e6fc15ff147e
白熱 <sonne@asaki.me>
2026-01-06 19:05:04 +0800

    fix(debugger): add data attribute to Fab component for debugging

--- examples/tailwind.config.ts
+++ examples/tailwind.config.ts
@@ -24,8 +24,9 @@ import animate from 'tailwindcss-animate';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const packagesDir = fs.readdirSync(path.resolve(__dirname, '../packages')).map((dir) => path.resolve(__dirname, `../packages/${dir}`));
+const packagesExperimentalDir = fs.readdirSync(path.resolve(__dirname, '../common')).map((dir) => path.resolve(__dirname, `../common/${dir}`));
 
-const tailwindProjects = packagesDir.reduce((acc, dir) => {
+const tailwindProjects = packagesDir.concat(packagesExperimentalDir).reduce((acc, dir) => {
     const tailwindConfig = path.resolve(dir, 'tailwind.config.ts');
     if (fs.existsSync(tailwindConfig)) {
         acc.push(`${dir}/src/**/*.{js,ts,jsx,tsx}`);

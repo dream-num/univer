83fd9a94799ace99c16b7cb751bcc1c5fbff56ef
白熱 <sonne@asaki.me>
2026-05-13 12:00:23 +0800

    fix(ui): add box-sizing to context menu item for proper sizing

--- packages/ui/src/views/components/context-menu/ContextMenuPanel.tsx
+++ packages/ui/src/views/components/context-menu/ContextMenuPanel.tsx
@@ -540,8 +540,8 @@ function ContextMenuMenuItem(props: IContextMenuMenuItemProps) {
                                             const optionHoverable = !isNonHoverableLabel(option.label);
                                             const optionClassName = clsx(
                                                 `
-                                                  univer-relative univer-flex univer-min-h-8 univer-w-full
-                                                  univer-items-center univer-rounded-md univer-border-none
+                                                  univer-relative univer-box-border univer-flex univer-min-h-8
+                                                  univer-w-full univer-items-center univer-rounded-md univer-border-none
                                                   univer-bg-transparent univer-px-2 univer-text-left univer-text-sm
                                                   dark:!univer-text-white
                                                 `,

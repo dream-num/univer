a3968fba4a7786f9f95dbb3087ecfb7cdf36ab92
Wpxp123456 <2677556700@qq.com>
2026-06-02 16:08:21 +0800

    refactor(docs): move core editing commands and facade APIs into docs package

--- examples/src/sheets/main.ts
+++ examples/src/sheets/main.ts
@@ -67,6 +67,8 @@ import ImportCSVButtonPlugin from './custom/import-csv-button';
 
 import '@univerjs/sheets/facade';
 import '@univerjs/ui/facade';
+import '@univerjs/docs/facade';
+import '@univerjs/docs-ui/facade';
 import '@univerjs/sheets-ui/facade';
 import '@univerjs/sheets-data-validation/facade';
 import '@univerjs/engine-formula/facade';

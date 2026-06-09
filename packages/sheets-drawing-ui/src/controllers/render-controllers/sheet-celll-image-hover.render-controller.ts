cd9c7fba56e38847554ebf95e494530d937033ce
白熱 <sonne@asaki.me>
2026-06-09 12:01:03 +0800

    fix(sheets-drawing-ui): fix image zoom-in issue in merged cells (#7041)
    Co-authored-by: Wpxp123456 <2677556700@qq.com>

--- packages/sheets-drawing-ui/src/controllers/render-controllers/sheet-celll-image-hover.render-controller.ts
+++ packages/sheets-drawing-ui/src/controllers/render-controllers/sheet-celll-image-hover.render-controller.ts
@@ -48,16 +48,32 @@ export class SheetCellImageHoverRenderController extends Disposable implements I
                 if (richText !== null) {
                     currentSelections = this._selectionsService.getWorkbookSelections(this._context.unitId).getCurrentSelections();
                 }
+
                 if (
-                    currentSelections.length > 0 &&
                     richText?.unitId === this._context.unitId &&
                     richText?.drawing &&
                     currentSelections.length === 1 &&
-                    currentSelections[0].primary?.actualRow === richText.row &&
-                    currentSelections[0].primary?.actualColumn === richText.col
+                    currentSelections[0].primary
                 ) {
-                    this._isSetCursor = true;
-                    this._context.scene.setCursor(CURSOR_TYPE.ZOOM_IN);
+                    const { row, col } = richText;
+                    const { actualRow, actualColumn, startRow, startColumn, endRow, endColumn, isMerged, isMergedMainCell } = currentSelections[0].primary;
+
+                    if (
+                        (isMerged || isMergedMainCell) &&
+                        row >= startRow &&
+                        row <= endRow &&
+                        col >= startColumn &&
+                        col <= endColumn
+                    ) {
+                        this._isSetCursor = true;
+                        this._context.scene.setCursor(CURSOR_TYPE.ZOOM_IN);
+                    } else if (row === actualRow && col === actualColumn) {
+                        this._isSetCursor = true;
+                        this._context.scene.setCursor(CURSOR_TYPE.ZOOM_IN);
+                    } else if (this._isSetCursor) {
+                        this._isSetCursor = false;
+                        this._context.scene.resetCursor();
+                    }
                 } else if (this._isSetCursor) {
                     this._isSetCursor = false;
                     this._context.scene.resetCursor();

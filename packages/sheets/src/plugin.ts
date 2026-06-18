a7899e0589c31eeb66966b3373fad2aba8b1c554
Univer <wbfsa@qq.com>
2026-06-19 00:23:19 +0800

    fix(embed): register product embed contributions

--- packages/sheets/src/plugin.ts
+++ packages/sheets/src/plugin.ts
@@ -33,6 +33,7 @@ import { SheetPermissionCheckController } from './controllers/permission/sheet-p
 import { SheetPermissionInitController } from './controllers/permission/sheet-permission-init.controller';
 import { SheetPermissionViewModelController } from './controllers/permission/sheet-permission-view-model.controller';
 import { ZebraCrossingCacheController } from './controllers/zebar-crossing.controller';
+import { registerSheetsEmbedGuestContribution, registerSheetsEmbedHostCapabilities } from './embed-guest';
 import { RangeProtectionRenderModel } from './models/range-protection-render.model';
 import { RangeProtectionRuleModel } from './models/range-protection-rule.model';
 import { RangeProtectionCache } from './models/range-protection.cache';
@@ -153,6 +154,9 @@ export class UniverSheetsPlugin extends Plugin {
     }
 
     override onStarting(): void {
+        registerSheetsEmbedHostCapabilities(this._injector);
+        registerSheetsEmbedGuestContribution(this._injector);
+
         touchDependencies(this._injector, [
             [BasicWorksheetController],
             [MergeCellController],

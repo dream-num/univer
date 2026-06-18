e295f30a7da5650a4bc7a3a31fa9fe1d0b71fe37
Univer <wbfsa@qq.com>
2026-06-18 23:04:44 +0800

    fix(embed): centralize floating menu root resolution

---
+++ packages/embed-ui/src/services/embed-floating-menu-root.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2023-present DreamNum Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { EmbedFloatingMenuMountContext } from '../types/embed-ui';
+
+export function resolveEmbedFloatingMenuRoot(context: Pick<EmbedFloatingMenuMountContext, 'renderScope' | 'runtimeScope'>): HTMLElement {
+    if (context.renderScope.fullscreen && context.runtimeScope.roots.menuSlot) {
+        return context.runtimeScope.roots.menuSlot;
+    }
+
+    return context.runtimeScope.roots.overlay
+        ?? context.renderScope.overlayRoot
+        ?? context.renderScope.rootElement;
+}

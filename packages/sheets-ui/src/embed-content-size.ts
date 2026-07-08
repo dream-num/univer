35d4f7dc5a096892bd19d26d3871a7abd45077ab
Univer <wbfsa@qq.com>
2026-07-08 11:47:27 +0800

    refactor(embed): remove empty ui compatibility stubs

--- packages/sheets-ui/src/embed-content-size.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright 2023-present DreamNum Co., Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- */
-
-export function createSheetsContentSizeProvider(): undefined {
-    return undefined;
-}

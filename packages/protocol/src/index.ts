57d36e11390d12574d1fab7610d21357a14afc7e
Wpxp123456 <2677556700@qq.com>
2026-07-15 14:30:16 +0800

    Revert "refactor(SSC): remove SSC function (#7264)"
    This reverts commit 113a7519eeb1ce4920d5cda74220bdc9a768724a.

--- packages/protocol/src/index.ts
+++ packages/protocol/src/index.ts
@@ -93,6 +93,15 @@ export {
     type ICreateLatestSnapshotInBackgroundResponse,
 } from './ts/univerpro/v1/helper';
 export type {
+    IComputeRequest,
+    IComputeResponse,
+    IGetPreprocessRangesRequest,
+    IGetPreprocessRangesResponse,
+    IGetValuesRequest,
+    IGetValuesResponse,
+    ITableInfoList,
+} from './ts/univerpro/v1/ssc';
+export type {
     IGetSSRRequest,
     IGetSSRResponse,
 } from './ts/univerpro/v1/ssr';

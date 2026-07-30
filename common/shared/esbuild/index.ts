b13870b179ce17a705361027ed5bbba342f84957
Univer <wbfsa@qq.com>
2026-07-30 15:13:26 +0800

    revert(build): keep shared css matching unchanged

--- common/shared/esbuild/index.ts
+++ common/shared/esbuild/index.ts
@@ -30,7 +30,7 @@ export function ignoreGlobalCssPlugin(): IEsbuildPlugin {
         name: 'ignore-global-css',
         setup(build: IPluginBuild) {
             build.onResolve({ filter: /\/global\.css$/ }, (args: IResolveArgs) => {
-                if (/[\\/]packages(?:-experimental)?[\\/][^\\/]+[\\/]src[\\/]/.test(args.importer)) {
+                if (args.importer.includes('packages')) {
                     return {
                         path: args.path,
                         namespace: 'ignore-global-css',

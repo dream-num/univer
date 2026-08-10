8a25f7602e05a726a2401381edc6f007425b73b9
Univer <68851825+DR-Univer@users.noreply.github.com>
2026-08-11 01:52:53 +0800

    fix(engine-render): export document extension type (#7473)

--- packages/engine-render/src/index.ts
+++ packages/engine-render/src/index.ts
@@ -46,6 +46,7 @@ export * from './components';
 export type { DocsCustomBlockRenderViewportProvider, IDocsCustomBlockRenderViewport, IDocsCustomBlockRenderViewportInput } from './components/docs/custom-block-render-viewport';
 export { getDocsCustomBlockRenderViewport, setDocsCustomBlockRenderViewportProvider } from './components/docs/custom-block-render-viewport';
 export { DocBackground } from './components/docs/doc-background';
+export { DOCS_EXTENSION_TYPE } from './components/docs/doc-extension';
 export { Documents } from './components/docs/document';
 export type { IPageRenderConfig } from './components/docs/document';
 export type { IDocumentOffsetConfig } from './components/docs/document';

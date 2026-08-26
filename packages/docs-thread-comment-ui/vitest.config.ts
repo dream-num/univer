5e9f45b5d52f6b7ed6ee20285d80bb77a7b577be
Univer <wbfsa@qq.com>
2026-08-26 11:59:27 +0800

    test(thread-comment): restore shared vitest config

--- packages/docs-thread-comment-ui/vitest.config.ts
+++ packages/docs-thread-comment-ui/vitest.config.ts
@@ -1,18 +1,3 @@
-import { createRequire } from 'node:module';
 import createConfig from '@univerjs-infra/shared/vitest';
 
-const require = createRequire(import.meta.url);
-const reactRequire = createRequire(require.resolve('react'));
-
-export default createConfig({
-    resolve: {
-        alias: [
-            { find: /^react$/, replacement: require.resolve('react') },
-            { find: /^react\/jsx-runtime$/, replacement: require.resolve('react/jsx-runtime') },
-            { find: /^react\/jsx-dev-runtime$/, replacement: require.resolve('react/jsx-dev-runtime') },
-            { find: /^react-dom$/, replacement: reactRequire.resolve('react-dom') },
-            { find: /^react-dom\/client$/, replacement: reactRequire.resolve('react-dom/client') },
-            { find: /^react-dom\/test-utils$/, replacement: reactRequire.resolve('react-dom/test-utils') },
-        ],
-    },
-});
+export default createConfig();

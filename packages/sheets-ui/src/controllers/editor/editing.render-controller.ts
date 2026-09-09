64ce93b59bc8c339f1386974aab9b8efe9a1b14f
Wpxp123456 <2677556700@qq.com>
2026-09-09 12:27:17 +0800

    fix(sheets-ui): restore formula bar after canceling edit

--- packages/sheets-ui/src/controllers/editor/editing.render-controller.ts
+++ packages/sheets-ui/src/controllers/editor/editing.render-controller.ts
@@ -731,6 +731,7 @@ export class EditingRenderController extends Disposable {
             if (this._editorBridgeService.isForceKeepVisible()) {
                 this._editorBridgeService.disableForceKeepVisible();
             }
+            this._editorBridgeService.refreshEditCellState();
             this._refreshCurrentSelections(sheetId);
             return;
         }
